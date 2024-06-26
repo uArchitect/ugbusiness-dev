2bdcc454c862414e07209fc0712a7e728590c9da
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-06-26 16:28:13 +0300

    0

--- application/controllers/Musteri.php
+++ application/controllers/Musteri.php
@@ -19,8 +19,8 @@ class Musteri extends CI_Controller {
 	public function index()
 	{ 
         yetki_kontrol("musterileri_goruntule");
-        $data = $this->Musteri_model->get_all(); 
-		$viewData["musteriler"] = $data;
+      //  $data = $this->Musteri_model->get_all(); 
+		//$viewData["musteriler"] = $data;
 		$viewData["page"] = "musteri/list";
 		$this->load->view('base_view',$viewData);
 	}
@@ -222,4 +222,61 @@ class Musteri extends CI_Controller {
         }
 		redirect(site_url('musteri'));
 	}
+
+
+
+
+
+
+
+    public function musteriler_ajax() {
+        yetki_kontrol("musterileri_goruntule");
+        $limit = $this->input->get('length');
+        $start = $this->input->get('start');
+        $search = $this->input->get('search')['value']; 
+        $order = $this->input->get('order')[0]['column'];
+        $dir = $this->input->get('order')[0]['dir'];
+
+        if(!empty($search)) {
+            $this->db->like('musteri_ad', $search); 
+        }
+
+        $query = $this->db
+                      ->select('musteriler.musteri_id,musteriler.musteri_ad,musteriler.musteri_kod,musteriler.musteri_iletisim_numarasi, merkezler.merkez_adi,merkezler.merkez_adi,sehirler.sehir_adi,ilceler.ilce_adi')
+                      ->from('musteriler')
+                      ->join('(SELECT * FROM merkezler ORDER BY merkez_id DESC) as merkezler', 'merkezler.merkez_yetkili_id = musteri_id', 'left')
+                      ->join('sehirler', 'sehirler.sehir_id = merkez_il_id','left')
+                      ->join('ilceler', 'ilceler.ilce_id = merkez_ilce_id','left')
+                      ->order_by($order, $dir)
+                      ->limit($limit, $start)
+                      ->group_by('musteriler.musteri_id')
+                      ->get();
+
+        $data = [];
+        foreach ($query->result() as $row) {
+            $data[] = [
+                $row->musteri_kod,
+                '<a style="font-weight: 500;" href="https://ugbusiness.com.tr/musteri/profil/'.$row->musteri_id.'">'.$row->musteri_ad.'</a>',
+                $row->merkez_adi,
+                $row->ilce_adi."/".$row->sehir_adi,
+                formatTelephoneNumber($row->musteri_iletisim_numarasi), 
+                '<a href="https://ugbusiness.com.tr/cihaz/cihaz_tanimlama_view/'.$row->musteri_id.'" class="btn btn-xs btn-warning">Cihaz Tanımla</a>
+                <a href="https://ugbusiness.com.tr/cihaz/cihaz_tanimlama_view/'.$row->musteri_id.'" class="btn btn-xs btn-dark">Düzenle</a>',
+            ];
+        }
+       
+        $totalData = $this->db->count_all('musteriler');
+        $totalFiltered = $totalData;
+
+        $json_data = [
+            "draw" => intval($this->input->get('draw')),
+            "recordsTotal" => intval($totalData),
+            "recordsFiltered" => intval($totalFiltered),
+            "data" => $data
+        ];
+
+        echo json_encode($json_data);
+    }
+
+
 }
--- application/views/musteri/list/main_content.php
+++ application/views/musteri/list/main_content.php
@@ -2,67 +2,28 @@
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper" style="padding-top:10px">
  
-<section class="content text-md">
+<section class="content text-md" style="    margin-top: -5px;">
 <div class="card card-dark" style="border-radius:0px !important;">
               <div class="card-header" style="background:#00264f!important">
               <h3 class="card-title"><strong>UG Business</strong> - Müşteri Bilgileri</h3>
                 <a href="<?=base_url("musteri/ekle")?>" onclick="waiting('Yeni Müşteri Ekle');" type="button" class="btn btn-primary btn-sm" style="float: right!important;padding: 0px;padding-left: 5px;padding-right: 5px;"><i class="fa fa-plus" style="font-size:12px" aria-hidden="true"></i> Yeni Kayıt Ekle</a>
               </div>
               <!-- /.card-header -->
-              <div class="card-body">
-                <table id="examplemusteriler" class="table table-bordered table-striped nowrap">
-                  <thead>
-                  <tr>
-                    <th style="width: 42px;">ID</th> 
-                  
-                     <th>Müşteri Adı</th>
-                     <th>Merkez Bilgisi</th>
-                     <th>Adres</th>
-                    <th>İletişim Numarası</th>
-                    <th style="width: 130px;">İşlem</th> 
-                  </tr>
-                  </thead>
-                  <tbody>
-
-                    <?php $count=0;  foreach ($musteriler as $musteri) : ?>
-                      <?php $count++?>
-                    <tr>
-                      <td> <span style="opacity:0.5"><?=$musteri->musteri_kod?></span></td> 
-                     
-                      <td>
-                      <a style="   color:#000000;" class="custom-href" href="<?=base_url("musteri/profil/".$musteri->musteri_id)?>">
-                      <b>
-                      <?=$musteri->musteri_ad?></b>
-                     <b class="d-none">
-                      <?=mb_strtoupper(str_replace('ı', 'I', str_replace('i', 'İ', $musteri->musteri_ad)), 'UTF-8')?></b>
-                      <b class="d-none">
-                      <?=mb_strtolower(str_replace('I', 'ı', str_replace('İ', 'i', $musteri->musteri_ad)), 'UTF-8')?></b>
-                    </a>
-                    </td>
-                    <td >
-                      
-                        <?=$musteri->merkez_adi?>
-                       </td>
-                      <td >
-                          <?=$musteri->ilce_adi?> <?=($musteri->sehir_adi != "") ? " / ".$musteri->sehir_adi : ""?>
-                      </td>
-                      <td>
-                        <?=$musteri->musteri_iletisim_numarasi?>
-
-                      </td>
-                     
-                      <td>
-                      <a href="https://ugbusiness.com.tr/cihaz/cihaz_tanimlama_view/<?=$musteri->merkez_id?>" class="text-orange">Cihaz Tanımla</a>
-                     
-                          <a href="<?=site_url("musteri/duzenle/$musteri->musteri_id")?>" onclick="waiting('Müşteri Bilgilerini Düzenle');" type="button"  >Düzenle</a>
-                          
-                      </td>
-                       
-                    </tr>
-                  <?php  endforeach; ?>
-                  </tbody>
+              <div class="card-body" style="    min-height: 790px !important;">
+              <table id="users_table"   class="table table-bordered table-striped nowrap">
+        <thead>
+            <tr>
+                <th style="width:100px">Müşteri ID</th>
+                <th>Müşteri Adı</th>
+                <th>Merkez Bilgisi</th>
+                <th>Adres</th>
+                <th>İletişim Numarası</th>
+                <th style="width:120px">İşlem</th>
               
-                </table>
+                
+            </tr>
+        </thead>
+    </table>
               </div>
               <!-- /.card-body -->
             </div>
@@ -91,15 +52,56 @@
 
 
 
- <style>
-     .table th {
-    background: #ffffff !important;
-    color: #0a0a0a!important;
-    padding: 10px!important;
-    padding-left: 10px !important;
-}
-  .custom-href:hover {
-        text-decoration: underline;
-      }
-   
- </style>
+            <style>
+      
+      
+
+      .custom-href:hover {
+            text-decoration: underline;
+          }
+    
+          .users_table_processing{
+            margin-top:50px;
+          }
+       
+         
+    table.dataTable tbody th, table.dataTable tbody td {
+        padding: 8px 10px  ;
+    }
+     </style>
+    
+    
+    
+    
+    
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#users_table').DataTable({
+                "processing": true,
+                "serverSide": true,
+                "pageLength": 17,
+                "ajax": {
+                    "url": "<?php echo site_url('musteri/musteriler_ajax'); ?>",
+                    "type": "GET"
+                },
+                "language": {
+                        "processing": '<i class="fa fa-spinner fa-spin fa-3x fa-fw"></i>'
+                    },
+                "columns": [
+                    { "data": 0 },
+                    { "data": 1 },
+                    { "data": 2 },
+                    { "data": 3 },
+                    { "data": 4 },
+                    { "data": 5 }
+                ]
+            });
+    
+            $('#users_table').on('click', '.edit-btn', function() {
+                var id = $(this).data('id');
+                window.location.href = "<?php echo site_url('users/edit/'); ?>" + id;
+            });
+    
+    
+        });
+    </script>
