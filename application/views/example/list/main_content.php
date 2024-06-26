6a185740fbc69ade2f4704b50265e47b14b5b8a1
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-06-26 15:10:13 +0300

    0

--- application/views/example/list/main_content.php
+++ application/views/example/list/main_content.php
@@ -94,7 +94,7 @@ table.dataTable tbody th, table.dataTable tbody td {
         $('#users_table').DataTable({
             "processing": true,
             "serverSide": true,
-            "pageLength": 17,
+            "pageLength": 18,
             "ajax": {
                 "url": "<?php echo site_url('exampledata/get_users'); ?>",
                 "type": "GET"
