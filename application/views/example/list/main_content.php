5e0197aaac9fe116b26f73569f95df1e80dea3a6
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-06-26 11:41:10 +0300

    0

--- application/views/example/list/main_content.php
+++ application/views/example/list/main_content.php
@@ -1,8 +1,6 @@
  
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.css">
-    <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.js"></script>
- 
+     <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
 
 <div class="container">
     <h2>Users List</h2>
