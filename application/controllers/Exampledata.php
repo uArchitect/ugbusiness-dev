9a9622ed46d65ef66239ebf2a17e00676f645ac9
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-06-26 11:22:57 +0300

    0

--- application/controllers/Exampledata.php
+++ application/controllers/Exampledata.php
@@ -5,7 +5,6 @@ class Exampledata extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
-        $this->load->database();
     }
 
     public function index() {
