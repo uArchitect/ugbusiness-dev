505abe07c9755c1726af8cee2054a633d9850cfb
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-07-03 16:28:16 +0300

    0

--- application/controllers/Kullanici.php
+++ application/controllers/Kullanici.php
@@ -77,7 +77,7 @@ class Kullanici extends CI_Controller {
 	{  
 
         if($id == 1 && aktif_kullanici()->kullanici_id != 1){
-            $this->session->set_flashdata('flashDanger','Bu kullanının bilgileri düzenlenemez. Sistem yöneticiniz ile iletişime geçiniz.');
+            $this->session->set_flashdata('flashDanger','Bu kullanıcının bilgileri düzenlenemez. Sistem yöneticiniz ile iletişime geçiniz.');
 			redirect(base_url("kullanici"));
         }
          // Kullanıcı Yetki Kontrol
