b35d6d590daaac7ffb6dcd924cccc32d6ecaad20
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-07-04 13:22:07 +0300

    0

--- application/views/cihaz/list/main_content.php
+++ application/views/cihaz/list/main_content.php
@@ -159,7 +159,7 @@ if (pageValue) {
 
         function showWindow($url) {
           var width = 750;
-        var height = 590;
+        var height = 620;
 
         // Pencerenin konumunu hesapla
         var left = (screen.width / 2) - (width / 2);
