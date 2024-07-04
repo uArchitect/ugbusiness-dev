651011aa43351fdd91cb0a50120d2de8b369f0b6
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-07-04 13:13:11 +0300

    0

--- application/views/musteri/updatewindow.php
+++ application/views/musteri/updatewindow.php
@@ -3,35 +3,6 @@
 <head>
     <title>Başarı</title>
     <script type="text/javascript">
-  var isPopup = false;
-
-  function openPopup() {
-    var popup = window.open('', '_blank');
-    if (popup) {
-      isPopup = true;
-      popup.close();
-    }
-  }
-
-  function isPopupWindow() {
-    return isPopup;
-  }
-
-  function closeWindow() {
-    if (isPopupWindow()) {
-      alert("Not a popup");
-      window.close();
-    } else {
-      alert("It is a popup");
-      window.location.href = "<?php echo $redirect_url; ?>";
-    }
-  }
-
-  window.onload = function() {
-    openPopup(); // Bu işlevi yalnızca test amacıyla çalıştırın
-    closeWindow();
-  };
-
 
 
   function isPopup() {
