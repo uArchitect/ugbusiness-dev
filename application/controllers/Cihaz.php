61fc955150279df25fde2dfcb06b76f2665061d3
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-07-04 13:31:30 +0300

    0

--- application/controllers/Cihaz.php
+++ application/controllers/Cihaz.php
@@ -664,7 +664,7 @@ $urlcustom = base_url("siparis/report/").urlencode(base64_encode("Gg3TGGUcv29CpA
              "<span style='font-weight:normal'>". $gbaslangic."<br>".$gbitis."</span>", 
               '
               <a type="button" href="https://ugbusiness.com.tr/cihaz/duzenle/'.$row->siparis_urun_id.'" class="btn btn-outline-primary mt-1" style="font-size: 12px!important;font-weight:normal"><i class="fa fa-pen"></i> Düzenle</a>
-              <a type="button" href="https://ugbusiness.com.tr/cihaz/cihaz_degisim/'.$row->siparis_urun_id.'" class="btn btn-outline-orange mt-1" style="font-size: 12px!important;font-weight:normal"><i class="fas fa-random"></i> Değişim</a>
+              <a type="button" href="https://ugbusiness.com.tr/cihaz/cihaz_degisim/'.$row->siparis_urun_id.'" class="btn btn-outline-warning mt-1" style="font-size: 12px!important;font-weight:normal"><i class="fas fa-random"></i> Değişim</a>
              
               <a type="button" href="https://ugbusiness.com.tr/egitim/add/'.$row->siparis_urun_id.'" class="btn btn-outline-danger mt-1 " style="font-size: 12px!important;font-weight:normal"><i class="fa fa-plus"></i> Eğitim Ekle</a>
               '
