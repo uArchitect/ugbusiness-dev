87b3061638a18d7857651cd35e38d2d1382cd126
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-07-08 16:29:27 +0300

    01

--- application/controllers/Istek.php
+++ application/controllers/Istek.php
@@ -24,10 +24,10 @@ class Istek extends CI_Controller {
 	{  
      
         $check_id = $this->Kullanici_model->get_by_id($this->session->userdata('aktif_kullanici_id')); 
-        if($check_id[0]->kullanici_id == 1 || $check_id[0]->kullanici_id == 7  || $check_id[0]->kullanici_id == 9 || $check_id[0]->kullanici_id == 4 ){
+        if($check_id[0]->kullanici_id == 1){
             $data = $this->Istek_model->get_all(); 
         }else{
-            $data = $this->Istek_model->get_all(["istek_sorumlu_kullanici_id" => $check_id[0]->kullanici_id]); 
+            $data = $this->Istek_model->get_all(["istek_sorumlu_kullanici_id" => $check_id[0]->kullanici_id],["istek_yonetici_id" => $check_id[0]->kullanici_id]); 
         }
 
          
