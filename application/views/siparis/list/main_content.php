b7641021ca47c11e6a6532015df58b41f90e1e5a
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-07-10 18:06:06 +0300

    0

--- application/views/siparis/list/main_content.php
+++ application/views/siparis/list/main_content.php
@@ -32,7 +32,7 @@
     <h3 class="card-title"><strong>UG Business</strong> - Onay Bekleyen Siparişler</h3>
     <?php 
     $i_kul = aktif_kullanici()->kullanici_id;
-    if($i_kul == 1 || $i_kul == 9 || $i_kul == 7){
+    if($i_kul == 1 || $i_kul == 9 || $i_kul == 7 || $i_kul == 37){
 ?>
 <h4 style="font-size:15px" class="card-tools">Tamamlanmayan tüm siparişleri görüntülemek için <a style="color: #0064ff; text-decoration: underline;" href="<?=base_url("siparis/tamamlanmayan_siparisler")?>"> tıklayınız</a></h4>
 
