91e190996f1b26fa9faf97590a76b5e888963e1a
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-07-10 09:42:32 +0300

    01

--- application/views/talep/list_boxed/main_content.php
+++ application/views/talep/list_boxed/main_content.php
@@ -272,7 +272,7 @@ if($count == 0){
                   <thead>
                   <tr>
               
-                    <th style="width:230px">Sonuç</th>   
+                    <th style="width:130px">Sonuç</th>   
                        <th >Müşteri Adı Soyadı</th>
               
                     <th style="width:120px">İletişim Numarası</th>
