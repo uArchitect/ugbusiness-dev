67ac71df2b88cb63ac5d927b616ed7ef283ef525
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-07-10 09:30:23 +0300

    01

--- application/views/siparis/report/main_content.php
+++ application/views/siparis/report/main_content.php
@@ -1450,8 +1450,8 @@ $k_fiyat+=$urun->kapora_fiyati;$p_fiyat+=$urun->pesinat_fiyati;
 
   if($urun->basliklar != null && $urun->basliklar != "" && $urun->basliklar != "null")
   { 
-    $edited_text =mb_strtoupper(str_replace(" 2","",str_replace(" 1","",implode(" BAŞLIK, ", $basliklar))))."BAŞLIK" ;
-    echo "\n".str_replace("SOĞUK HAVA  BAŞLIK, SOĞUK HAVA BAŞLIK","2 ADET SOĞUK HAVA BAŞLIK",$edited_text);
+    $edited_text =mb_strtoupper(str_replace("  "," ",str_replace(" 2","",str_replace(" 1","",implode(" BAŞLIK, ", $basliklar)))))."BAŞLIK" ;
+    echo "\n".str_replace("SOĞUK HAVA BAŞLIK, SOĞUK HAVA BAŞLIK","2 ADET SOĞUK HAVA BAŞLIK",$edited_text);
 
   }
   else{
