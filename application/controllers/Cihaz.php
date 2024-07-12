ef0f90698a639cdd1aa5e8db6da5437c49c4e0fe
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-07-12 14:55:50 +0300

    0

--- application/controllers/Cihaz.php
+++ application/controllers/Cihaz.php
@@ -603,7 +603,7 @@ public function stok_tanim_sil($id)
  
 
 
-		$query = $this->db
+		$query = $this->db->(["siparis_urun_aktif"=>1])
         ->select("musteriler.musteri_kayit_tarihi,kullanicilar.kullanici_ad_soyad,merkezler.merkez_kayit_guncelleme_notu,musteriler.musteri_kayit_guncelleme_notu,musteriler.musteri_ad,borclu_cihazlar.borc_durum as cihaz_borc_uyarisi,musteriler.musteri_id,musteriler.musteri_kod,musteriler.musteri_iletisim_numarasi,
         merkezler.merkez_adi,merkezler.merkez_adresi,merkezler.merkez_yetkili_id,  merkezler.merkez_id,
                   urunler.urun_adi, urunler.urun_slug,siparisler.siparis_kodu,siparisler.siparis_id,
