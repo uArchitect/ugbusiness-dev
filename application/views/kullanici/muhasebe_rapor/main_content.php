41611cce7692c13805b8e32ded43ad404675bba4
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2024-07-17 01:30:32 +0300

    0

--- application/views/kullanici/muhasebe_rapor/main_content.php
+++ application/views/kullanici/muhasebe_rapor/main_content.php
@@ -36,14 +36,8 @@
 window.onload = function () {
 
   CanvasJS.addColorSet("greenShades",
-                [//colorSet Array
-
-                "#8BC1F7",
-                "#519DE9",
-                "#06C",
-                "#004B95",
-                "#002F5D"                
-                ]);
+  ["#115f9a", "#1984c5", "#22a7f0", "#48b5c4", "#76c68f", "#a6d75b", "#c9e52f", "#d0ee11", "#d0f400"]
+);
 
 
   var donutData=[];
