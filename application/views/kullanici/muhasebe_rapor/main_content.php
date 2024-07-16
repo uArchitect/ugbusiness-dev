a2c10151e129115a15f4f0a143de0fbe355340cf
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2024-07-17 00:59:00 +0300

    0

--- application/views/kullanici/muhasebe_rapor/main_content.php
+++ application/views/kullanici/muhasebe_rapor/main_content.php
@@ -28,6 +28,46 @@
                 </div>
               </div>
               <div class="card-body" style="border: 1px solid black;">
+
+
+              <script>
+window.onload = function () {
+
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	title:{
+		text: "Email Categories",
+		horizontalAlign: "left"
+	},
+	data: [{
+		type: "doughnut",
+		startAngle: 60,
+		//innerRadius: 60,
+		indexLabelFontSize: 17,
+		indexLabel: "{label} - #percent%",
+		toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+		dataPoints: [
+			{ y: 67, label: "Inbox" },
+			{ y: 28, label: "Archives" },
+			{ y: 10, label: "Labels" },
+			{ y: 7, label: "Drafts"},
+			{ y: 15, label: "Trash"},
+			{ y: 6, label: "Spam"}
+		]
+	}]
+});
+chart.render();
+
+}
+</script>
+              <div id="chartContainer" style="height: 370px; width: 100%;"></div>
+              <script src="https://cdn.canvasjs.com/canvasjs.min.js"></script>
+
+
+
+
+
+
                 <canvas id="pieChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
               </div>
               <!-- /.card-body -->
