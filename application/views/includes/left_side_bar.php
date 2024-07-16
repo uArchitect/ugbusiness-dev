4bb2a140be629094b3e54c06664b7ec60a0102af
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-07-16 15:27:33 +0300

    0

--- application/views/includes/left_side_bar.php
+++ application/views/includes/left_side_bar.php
@@ -794,7 +794,7 @@
             <a href="#" class="nav-link">
             <i class="nav-icon 	fas fa-users-cog text-success" style="font-size:13px"></i>
               <p style="font-size:15px">
-                Başlık Teknik Servis
+                Teknik Servis
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
