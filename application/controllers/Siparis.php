891515ba40a150bd21919b2b153a1c6e304d2057
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2024-07-21 19:54:04 +0300

    00

--- application/controllers/Siparis.php
+++ application/controllers/Siparis.php
@@ -1134,7 +1134,7 @@ class Siparis extends CI_Controller {
 		   ->join('kullanicilar', 'kullanicilar.kullanici_id = siparisler.siparisi_olusturan_kullanici','left')
 		   ->join(
 			 '(SELECT *, ROW_NUMBER() OVER (PARTITION BY siparis_no ORDER BY onay_tarih DESC) as row_num
-			   FROM siparis_onay_hareketleri) as siparis_onay_hareketleri',
+			   FROM siparis_onay_hareketleri) as siparis_onay_hareketleri ORDER BY `siparis_onay_hareketleri`.`siparis_onay_hareket_id` DESC',
 			 'siparis_onay_hareketleri.siparis_no = siparisler.siparis_id AND siparis_onay_hareketleri.row_num = 1'
 		 )
 		 ->join('siparis_onay_adimlari', 'siparis_onay_adimlari.adim_id = adim_no')
