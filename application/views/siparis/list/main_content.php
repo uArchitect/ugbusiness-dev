bd2a5880189b32e7a246a78ec3fa914b9329dad0
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-07-22 10:43:36 +0300

    0

--- application/views/siparis/list/main_content.php
+++ application/views/siparis/list/main_content.php
@@ -27,7 +27,7 @@
     $i_kul = aktif_kullanici()->kullanici_id;
     if($i_kul == 1 || $i_kul == 9 || $i_kul == 7 ){
 ?>
-<h4 style="font-size:15px" class="card-tools">Tamamlanmayan tüm siparişleri görüntülemek için <a style="color: #0064ff; text-decoration: underline;" href="<?=base_url("siparis/tamamlanmayan_view")?>"> tıklayınız</a></h4>
+<h4 style="font-size:15px" class="card-tools">Tamamlanmayan tüm siparişleri görüntülemek için <a style="color: #0064ff; text-decoration: underline;" href="<?=base_url("siparis/tamamlanmayanlar_view")?>"> tıklayınız</a></h4>
 
 <?php
     }
