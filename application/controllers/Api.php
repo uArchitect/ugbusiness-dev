778d17e5e395f4d8bcff54ec2725bbe2a03f5923
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-07-23 14:48:41 +0300

    0

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -10,34 +10,48 @@ class Api extends CI_Controller {
 
 	
 
-	public function index()
+	public function index($apikey = "")
 	{
-		 
+		$json_data = [
+			"userName" => "error",
+			"userTitle" => "error", 
+			"data" => null
+		];
+
+		if($apikey != "" && $apikey != null){
 			
-		$query = $this->db
-		->select('istekler.*, kullanicilar.kullanici_ad_soyad as kullanici_ad_soyad, yonetici_kullanicilar.kullanici_ad_soyad as gorevlendirilen_kullanici_ad_soyad')
-		->join('kullanicilar', 'kullanicilar.kullanici_id = istekler.istek_sorumlu_kullanici_id', 'left')
-		->join('kullanicilar as yonetici_kullanicilar', 'yonetici_kullanicilar.kullanici_id = istekler.istek_yonetici_id', 'left')
-		->from('istekler')
-		->get();
-						 
-					
-	
-						  
-	
+			$kquery = $this->db->where("kullanici_api_pc_key",$apikey)
+			->select('kullanicilar.*')->from('kullanicilar')
+			->get();
+			if(count($kquery)>=0){
+				$query = $this->db
+				->where("istek_sorumlu_kullanici_id",$kquery[0]->kullanici_id)
+				->or_where("istek_yonetici_id",$kquery[0]->kullanici_id)
+				->select('istekler.*, kullanicilar.kullanici_ad_soyad as kullanici_ad_soyad, yonetici_kullanicilar.kullanici_ad_soyad as gorevlendirilen_kullanici_ad_soyad')
+				->join('kullanicilar', 'kullanicilar.kullanici_id = istekler.istek_sorumlu_kullanici_id', 'left')
+				->join('kullanicilar as yonetici_kullanicilar', 'yonetici_kullanicilar.kullanici_id = istekler.istek_yonetici_id', 'left')
+				->from('istekler')
+				->get();
+				if(count($query)>=0){
+					$json_data = [
+						"userName" => $kquery[0]->kullanici_ad_soyad,
+						"userTitle" => $kquery[0]->kullanici_unvan, 
+						"data" => $query->result()
+					];
 			
-		   
-			$totalData = $this->db->count_all('siparisler');
-			$totalFiltered = $totalData;
-	
-			$json_data = [
-				"draw" => intval($this->input->get('draw')),
-				"recordsTotal" => intval($totalData),
-				"recordsFiltered" => intval($totalFiltered),
-				"data" => $query->result()
-			];
-	
-			echo json_encode($json_data);
+					echo json_encode($json_data);
+				}else{
+					$json_data = [
+						"userName" => $kquery[0]->kullanici_ad_soyad,
+						"userTitle" => $kquery[0]->kullanici_unvan, 
+						"data" => null
+					];
+			
+					echo json_encode($json_data);
+				}
+			}
+		}
+		echo json_encode($json_data);		
 		}
 	
 	}
