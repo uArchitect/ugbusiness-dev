e3c0ef9b76060f65d440c081f671f631cad8fb01
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-07-26 13:05:24 +0300

    0

--- application/views/istek/list/main_content.php
+++ application/views/istek/list/main_content.php
@@ -232,7 +232,7 @@
                         <br>
                         <?=date('d.m.Y H:i',strtotime($istek->istek_kayit_tarihi));?></td>
                     
-                      <td class="bg-dark" style="align-items-center;">
+                      <td class="bg-dark" style="vertical-align: middle;">
                         <i class="fa fa-user-circle" style="margin-right:5px;opacity:0.8"></i> 
                         <b><?=$istek->gonderilen_kullanici?></b>
                         <br>
