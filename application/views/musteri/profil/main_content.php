4c285c01895a18e98029422d6881c844e4f867f0
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-07-29 15:15:57 +0300

    0

--- application/views/musteri/profil/main_content.php
+++ application/views/musteri/profil/main_content.php
@@ -195,7 +195,7 @@
 
                             </div>
                             <div class="timeline-footer" style="      padding-top: 0;  padding-left: 16px;">
-                              <a href="<?=base_url("merkez/duzenle/".$merkez->merkez_id)?>" class="btn btn-warning btn-sm text-dark"><i class="far fa-eye"></i> Merkez Bilgilerini Düzenle</a>
+                              <a style="background: #042657; color: white !important; border: 1px solid #042657;" href="<?=base_url("merkez/duzenle/".$merkez->merkez_id)?>" class="btn btn-warning btn-sm text-dark"><i class="far fa-eye"></i> Merkez Bilgilerini Düzenle</a>
                               <a href="<?=base_url("cihaz/cihaz_tanimlama_view/".$merkez->merkez_id)?>" class="btn btn-primary btn-sm"><i class="fas fa-plus-circle nav-icon"></i> Cihaz Tanımla</a>
                             </div>
                           </div>
