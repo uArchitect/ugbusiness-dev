a5ac975008e5e0dd469b32449b717656a6483c57
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-07-29 14:26:00 +0300

    0

--- application/views/musteri/profil/main_content.php
+++ application/views/musteri/profil/main_content.php
@@ -144,7 +144,7 @@
                               <?php foreach ($urunler as $urun) : ?>
                                 <?php if($urun->merkez_id != $merkez->merkez_id){continue;} ?>
                                 <div class="btn-group mb-2" style="display: flow;">
-                                <button onclick="showcihaz(<?=$urun->siparis_urun_id?>);" type="button" class="btn btn-default text-left">   
+                                <button onclick="if (event.target.tagName.toLowerCase() === 'a') { event.stopPropagation(); } else{ showcihaz(<?=$urun->siparis_urun_id?>); }" type="button" class="btn btn-default text-left">   
                                  <span style="min-width: 230px; width: 230px; display: inline-block;"> <b><?=$urun->urun_adi?></b> /  <?=$urun->seri_numarasi != "" ? $urun->seri_numarasi : "<span class='text-danger'>Seri No Atanmadı</span>"?> </span> <b>Garanti Başlangıç :</b><?=date("d.m.Y",strtotime($urun->garanti_baslangic_tarihi))?> <b>Garanti Bitiş :</b><?=date("d.m.Y",strtotime($urun->garanti_bitis_tarihi))?>
                             <br>
                             <?php 
