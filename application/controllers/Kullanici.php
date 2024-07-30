0232193b0fd25c11101d286824b86311e52c7bd4
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-07-30 16:04:27 +0300

    0

--- application/controllers/Kullanici.php
+++ application/controllers/Kullanici.php
@@ -389,7 +389,7 @@ $query = $this->db->query($sql);
             INNER JOIN urunler ON urunler.urun_id = siparis_urunleri.urun_no
             INNER JOIN kullanicilar ON kullanicilar.kullanici_id = siparisler.siparisi_olusturan_kullanici
         WHERE 
-            kullanicilar.kullanici_departman_id = 12 
+          (kullanicilar.kullanici_departman_id = 12 or kullanicilar.kullanici_departman_id = 17 or kullanicilar.kullanici_departman_id = 18 or kullanicilar.kullanici_id = 2 or kullanicilar.kullanici_id = 9)
             AND siparisler.siparis_aktif = 1 AND siparis_urunleri.odeme_secenek = 2
         GROUP BY 
             kullanicilar.kullanici_ad_soyad";
@@ -417,7 +417,7 @@ $query = $this->db->query($sql);
             INNER JOIN urunler ON urunler.urun_id = siparis_urunleri.urun_no
             INNER JOIN kullanicilar ON kullanicilar.kullanici_id = siparisler.siparisi_olusturan_kullanici
         WHERE 
-            kullanicilar.kullanici_departman_id = 12 
+           (kullanicilar.kullanici_departman_id = 12 or kullanicilar.kullanici_departman_id = 17 or kullanicilar.kullanici_departman_id = 18 or kullanicilar.kullanici_id = 2 or kullanicilar.kullanici_id = 9)
             AND siparisler.siparis_aktif = 1  AND siparis_urunleri.odeme_secenek = 1
         GROUP BY 
             kullanicilar.kullanici_ad_soyad";
@@ -453,7 +453,7 @@ $query = $this->db->query($sql);
                  INNER JOIN siparisler ON siparis_urunleri.siparis_kodu = siparisler.siparis_id
                  INNER JOIN kullanicilar ON kullanicilar.kullanici_id = siparisler.siparisi_olusturan_kullanici
              WHERE 
-                 kullanicilar.kullanici_departman_id = 12 
+                 (kullanicilar.kullanici_departman_id = 12 or kullanicilar.kullanici_departman_id = 17 or kullanicilar.kullanici_departman_id = 18 or kullanicilar.kullanici_id = 2 or kullanicilar.kullanici_id = 9)
                  AND siparisler.siparis_aktif = 1 
              GROUP BY 
                 
@@ -491,7 +491,7 @@ $query = $this->db->query($sql);
                     INNER JOIN siparisler ON siparis_urunleri.siparis_kodu = siparisler.siparis_id
                     INNER JOIN kullanicilar ON kullanicilar.kullanici_id = siparisler.siparisi_olusturan_kullanici
                 WHERE
-                    kullanicilar.kullanici_departman_id = 12
+                    (kullanicilar.kullanici_departman_id = 12 or kullanicilar.kullanici_departman_id = 17 or kullanicilar.kullanici_departman_id = 18 or kullanicilar.kullanici_id = 2 or kullanicilar.kullanici_id = 9)
                     AND siparisler.siparis_aktif = 1
                 GROUP BY
                     siparis_urunleri.urun_no
