f0e48a01c40695c09af4f9306d6c18b7289b8b9d
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-07-30 18:44:31 +0300

    0

--- application/views/talep/form/main_content.php
+++ application/views/talep/form/main_content.php
@@ -327,7 +327,7 @@
  <div class="form-group" id="gorusme_detay">
         <label for="formClient-Code" style="font-weight:normal"> Görüşmeyi Detayları </label>
         <label for="formClient-Name" style="font-weight:normal;  opacity:0.5; ">(*Zorunlu)</label>
-        <textarea name="gorusme_detay" required><?php if(!empty($talep_yonlendirme)){echo $talep_yonlendirme->gorusme_detay;}?></textarea>
+        <textarea name="gorusme_detay" minlength = "20" required><?php if(!empty($talep_yonlendirme)){echo $talep_yonlendirme->gorusme_detay;}?></textarea>
       </div>
 
  
