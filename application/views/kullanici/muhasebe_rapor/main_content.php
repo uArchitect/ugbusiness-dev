a56c8de3f8d876b916a855bbb9cec418ea990040
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2024-07-31 22:25:45 +0300

    0

--- application/views/kullanici/muhasebe_rapor/main_content.php
+++ application/views/kullanici/muhasebe_rapor/main_content.php
@@ -150,6 +150,35 @@ chart2.render();
 
 
 
+ 
+var chart3a = = new CanvasJS.Chart("chartContaineraa", {
+	animationEnabled: true,
+	theme: "light2", // "light1", "light2", "dark1", "dark2"
+	title:{
+		text: "Top Oil Reserves"
+	},
+	axisY: {
+		title: "Reserves(MMbbl)"
+	},
+	data: [{        
+		type: "column",  
+		showInLegend: true, 
+		legendMarkerColor: "grey",
+		legendText: "MMbbl = one million barrels",
+		dataPoints: [      
+			{ y: 300878, label: "Venezuela" },
+			{ y: 266455,  label: "Saudi" },
+			{ y: 169709,  label: "Canada" },
+			{ y: 158400,  label: "Iran" },
+			{ y: 142503,  label: "Iraq" },
+			{ y: 101500, label: "Kuwait" },
+			{ y: 97800,  label: "UAE" },
+			{ y: 80000,  label: "Russia" }
+		]
+	}]
+});
+chart3a.render();
+
 
 }
 </script>
@@ -158,6 +187,12 @@ chart2.render();
 
  
               </div>
+
+              <div id="chartContaineraa" style="height: 260px; width: 100%;"></div>
+           
+
+ 
+           </div>
               <!-- /.card-body -->
             </div>
             <!-- /.card -->
