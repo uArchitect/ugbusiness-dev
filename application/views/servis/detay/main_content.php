362bae02d86ec03539432f8ff4c82c522500607e
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2024-08-01 20:00:29 +0300

    d

--- application/views/servis/detay/main_content.php
+++ application/views/servis/detay/main_content.php
@@ -1070,7 +1070,7 @@ if($servis->servis_durum_tanim_id == 2){
   function showWindow($url) {
         
         var width = 750;
-      var height = 620;
+      var height = 660;
 
      
       var left = (screen.width / 2) - (width / 2);
