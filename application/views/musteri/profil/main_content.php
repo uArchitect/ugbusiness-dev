cc4cb29dfec0811aadab13fd3bee0cbaace66563
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2024-08-01 13:21:26 +0300

    s

--- application/views/musteri/profil/main_content.php
+++ application/views/musteri/profil/main_content.php
@@ -144,7 +144,7 @@ body {
             <!---whatsap--->
 
             <div class="card card-dark mb-2 m-1 mr-2">
-              <div class="card-header p-0 pl-2 pt-2 pb-2">
+              <div class="card-header  ">
                 <h3 class="card-title" style="font-size:15px">WHATSAPP MESAJ GÖNDER</h3>
               </div>
               <!-- /.card-header -->
@@ -171,7 +171,7 @@ body {
                <!---whatsap--->
 
                <div class="card card-dark mb-2 m-1 mr-2">
-              <div class="card-header p-0 pl-2 pt-2 pb-2">
+              <div class="card-header  ">
                 <h3 class="card-title" style="font-size:15px">SMS GÖNDER</h3>
               </div>
               <!-- /.card-header -->
