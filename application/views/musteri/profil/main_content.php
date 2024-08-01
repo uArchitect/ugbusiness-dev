843f57195614f05bab3544c964a2562a4a42934f
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2024-08-01 12:54:18 +0300

    1

--- application/views/musteri/profil/main_content.php
+++ application/views/musteri/profil/main_content.php
@@ -162,7 +162,7 @@ body {
                <!---whatsap--->
 
                <div class="card card-primary mb-2 m-1 mr-2">
-              <div class="card-header p-0 pl-3">
+              <div class="card-header p-0 pl-2 pt-3 pb-3">
                 <h3 class="card-title" style="font-size:15px">SMS GÖNDER</h3>
               </div>
               <!-- /.card-header -->
