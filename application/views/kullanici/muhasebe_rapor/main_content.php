577727e577bb59f845149957dfb937b6b1e1a868
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2024-08-01 11:52:18 +0300

    1

--- application/views/kullanici/muhasebe_rapor/main_content.php
+++ application/views/kullanici/muhasebe_rapor/main_content.php
@@ -451,7 +451,8 @@ chart3a.render();
                     setlocale(LC_MONETARY, 'tr_TR');
  
                     ?>
-                     <tr>   
+                     <tr style="    background: red;
+    color: white;">   
                     <td style="    text-align: end;font-weight:bold" colspan="5">TOPLAM : </td>
                     <td style="font-weight:bold"><?=money_format('%i', $t_satis_fiyati)?></td>
                     <td style="font-weight:bold"><?=money_format('%i', $t_kapora)?></td>
