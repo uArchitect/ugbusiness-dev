bbf693a3471f0300094f06f48e319394317d68a4
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2024-08-01 17:43:17 +0300

    0

--- application/views/kullanici/muhasebe_rapor/main_content.php
+++ application/views/kullanici/muhasebe_rapor/main_content.php
@@ -351,6 +351,20 @@ chart3a.render();
                     $t_takas_bedeli = 0;
                     $t_taksit = 0;
                     $t_fatura = 0;
+
+                    $vadeli_t_satis_fiyati = 0;
+                    $vadeli_t_kapora = 0;
+                    $vadeli_t_pesinat = 0;
+                    $vadeli_t_takas_bedeli = 0;
+                    $vadeli_t_taksit = 0;
+                    $vadeli_t_fatura = 0;
+
+                    $pesin_t_satis_fiyati = 0;
+                    $pesin_t_kapora = 0;
+                    $pesin_t_pesinat = 0;
+                    $pesin_t_takas_bedeli = 0;
+                    $pesin_t_taksit = 0;
+                    $pesin_t_fatura = 0;
                     ?>
                    <?php foreach ($kullanicilar as $kullanici){?>
                     <?php 
@@ -361,6 +375,23 @@ chart3a.render();
                     $t_takas_bedeli += $kullanici->takas_bedeli;
                     $t_fatura += $kullanici->fatura_tutari;
                    
+                    if($kullanici->odeme_secenek == "1"){
+                      $pesin_t_satis_fiyati += $kullanici->satis_fiyati;
+                      $pesin_t_kapora += $kullanici->kapora_fiyati;
+                      $pesin_t_pesinat += $kullanici->pesinat_fiyati;
+                      $pesin_t_takas_bedeli += $kullanici->takas_bedeli;
+                      $pesin_t_fatura += $kullanici->fatura_tutari;
+                  
+                    }else{
+                      $vadeli_t_satis_fiyati += $kullanici->satis_fiyati;
+                      $vadeli_t_kapora += $kullanici->kapora_fiyati;
+                      $vadeli_t_pesinat += $kullanici->pesinat_fiyati;
+                      $vadeli_t_takas_bedeli += $kullanici->takas_bedeli;
+                      $vadeli_t_fatura += $kullanici->fatura_tutari;
+                    }
+                  
+
+                  
                     
                     ?>
                     <tr>
@@ -438,7 +469,14 @@ chart3a.render();
                             echo " (".(($f_kontrol ? number_format($kalan_tutar ,2)." ₺" : "<span class='text-danger'>**.***</span>"));
                             echo "<span style='opacity:0.6'> - Taksit :".($f_kontrol ? number_format($kalan_tutar/$kullanici->vade_sayisi)." ₺</span>)" : "<span class='text-danger'>**.***</span>)");
                           $t_taksit += ($kalan_tutar/$kullanici->vade_sayisi);
+                          if($kullanici->odeme_secenek == "1"){
+                            $pesin_t_taksit += ($kalan_tutar/$kullanici->vade_sayisi);
                         
+                          }else{
+                            $vadeli_t_taksit += ($kalan_tutar/$kullanici->vade_sayisi);
+                          }
+                         
+                         
                         }
                         
                         ?>
@@ -453,16 +491,35 @@ chart3a.render();
                     setlocale(LC_MONETARY, 'tr_TR');
  
                     ?>
-                     <tr style="    background: #7d0000;
-    color: white;">   
-                    <td style="    text-align: end;font-weight:bold" colspan="5">TOPLAM : </td>
-                    <td style="font-weight:bold"><?=money_format('%i', $t_satis_fiyati)?></td>
-                    <td style="font-weight:bold"><?=money_format('%i', $t_kapora)?></td>
-                    <td style="font-weight:bold"><?=money_format('%i', $t_pesinat)?></td>
-                    <td style="font-weight:bold"><?=money_format('%i', $t_takas_bedeli)?></td>
-                    <td style="font-weight:bold"><?=money_format('%i', $t_fatura)?></td>
-                    <td style="font-weight:bold">-</td>
-                    <td style="font-weight:bold"><?=money_format('%i', $t_taksit)?></td>
+                      <tr style="background: #7d0000;color: white;">   
+                        <td style="text-align: end;font-weight:bold" colspan="5">VADELİ TOPLAM : </td>
+                        <td style="font-weight:bold"><?=money_format('%i', $vadeli_t_satis_fiyati)?></td>
+                        <td style="font-weight:bold"><?=money_format('%i', $vadeli_t_kapora)?></td>
+                        <td style="font-weight:bold"><?=money_format('%i', $vadeli_t_pesinat)?></td>
+                        <td style="font-weight:bold"><?=money_format('%i', $vadeli_t_takas_bedeli)?></td>
+                        <td style="font-weight:bold"><?=money_format('%i', $vadeli_t_fatura)?></td>
+                        <td style="font-weight:bold">-</td>
+                        <td style="font-weight:bold"><?=money_format('%i', $vadeli_t_taksit)?></td>
+                      </tr>
+                      <tr style="background: #7d0000;color: white;">   
+                        <td style="text-align: end;font-weight:bold" colspan="5">PEŞİN TOPLAM : </td>
+                        <td style="font-weight:bold"><?=money_format('%i', $pesin_t_satis_fiyati)?></td>
+                        <td style="font-weight:bold"><?=money_format('%i', $pesin_t_kapora)?></td>
+                        <td style="font-weight:bold"><?=money_format('%i', $pesin_t_pesinat)?></td>
+                        <td style="font-weight:bold"><?=money_format('%i', $pesin_t_takas_bedeli)?></td>
+                        <td style="font-weight:bold"><?=money_format('%i', $pesin_t_fatura)?></td>
+                        <td style="font-weight:bold">-</td>
+                        <td style="font-weight:bold"><?=money_format('%i', $pesin_t_taksit)?></td>
+                      </tr>
+                      <tr style="background: #7d0000;color: white;">   
+                        <td style="text-align: end;font-weight:bold" colspan="5">GENEL TOPLAM : </td>
+                        <td style="font-weight:bold"><?=money_format('%i', $t_satis_fiyati)?></td>
+                        <td style="font-weight:bold"><?=money_format('%i', $t_kapora)?></td>
+                        <td style="font-weight:bold"><?=money_format('%i', $t_pesinat)?></td>
+                        <td style="font-weight:bold"><?=money_format('%i', $t_takas_bedeli)?></td>
+                        <td style="font-weight:bold"><?=money_format('%i', $t_fatura)?></td>
+                        <td style="font-weight:bold">-</td>
+                        <td style="font-weight:bold"><?=money_format('%i', $t_taksit)?></td>
                       </tr>
                     <?php
                   }
