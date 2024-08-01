d89df6788ba9d9c755229866b8b985a41247214e
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2024-08-01 13:43:48 +0300

    s

--- application/views/musteri/profil/main_content.php
+++ application/views/musteri/profil/main_content.php
@@ -895,6 +895,12 @@ foreach ($kursiyerler as $key => $kursiyer) {
       var left = (screen.width / 2) - (width / 2);
       var top = (screen.height / 2) - (height / 2);
       var newWindow = window.open($url, 'Yeni Pencere', 'width=' + width + ',height=' + height + ',top=' + top + ',left=' + left);
-      location.reload();
+      var interval = setInterval(function() {
+            if (newWindow.closed) {
+                clearInterval(interval);
+                location.reload();
+              
+            }
+        }, 1000); 
   };
     </script>
