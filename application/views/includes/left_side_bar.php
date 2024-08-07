1f1b838fff6643781db95454ffdecdb50c29a770
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-08-07 13:20:40 +0300

    a

--- application/views/includes/left_side_bar.php
+++ application/views/includes/left_side_bar.php
@@ -353,7 +353,7 @@
 {
 ?>
 <li class="nav-item">
-                    <a href="<?=base_url("musteri")?>"  style="border-left: 0;" class="nav-link">
+                    <a href="<?=base_url("musteri")?>"  style="border-left: 0;" >
                     <i class="nav-icon fas fa-users text-primary" style="font-size:13px"></i>
                       <p style="font-size:15px">TÜM MÜŞTERİLERİ GÖRÜNTÜLE</p>
                     </a>
