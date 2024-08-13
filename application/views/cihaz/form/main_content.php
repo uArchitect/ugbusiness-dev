57d16b7f61200dd85105e129497c0be34f66626b
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-08-13 13:40:47 +0300

    1

--- application/views/cihaz/form/main_content.php
+++ application/views/cihaz/form/main_content.php
@@ -400,14 +400,14 @@ if(count($basliklar)<=0){
                 <div class="row">
                 <form action="<?=base_url("cihaz/urun_takas_guncelle/$urun->siparis_urun_id")?>" method="POST">
               
-                  <label for="formClient-Name">Cihaz Takas Olarak Alındı Mı ?</label>
+                  <label for="formClient-Name">Cihaz Takas Olarak Alındı Mı ?</label><br>
                   <select name="takas_cihaz_mi" class="select2 form-control ">
                     <option value="1" <?=$urun->takas_cihaz_mi == 1 ? "selected" : "" ?>> EVET</option>
                     <option value="0" <?=$urun->takas_cihaz_mi == 0 ? "selected" : "" ?>> HAYIR</option>
-                  </select>  
+                  </select>  <br>
                   <label for="formClient-Name">
                     Takas Alınan Merkez
-                  </label>
+                  </label><br>
                   <select name="takas_alinan_merkez_id" id="takas_alinan_merkez_id" class="select2">
                     <?php foreach($mymusteriler as $mymusteri) : ?> 
                       <option value="<?=$mymusteri->merkez_id?>" <?= $mymusteri->merkez_id == $urun->takas_alinan_merkez_id ? 'selected' : '' ?>><?=$mymusteri->musteri_ad?>(<?=$mymusteri->merkez_adi?>) <?=$mymusteri->ilce_adi?> / <?=$mymusteri->sehir_adi?> / <?=$mymusteri->musteri_iletisim_numarasi?></option>
