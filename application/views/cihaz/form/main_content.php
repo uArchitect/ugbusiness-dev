49642689a36effec6c939287ffdea6160d57e1b1
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-08-13 13:51:32 +0300

    a

--- application/views/cihaz/form/main_content.php
+++ application/views/cihaz/form/main_content.php
@@ -710,34 +710,7 @@ foreach ($kursiyerler as $key => $kursiyer) {
 
 
 
-                  <div class="row" style="    display: block;">
-                <form action="<?=base_url("cihaz/urun_takas_guncelle/$urun->siparis_urun_id")?>" method="POST">
-              
-                  <label>Cihaz Takas Olarak Alındı Mı ?</label><br>
-                  <select name="takas_cihaz_mi" class="select2 form-control ">
-                    <option value="1" <?=$urun->takas_cihaz_mi == 1 ? "selected" : "" ?>> EVET</option>
-                    <option value="0" <?=$urun->takas_cihaz_mi == 0 ? "selected" : "" ?>> HAYIR</option>
-                  </select> 
                   
-                  <br>
-                  <label>
-                    Takas Alınan Merkez
-                  </label><br>
-                  <select name="takas_alinan_merkez_id" id="takas_alinan_merkez_id" class="select2">
-                    <?php foreach($mymusteriler as $mymusteri) : ?> 
-                      <option value="<?=$mymusteri->merkez_id?>" <?= $mymusteri->merkez_id == $urun->takas_alinan_merkez_id ? 'selected' : '' ?>><?=$mymusteri->musteri_ad?>(<?=$mymusteri->merkez_adi?>) <?=$mymusteri->ilce_adi?> / <?=$mymusteri->sehir_adi?> / <?=$mymusteri->musteri_iletisim_numarasi?></option>
-                  
-                      <?php endforeach; ?> 
-                  </select><br>
-                  <button type="submit" class="btn  btn-success">
-                    <i class="ion ion-checkmark-circled"></i>
-                   Bilgileri Güncelle
-                  </button>
-
-                  </form>
-                </div>
-
-                
 
                   </div>
                   </div>
@@ -776,6 +749,45 @@ foreach ($kursiyerler as $key => $kursiyer) {
 
          </div>
 </div>
+
+
+
+
+
+<div class="row" style="    display: block;">
+                <form action="<?=base_url("cihaz/urun_takas_guncelle/$urun->siparis_urun_id")?>" method="POST">
+              
+                  <label>Cihaz Takas Olarak Alındı Mı ?</label><br>
+                  <select name="takas_cihaz_mi" class="select2 form-control ">
+                    <option value="1" <?=$urun->takas_cihaz_mi == 1 ? "selected" : "" ?>> EVET</option>
+                    <option value="0" <?=$urun->takas_cihaz_mi == 0 ? "selected" : "" ?>> HAYIR</option>
+                  </select> 
+                  
+                  <br>
+                  <label>
+                    Takas Alınan Merkez
+                  </label><br>
+                  <select name="takas_alinan_merkez_id" id="takas_alinan_merkez_id" class="select2">
+                    <?php foreach($mymusteriler as $mymusteri) : ?> 
+                      <option value="<?=$mymusteri->merkez_id?>" <?= $mymusteri->merkez_id == $urun->takas_alinan_merkez_id ? 'selected' : '' ?>><?=$mymusteri->musteri_ad?>(<?=$mymusteri->merkez_adi?>) <?=$mymusteri->ilce_adi?> / <?=$mymusteri->sehir_adi?> / <?=$mymusteri->musteri_iletisim_numarasi?></option>
+                  
+                      <?php endforeach; ?> 
+                  </select><br>
+                  <button type="submit" class="btn  btn-success">
+                    <i class="ion ion-checkmark-circled"></i>
+                   Bilgileri Güncelle
+                  </button>
+
+                  </form>
+                </div>
+
+
+
+
+
+
+
+
 
 
            
