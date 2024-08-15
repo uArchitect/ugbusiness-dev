27e4c64675d48a455917cc8c61ae5ffdcadbebf7
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-08-15 09:57:30 +0300

    2

--- application/controllers/Siparis.php
+++ application/controllers/Siparis.php
@@ -1265,9 +1265,8 @@ class Siparis extends CI_Controller {
 				$row->kullanici_ad_soyad,
 			
 				'
-				<a type="button" href="'.$urlcustom.'"    class="btn btn-warning btn-xs"><i class="fa fa-pen" style="font-size:12px" aria-hidden="true"></i> Düzenle</a>
-				<a type="button" onclick="showdetail(\''.$urlcustom.'/1\');"    class="btn btn-dark btn-xs"><i class="fa fa-search" style="font-size:12px" aria-hidden="true"></i> Görüntüle</a>
-				 '
+				<a type="button" onclick="showWindow(\''.$urlcustom.'\');"     class="btn btn-warning btn-xs"><i class="fa fa-pen" style="font-size:12px" aria-hidden="true"></i> Düzenle</a>
+				  '
 			
 			  
 			];
