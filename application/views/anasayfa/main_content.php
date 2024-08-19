1586d23187bcd6cb95a457996fafafa9842c3353
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-08-19 11:56:55 +0300

    1

--- application/views/anasayfa/main_content.php
+++ application/views/anasayfa/main_content.php
@@ -52,7 +52,7 @@
 <div class="widget-user-header bg-dark" style="background:#181818 !important;">
 <div class="widget-user-image">
 <img style="BACKGROUND: #001cab;
-    border: 2px solid;object-fit: cover;width:65px;height:65px" class="img-circle elevation-2" src="<?=aktif_kullanici()->kullanici_resim ? base_url("uploads/$aktif_kullanici->kullanici_resim") : base_url("uploads/default.png")?>" alt="User Avatar">
+    border: 2px solid white !important;object-fit: cover;width:65px;height:65px" class="img-circle elevation-2" src="<?=aktif_kullanici()->kullanici_resim ? base_url("uploads/$aktif_kullanici->kullanici_resim") : base_url("uploads/default.png")?>" alt="User Avatar">
 </div>
 
 <h3 class="widget-user-username"><?=$aktif_kullanici->kullanici_ad_soyad?> / <?=$aktif_kullanici->kullanici_unvan?></h3>
