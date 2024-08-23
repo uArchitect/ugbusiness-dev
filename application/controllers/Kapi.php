44d9b258e1e82b6e8459770f6e8a5e186106e8a8
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-08-23 16:42:26 +0300

    1

--- application/controllers/Kapi.php
+++ application/controllers/Kapi.php
@@ -30,16 +30,7 @@ class Kapi extends CI_Controller {
 		$viewData["page"] = "kapi/list";
 		redirect(base_url("kapi"));
 	}
-	public function index()
-	{
-        yetki_kontrol("kapi_yonetim");
-        //sendSmsData("05382197344","Kapı Giriş : ".aktif_kullanici()->kullanici_ad_soyad." - Tarih : ".date("d.m.Y H:i"));
-		$viewData["kullanicilar_aktif"] = $this->db->where("kullanici_aktif",1)->where("kapi1_giris",1)->get("kullanicilar")->result();
-		$viewData["kullanicilar_pasif"] = $this->db->where("kullanici_aktif",1)->where("kapi1_giris",0)->get("kullanicilar")->result();
-		
-		$viewData["page"] = "kapi/list";
-		$this->load->view('base_view',$viewData);
-	}
+	
 
 
 
