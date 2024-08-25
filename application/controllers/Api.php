f0ca5c7fd596032e38ee5844b3e4c365d2a9020f
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2024-08-25 19:53:26 +0300

    1

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -12,7 +12,7 @@ class Api extends CI_Controller {
 
 	public function door_control($user_id)
 	{
-		echo json_encode($this->db->where("kullanici",$user_id)->select("kullanicilar")->get()->result());
+		echo "sdfsdf";
 	}
 
 
