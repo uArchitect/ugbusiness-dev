f057c1a2a7a0773683a4b9e95d5b3a6537cae727
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-08-26 18:01:11 +0300

    1

--- application/views/stok/stok_kayit/main_content.php
+++ application/views/stok/stok_kayit/main_content.php
@@ -19,22 +19,7 @@
 <label for="exampleInputPassword1">Tanımlı Olduğu Stok</label>
 <input type="text" disabled class="form-control <?=($ust_data != null ? "" : "text-danger")?>" id="exampleInputPassword1" value="<?=($ust_data != null ? $ust_data->stok_tanim_ad." - ".$ust_data->stok_seri_kod : "Herhangi bir stoğa tanımlı değil")?>">
 </div>
-<div class="form-group">
-<label for="exampleInputFile">File input</label>
-<div class="input-group">
-<div class="custom-file">
-<input type="file" class="custom-file-input" id="exampleInputFile">
-<label class="custom-file-label" for="exampleInputFile">Choose file</label>
-</div>
-<div class="input-group-append">
-<span class="input-group-text">Upload</span>
-</div>
-</div>
-</div>
-<div class="form-check">
-<input type="checkbox" class="form-check-input" id="exampleCheck1">
-<label class="form-check-label" for="exampleCheck1">Check me out</label>
-</div>
+ 
 </div>
 
 <div class="card-footer">
