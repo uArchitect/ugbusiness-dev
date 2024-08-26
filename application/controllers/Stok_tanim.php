110b78b9db66fcbac836f87059a655ea1fac62aa
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-08-26 16:41:50 +0300

    1

---
+++ application/controllers/Stok_tanim.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Stok_tanim extends CI_Controller {
+	function __construct(){
+        parent::__construct();
+        session_control();
+		$this->load->model('Stok_model');
+        date_default_timezone_set('Europe/Istanbul');
+       
+    }
+	public function index($stok_id)
+	{   
+        $stok = $this->Stok_model->stok_kayitlari_all(["stok_id"=>$stok_id]);
+		redirect(base_url("stok/stok_kayit"));
+	}
+}
---
+++ application/views/stok/stok_kayit/main_content.php
@@ -0,0 +1,48 @@
+ 
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+<section class="content col-md-4">
+
+<div class="card card-primary">
+<div class="card-header">
+<h3 class="card-title">Stok Bilgilerini Düzenle</h3>
+</div>
+
+
+<form action="stok">
+<div class="card-body">
+<div class="form-group">
+<label for="exampleInputEmail1">Email address</label>
+<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+</div>
+<div class="form-group">
+<label for="exampleInputPassword1">Password</label>
+<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+</div>
+<div class="form-group">
+<label for="exampleInputFile">File input</label>
+<div class="input-group">
+<div class="custom-file">
+<input type="file" class="custom-file-input" id="exampleInputFile">
+<label class="custom-file-label" for="exampleInputFile">Choose file</label>
+</div>
+<div class="input-group-append">
+<span class="input-group-text">Upload</span>
+</div>
+</div>
+</div>
+<div class="form-check">
+<input type="checkbox" class="form-check-input" id="exampleCheck1">
+<label class="form-check-label" for="exampleCheck1">Check me out</label>
+</div>
+</div>
+
+<div class="card-footer">
+<button type="submit" class="btn btn-primary">Submit</button>
+</div>
+</form>
+</div>
+
+
+</section>
+            </div>
