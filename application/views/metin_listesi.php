e319da3e288aa520a00f69e55c1b822b7ee7d06e
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-08-29 16:50:05 +0300

    1

--- application/views/metin_listesi.php
+++ application/views/metin_listesi.php
@@ -10,18 +10,18 @@
         <tr>
             <th style="padding:5px;width:8%;background:black;color:white;">İşlem</th>
             <th style="padding:5px;width:19%;background:black;color:white;">Türkçe</th>
+            <th style="padding:5px;width:19%;background:black;color:white;">İngilizce</th>
             <th style="padding:5px;width:19%;background:black;color:white;">Almanca</th>
             <th style="padding:5px;width:19%;background:black;color:white;">Arapça</th>
-            <th style="padding:5px;width:19%;background:black;color:white;">İngilizce</th>
             <th style="padding:5px;width:19%;background:black;color:white;">Rusça</th>
         </tr>
         <?php foreach ($metinler as $metin): ?>
         <tr>
             <td><a style="padding:10px;padding-right:0px;padding-left:0px;cursor:pointer;background:orange;min-width:100%;height:100%;display:block;text-align:Center;font-weight:bold;" href="<?= site_url('metinler/guncelle/' . $metin->metin_id); ?>">GÜNCELLE</a></td>
             <td style="padding:5px;"><?= $metin->metin_turkce; ?></td>
+            <td style="padding:10px;padding-left:15px;"><?= $metin->metin_ingilizce != "" ? $metin->metin_ingilizce : "<span style='color:red;'># Tanımlanmadı</span>" ?></td>
             <td style="padding:10px;padding-left:15px;"><?= $metin->metin_almanca != "" ? $metin->metin_almanca : "<span style='color:red;'># Tanımlanmadı</span>" ?></td>
             <td style="padding:10px;padding-left:15px;direction: rtl;"><?= $metin->metin_arapca != "" ? $metin->metin_arapca : "<span style='color:red;'># Tanımlanmadı</span>" ?></td>
-            <td style="padding:10px;padding-left:15px;"><?= $metin->metin_ingilizce != "" ? $metin->metin_ingilizce : "<span style='color:red;'># Tanımlanmadı</span>" ?></td>
             <td style="padding:10px;padding-left:15px;"><?= $metin->metin_rusca != "" ? $metin->metin_rusca : "<span style='color:red;'># Tanımlanmadı</span>" ?></td>
             
          
