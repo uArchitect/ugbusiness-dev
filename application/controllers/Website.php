a11ede0fef6747977f8f93f685dc1c1380272fb7
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-09-02 09:46:55 +0300

    1

--- application/controllers/Website.php
+++ application/controllers/Website.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Stok extends CI_Controller {
+class Website extends CI_Controller {
 	function __construct(){
         parent::__construct();
         session_control();
