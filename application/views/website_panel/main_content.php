05e1b16243e2d8750d70b87d35cdd618dea214c0
ergulkizilkaya <kizilkayaergul@gmail.com>
2024-09-02 09:47:37 +0300

    1

--- application/views/website_panel/main_content.php
+++ application/views/website_panel/main_content.php
@@ -5,7 +5,7 @@
 <iframe src="https://www.umex.com.tr/umexwebsitepanel/" style="
     width: -webkit-fill-available;
     height: 878px;
-    margin-top: -10px;
+    margin-top: 0px;
     margin-left: -2px;
     margin-right: 0px;
 "></iframe>
