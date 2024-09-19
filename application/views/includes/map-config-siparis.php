cdd04bb8a0bd9a82ef5e1c80f2f5cd8e727a9c44
ugyazilim <ugyazilim@gmail.com>
2024-09-19 09:34:08 +0300

    1

--- application/views/includes/map-config-siparis.php
+++ application/views/includes/map-config-siparis.php
@@ -18,7 +18,7 @@ foreach ($sehirler as $sehir) {
       "hover" => $sehir->sehir_adi,
       "url" => base_url("cihaz/cihaz_harita_il_detay/".$sehir->sehir_id."/".$secilen_urun),
       "target" => "same_window",
-      "upColor" => "#9a0000",
+      "upColor" => "#b90000",
       "overColor" => "#3535358f",
       "downColor" => "#0c0c0c",
       "active" => true
@@ -28,7 +28,7 @@ foreach ($sehirler as $sehir) {
       "hover" => $sehir->sehir_adi,
       "url" => base_url("cihaz/cihaz_harita_il_detay/".$sehir->sehir_id."/".$secilen_urun),
       "target" => "same_window",
-      "upColor" => "#007217",
+      "upColor" => "#008814",
       "overColor" => "#3535358f",
       "downColor" => "#0c0c0c",
       "active" => true
