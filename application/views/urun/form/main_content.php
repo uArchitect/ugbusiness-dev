ca43baccd143a9d491dacebee17118b10dfbafe3
ugyazilim <ugyazilim@gmail.com>
2024-09-25 14:14:32 +0300

    2

--- application/views/urun/form/main_content.php
+++ application/views/urun/form/main_content.php
@@ -1,22 +1,8 @@
  
 <!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper">
+<div class="content-wrapper pt-2">
     <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Ürün Form</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="<?=base_url()?>">Giriş</a></li>
-              <li class="breadcrumb-item active">Ürün Form</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
+   
     <!-- /.content-header -->
      <div class="row">
 <section class="content col-md-3">
