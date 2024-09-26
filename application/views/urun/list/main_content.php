1f470dc9e4540e8bee565685aa2e04dd9605dbd6
ugyazilim <ugyazilim@gmail.com>
2024-09-26 15:04:49 +0300

    2

--- application/views/urun/list/main_content.php
+++ application/views/urun/list/main_content.php
@@ -3,7 +3,7 @@
 <div class="content-wrapper" style="padding-top:10px">
  
 <section class="content text-md">
-<div class="card card-dark" style="border-radius:0px !important;">
+<div class="card card-dark col-8" style="border-radius:0px !important;">
               <div class="card-header">
               <h3 class="card-title"><strong>UG Business</strong> - Parametreler - Ürün Yönetimi</h3>
                 <a href="<?=base_url("urun/ekle")?>" type="button" class="btn btn-primary btn-sm" style="float: right!important;padding: 0px;padding-left: 5px;padding-right: 5px;"><i class="fa fa-plus" style="font-size:12px" aria-hidden="true"></i> Yeni Kayıt Ekle</a>
@@ -14,8 +14,7 @@
                   <thead>
                   <tr>
                     <th style="width: 42px;">ID</th> 
-                    <th>Ürün Adı</th>
-                    <th>Ürün Açıklaması</th>
+                    <th>Ürün Adı</th> 
                     <th >İşlem</th> 
                   </tr>
                   </thead>
@@ -27,11 +26,7 @@
                       <td><i class="far fa-file-alt" style="margin-right:5px;opacity:1"></i> 
                        <?=$urun->urun_adi?> 
                     </td>
-                      <td style="display: flex;">
-                        <i class="far fa-file-alt" style="margin-right:5px;opacity:0.8"></i>
-                        <?=($urun->urun_aciklama) ? $urun->urun_aciklama : "<span style='opacity:0.4'>Açıklama Girilmedi</span>"?>
- 
-                      </td>
+                      
                      
                       <td>
                     
@@ -45,10 +40,7 @@
                   <tfoot>
                   <tr>
                   <th style="width: 42px;">ID</th> 
-                    <th>Ürün Adı</th>
-                    <th>Ürün Açıklaması</th>
-                    <th style="width: 130px;">Kayıt Tarihi</th>
-                    <th style="width: 130px;">Güncelleme Tarihi</th>
+                    <th>Ürün Adı</th> 
                     <th style="width: 130px;">İşlem</th> 
                   </tr>
                   </tfoot>
