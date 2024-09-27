322891e72aba4b825306038d65ce75f629529bd2
ugyazilim <ugyazilim@gmail.com>
2024-09-27 12:51:26 +0300

    1

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -3,14 +3,16 @@
 class Api extends CI_Controller {
 	function __construct(){
         parent::__construct();
+		
 		header("Access-Control-Allow-Origin: *");
         header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
         header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
    
         date_default_timezone_set('Europe/Istanbul');
+		$this->load->model('Stok_model');
     }
 
-	$this->load->model('Stok_model');
+
 	public function stok_genel_bakis()
 	{
 		$data = $this->Stok_model->get_stok_genel_bakis();
