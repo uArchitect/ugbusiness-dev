6f8f95f07e958b97ec3415aa0e3f2739e2e68afa
ugyazilim <ugyazilim@gmail.com>
2024-09-27 14:26:51 +0300

    3

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -72,6 +72,13 @@ class Login extends CI_Controller {
                 if($query){
                     $this->db->where("kullanici_id",$query[0]->kullanici_id)->update("kullanicilar",["giris_deneme" => 0])
                   
+                    $this->session->set_flashdata('flashDanger', " 5 kere hatalı giriş denemesi yaptığınız için hesabınız bloklanmıştır. Lütfen sistem yöneticiniz ile iletişime geçiniz. ");
+                  
+                    redirect(base_url("giris-yap"));
+
+
+
+
 
                     $combine = $this->input->ip_address().$this->input->post('username');
                     $crypto = sha1(md5($combine));
