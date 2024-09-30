0a02e58da759373129788ba4befd3cdf5f11e567
ugyazilim <ugyazilim@gmail.com>
2024-09-30 15:05:35 +0300

    sf

--- application/controllers/Urun.php
+++ application/controllers/Urun.php
@@ -86,7 +86,7 @@ $viewData['secilen_urun'] = $id;
 
 
             $viewData['kullanicilar'] = $this->db->order_by("kullanici_id","asc")->where("kullanici_aktif",1)->get("kullanicilar")->result();
-            $viewData['limitkullanicilar'] = $this->db->where("kullanici_aktif",1)->where("kullanici_limit_kontrol",1)->get("kullanicilar")->result();
+            $viewData['limitkullanicilar'] = $this->db->order_by("kullanici_id","asc")->where("kullanici_aktif",1)->where("kullanici_limit_kontrol",1)->get("kullanicilar")->result();
 
 
 
