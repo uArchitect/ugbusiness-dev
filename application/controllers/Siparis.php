a448ffa5a835f964ded90433432c565726928c42
ugyazilim <ugyazilim@gmail.com>
2024-10-02 17:19:03 +0300

    ad

--- application/controllers/Siparis.php
+++ application/controllers/Siparis.php
@@ -336,7 +336,7 @@ class Siparis extends CI_Controller {
 
 		if($siparis[0]->musteri_degerlendirme_sms == 1 ){
 			
-			echo "<script>alert('Bu müşteri için değerlendirme sms'i zaten gönderilmiştir.')" ;
+			echo "<script>alert('Bu müşteri için değerlendirme sms'i zaten gönderilmiştir.');</script>" ;
 			redirect(base_url("tum-siparisler"));
 		}
 
