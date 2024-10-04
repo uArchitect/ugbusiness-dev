e6e1c29555bb04b328c803e0ef1a76bb4f1ae8ec
ugyazilim <ugyazilim@gmail.com>
2024-10-04 09:51:29 +0300

    sf

--- application/controllers/Siparis.php
+++ application/controllers/Siparis.php
@@ -1501,7 +1501,7 @@ class Siparis extends CI_Controller {
             $data[] = [
                 '<b><a href="" onclick="showWindow(\''.$urlcustom.'\');">'.$row->siparis_kodu.'</a></b><br><span style="font-weight:normal">'.date('d.m.Y H:i',strtotime($row->kayit_tarihi)).'</span>',
                 "<b>".$musteri."</b><br>"."<span style='font-weight:normal'>".formatTelephoneNumber($row->musteri_iletisim_numarasi)." (<b>SMS G.</b> : ".date("d.m.Y H:i",strtotime($row->degerlendirme_sms_gonderim_tarihi)).")</span>", 
-				"<b>".$row->merkez_adi."</b><span style='font-weight:normal'> / ".$row->sehir_adi." (".$row->ilce_adi.")"."</span><br>",
+				"<b>".$row->merkez_adi."</b><span style='font-weight:normal'> / ".$row->sehir_adi." (".$row->ilce_adi.")"."</span><br>".(($row->merkez_adresi == "" || $row->merkez_adresi == "." || $row->merkez_adresi == "0") ? '<span style="opacity:0.4;font-weight:normal">BU MERKEZE TANIMLI ADRES KAYDI BULUNAMADI</span>' : "<span title='".$row->merkez_adresi."' style='font-weight:normal'>".substr($row->merkez_adresi,0,90).(strlen($row->merkez_adresi)>90 ? "...":"")."...</span>"),
 			
 				($row->kullanici_id != 1 ? $row->kullanici_ad_soyad : "<span style='opacity:0.6'>Eski Kayıt</span>"),
 				($row->degerlendirme_soru_1 > 0 ? "<span class='btn btn-$color1 btn-xs' style='display: block;margin:auto;margin-top:5px;width:25px;'>".$row->degerlendirme_soru_1."</span>" : "<span style='opacity:0.5'>Beklemede</span>"),
