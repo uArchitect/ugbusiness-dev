da918c30be70bd92946828d7692fb0e89b5c5de6
ugyazilim <ugyazilim@gmail.com>
2024-10-07 17:00:54 +0300

    ad

--- application/views/musteri/karaliste/main_content.php
+++ application/views/musteri/karaliste/main_content.php
@@ -5,22 +5,32 @@
  
  <div class="card-body">
  <form action="<?=base_url("musteri/karaliste_view")?>" method="POST">
- <div class="row">
- 
- 
- <div class="col-md-9">
- <label for="exampleInputEmail1">Tekrar Aranmak İstemeyen Müşteri Numarası</label>
- <input type="text" required name="kara_liste_iletisim_numarasi" id="exampleInputEmail1" class="form-control">
- </div>
- <div class="col-md-3">
- <label for="exampleInputEmasil1">&nbsp;</label>
- <button type="submit" id="exampleInputEmasil1" class="btn btn-block btn-success btn-lg">Kaydet</button>
- </div>
- </div>
- 
- </form>
- 
- 
+    <div class="row">
+        <div class="col-md-9">
+            <label for="exampleInputEmail1">Tekrar Aranmak İstemeyen Müşteri Numarası</label>
+            <input type="text" required name="kara_liste_iletisim_numarasi" id="karaListeNumarasi" class="form-control" maxlength="11" oninput="maskNumber(this)">
+            <small id="error-message" style="color: red; display: none;">Numara "05" ile başlamalı ve 11 karakter olmalıdır.</small>
+        </div>
+        <div class="col-md-3">
+            <label for="exampleInputEmasil1">&nbsp;</label>
+            <button type="submit" id="exampleInputEmasil1" class="btn btn-block btn-success btn-lg">Kaydet</button>
+        </div>
+    </div>
+</form>
+<script>
+function maskNumber(input) {
+    const value = input.value.replace(/\D/g, ''); // Sadece rakamları al
+    if (value.length > 0 && !value.startsWith('05')) {
+        input.value = ''; // "05" ile başlamıyorsa temizle
+        document.getElementById('error-message').style.display = 'block';
+    } else if (value.length > 11) {
+        input.value = value.substring(0, 11); // 11 karakterden fazlasını kes
+    } else {
+        input.value = value; // Geçerli değeri ayarla
+        document.getElementById('error-message').style.display = 'none'; // Hata mesajını gizle
+    }
+}
+</script>
  </div>
  
  </div>
