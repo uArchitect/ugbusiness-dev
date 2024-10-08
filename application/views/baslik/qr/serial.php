8edf7ed1f680eb46fd97ddd290bce7907702d1bd
ugyazilim <ugyazilim@gmail.com>
2024-10-08 11:46:09 +0300

    f

--- application/views/baslik/qr/serial.php
+++ application/views/baslik/qr/serial.php
@@ -1,13 +1,12 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.qrcode/1.0/jquery.qrcode.min.js"></script>
 </head>
-<body onload="generateQRCodes(230900101, 10);">
+<body onload="generateQRCodes(230900101, 3);">
 
     <div id="yazdir3"></div>
 
+    <script src="<?=base_url('assets/dist/js/qr.js')?>"></script>
     <script>
         function generateQRCodes(startSerial, totalCount) {
             let yazdirDiv = document.getElementById("yazdir3");
@@ -18,38 +17,43 @@
 
                 qrContainer.innerHTML = `
                     <div class="col-lg-12 text-center" style="text-align:center">
-                       <p style="margin-top:-10px;text-align:center;font-weight:500;font-family: system-ui;margin-left: 2px;">
-                           <div id="canvas_${serialNo}" style="scale: 0.7;margin-left: 0px;margin-top: 26px;"></div>
-                          <b>LAMBA ETİKETİ</b><br>
+                        <div id="canvas5_${serialNo}" style="scale: 0.7;margin-left: 11px;margin-top: 26px;"></div>
+                        <p style="margin-top:-10px;text-align:center;font-weight:500;font-family: system-ui;margin-left: 2px;">
+                            <b>LAMBA ETİKETİ</b><br>
                             Seri No : <span>${serialNo}</span>
                         </p>
-                    </div>
-                      <div class="col-lg-12 text-center" style="text-align:center">
-                       <p style="margin-top:-10px;text-align:center;font-weight:500;font-family: system-ui;margin-left: 2px;">
-                           <div id="canvas1_${serialNo}" style="scale: 0.7;margin-left: 0px;margin-top: 26px;"></div>
-                          <b>LAMBA ETİKETİ</b><br>
-                            Seri No : <span>${serialNo}</span>
-                        </p>
+                        <div id="canvas6_${serialNo}" style="scale: 0.7;margin-left: 12px;margin-top: 20px;"></div>
                     </div>
                 `;
 
                 yazdirDiv.appendChild(qrContainer);
 
-                // QR kodunu oluştur
-                $('#canvas_' + serialNo).qrcode({
-                    text: serialNo.toString(),
+                // QR kodlarını oluştur
+                const qrCode5 = new QRCodeStyling({
                     width: 200,
-                    height: 200
+                    height: 200,
+                    type: "canvas",
+                    data: serialNo.toString(),
+                    backgroundOptions: {
+                        color: "#fff",
+                    }
                 });
-                $('#canvas1_' + serialNo).qrcode({
-                    text: serialNo.toString(),
+                const qrCode6 = new QRCodeStyling({
                     width: 200,
-                    height: 200
+                    height: 200,
+                    type: "canvas",
+                    data: serialNo.toString(),
+                    backgroundOptions: {
+                        color: "#fff",
+                    },
                 });
+
+                qrCode5.append(document.getElementById(`canvas5_${serialNo}`));
+                qrCode6.append(document.getElementById(`canvas6_${serialNo}`));
             }
 
             // QR kodları tamamlandıktan sonra yazdır
-            setTimeout(() => {
+            setTimeout(function () {
                 window.print();
                 window.close(); // Yazdırma bittiğinde pencereyi kapat
             }, 1000); // 1 saniye bekleme süresi
