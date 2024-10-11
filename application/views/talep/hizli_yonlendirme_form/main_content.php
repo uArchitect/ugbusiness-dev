75e5a103eaf651d1c322dbbc1b06a1112fd85c11
ugyazilim <ugyazilim@gmail.com>
2024-10-11 15:49:30 +0300

    sdg

--- application/views/talep/hizli_yonlendirme_form/main_content.php
+++ application/views/talep/hizli_yonlendirme_form/main_content.php
@@ -143,8 +143,22 @@ width:31%;border-radius:2px;">
 
 
 <section>
-  sontalepler
-  <?php echo json_encode($sontalepler); ?>
+  <div class="row">
+  <?php  
+  
+  foreach ($sontalepler as $stalep) {
+    ?>
+    <div class="col-12">
+    <button class="btn btn-default d-block" style="width: -webkit-fill-available;">
+      <span><?=$stalep->talep_cep_telefon?></span><br>
+      <span><?=$stalep->yonlenen_ad_soyad?></span><br>
+      <span><?=date("d.m.Y H:i",strtotime($stalep->yonlendirme_tarihi))?></span>
+    </button>
+    </div>
+    <?php
+  }
+  
+  ?></div>
 </section>
 
 
