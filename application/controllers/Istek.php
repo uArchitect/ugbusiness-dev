ed421ef5f518ae961cdb11d118a5779b33dd7294
ugyazilim <ugyazilim@gmail.com>
2024-10-11 16:23:07 +0300

    asf

--- application/controllers/Istek.php
+++ application/controllers/Istek.php
@@ -304,7 +304,7 @@ class Istek extends CI_Controller {
                 $data['istek_adi']  = escape($this->input->post('istek_adi'));
 
             }
-           $data['istek_aciklama']  = escape(strip_tags($this->input->post('istek_aciklama')));
+           $data['istek_aciklama']  = str_replace("\n","",escape(strip_tags($this->input->post('istek_aciklama'))));
      
            $this->Istek_model->insert($data);
            $inserted_id = $this->db->insert_id();
