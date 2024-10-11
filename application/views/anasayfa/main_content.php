730c8889e96145b66e7b3beb415fe7668d5ea0b4
ugyazilim <ugyazilim@gmail.com>
2024-10-11 11:41:55 +0300

    sdg

--- application/views/anasayfa/main_content.php
+++ application/views/anasayfa/main_content.php
@@ -33,7 +33,7 @@ function myFunction() {
       <div class="container-fluid pr-0" style="margin-left: -8px;">
          
         <!-- Main row -->
-        <div class="row">
+        <div class="row" style="margin-right:-8px;">
 
 
 
@@ -246,11 +246,11 @@ function myFunction() {
              
           </section> 
           <?php  if(goruntuleme_kontrol("yemek_listesi_goruntule")) : ?>
-          <section class="col-lg-4 pl-0 text-center">
-            
+          <section class="col-lg-4 pl-0 text-center pr-2" style="    padding-right: 1.0rem !important;">
+      
           <div class="col <?=($yemek->yemek_detay=="")?"d-none":""?>" style="border: 1px solid #d9d7d7; margin: 5px; padding: 0; border-radius: 8px;">
-  <div style="min-height:280px;position: relative; background-size: cover; background-position: center; background-image: url('https://beyazsayfayemek.com/wp-content/uploads/2021/10/awesome-indian-food-wallpaper-preview.jpg'); border-radius: 8px; overflow: hidden;">
-    <div style="align-content: center;min-height:280px;background: rgba(0, 0, 0, 0.7); padding: 20px; border-radius: 8px;">
+  <div style="    border: 5px solid #ffffff;outline: 1px solid #073773;min-height:270px;position: relative; background-size: cover; background-position: center; background-image: url('https://beyazsayfayemek.com/wp-content/uploads/2021/10/awesome-indian-food-wallpaper-preview.jpg'); border-radius: 8px; overflow: hidden;">
+    <div style="align-content: center;min-height:270px;background: rgba(0, 0, 0, 0.7); padding: 20px; border-radius: 8px;">
       <?php
       $guncelTarih = getdate();
       $gunSayisi = date('t', mktime(0, 0, 0, $guncelTarih['mon'], 1, $guncelTarih['year']));
