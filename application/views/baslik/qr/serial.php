c63341e2855951b098d0827bf1cf62d1fe212150
ugyazilim <ugyazilim@gmail.com>
2024-10-14 13:12:22 +0300

    sf

--- application/views/baslik/qr/serial.php
+++ application/views/baslik/qr/serial.php
@@ -2,7 +2,7 @@
 <head>
     <meta charset="utf-8">
 </head>
-<body onload="generateQRCodes(230900101, 3);">
+<body onload="generateQRCodes(230900120, 40);">
 
     <div id="yazdir3"></div>
 
@@ -16,13 +16,13 @@
                 let qrContainer = document.createElement("div");
 
                 qrContainer.innerHTML = `
-                    <div class="col-lg-12 text-center" style="text-align:center">
-                        <div id="canvas5_${serialNo}" style="scale: 0.7;margin-left: 11px;margin-top: 26px;"></div>
+                    <div class="col-lg-12 text-center" style="margin-top:36px;margin-bottom:15px;text-align:center">
+                        <div id="canvas5_${serialNo}" style="scale: 0.6;margin-left: 11px;margin-top: 26px;"></div>
                         <p style="margin-top:-10px;text-align:center;font-weight:500;font-family: system-ui;margin-left: 2px;">
                             <b>LAMBA ETİKETİ</b><br>
                             Seri No : <span>${serialNo}</span>
                         </p>
-                        <div id="canvas6_${serialNo}" style="scale: 0.7;margin-left: 12px;margin-top: 20px;"></div>
+                        <div id="canvas6_${serialNo}" style="scale: 0.6;margin-left: 12px;margin-top: 25px;"></div>
                     </div>
                 `;
 
@@ -33,31 +33,13 @@
                     width: 200,
                     height: 200, 
                     data: serialNo.toString(),
-                    dotsOptions: {
-            color: "#000",
-            type: "square"
-        },
-        backgroundOptions: {
-            color: "#fff",
-        },
-        imageOptions: {
-            crossOrigin: "anonymous"
-        }
+                 
                 });
                 const qrCode6 = new QRCodeStyling({
                     width: 200,
                     height: 200, 
                     data: serialNo.toString(),
-                    dotsOptions: {
-            color: "#000",
-            type: "square"
-        },
-        backgroundOptions: {
-            color: "#fff",
-        },
-        imageOptions: {
-            crossOrigin: "anonymous"
-        }
+                   
                 });
 
                 qrCode5.append(document.getElementById(`canvas5_${serialNo}`));
