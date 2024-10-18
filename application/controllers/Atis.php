2754c3551516a8bb30d9e7cdd4f8a72723ee5fb2
ugyazilim <ugyazilim@gmail.com>
2024-10-18 11:29:27 +0300

    s

--- application/controllers/Atis.php
+++ application/controllers/Atis.php
@@ -3,13 +3,9 @@
 class Atis extends CI_Controller {
 	function __construct(){
         parent::__construct();
-		
-		header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-        header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-   
+		 
         date_default_timezone_set('Europe/Istanbul');
-		$this->load->model('Stok_model');
+	 
     }
 
 	
