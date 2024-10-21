7a37634f093828372cda7d2173c1726cb01c795b
ugyazilim <ugyazilim@gmail.com>
2024-10-21 10:23:46 +0300

    s

--- application/views/anasayfatest/main_content.php
+++ application/views/anasayfatest/main_content.php
@@ -6,7 +6,7 @@
 
 
 
-<div style="position: relative; width: 100%; height: 0; padding-top: 50.0000%;
+<div tabindex="-1" style="position: relative; width: 100%; height: 0; padding-top: 50.0000%;
  padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16);  margin-bottom: 0.9em; overflow: hidden;
  border-radius: 8px; will-change: transform;">
   <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
@@ -14,10 +14,7 @@
   </iframe>
 </div> 
 
-
-
-
-
+ 
 
 
 
