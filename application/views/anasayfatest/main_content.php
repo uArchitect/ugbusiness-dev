2d0992209483891c7b73233f25bce39f337cac36
ugyazilim <ugyazilim@gmail.com>
2024-10-21 09:30:08 +0300

    4

--- application/views/anasayfatest/main_content.php
+++ application/views/anasayfatest/main_content.php
@@ -1,5 +1,26 @@
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper p-1 mobil-genislik" style="padding-top:0px; margin-left:250px;">
+
+
+
+
+
+
+<div style="position: relative; width: 100%; height: 0; padding-top: 50.0000%;
+ padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16);  margin-bottom: 0.9em; overflow: hidden;
+ border-radius: 8px; will-change: transform;">
+  <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
+    src="https://www.canva.com/design/DAGT7hcPHQc/tL9MP00uO79UoQqzpAqasw/view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+  </iframe>
+</div> 
+
+
+
+
+
+
+
+
   <div class="grid-container">
     <!-- 13 sütun ve 19 satır için butonlar -->
     <div class="grid">
