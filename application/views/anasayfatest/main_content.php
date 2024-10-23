d6e302815c58b825b2e974fea8142aeee2447b71
ugyazilim <ugyazilim@gmail.com>
2024-10-23 16:33:55 +0300

    asf

--- application/views/anasayfatest/main_content.php
+++ application/views/anasayfatest/main_content.php
@@ -5,7 +5,7 @@
 
 
 
-
+<div style="background:blue;height:40px;width:100%;"></div>
 <div tabindex="-1" style="position: relative; width: 100%;  height: 1051px; padding-top: 50.0000%;
  padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16);  margin-bottom: 0.9em; overflow: hidden;
  border-radius: 8px; will-change: transform;">
