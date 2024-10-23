2b247040fabde6ef45b9455a217610682636943f
ugyazilim <ugyazilim@gmail.com>
2024-10-23 16:41:08 +0300

    s

--- application/views/anasayfatest/main_content.php
+++ application/views/anasayfatest/main_content.php
@@ -6,7 +6,7 @@
 
 
 <div style="background:#07277d;height:40px;width:100%;color: white;
-    text-align: right;
+    text-align: right;background: linear-gradient(181deg, rgba(2, 0, 36, 1) 0%, rgba(9, 9, 121, 1) 35%, rgb(17 72 216) 100%);
     padding-top: 9px;" class="<?=($yemek->yemek_detay=="")?"d-none":""?>">
 
 
