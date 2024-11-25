0115f797ed3b4851432841d3a185d8dd5d560216
ugyazilim <ugyazilim@gmail.com>
2024-11-25 11:51:21 +0300

    asf

--- application/views/siparis/siparise_urun_ekle/main_content.php
+++ application/views/siparis/siparise_urun_ekle/main_content.php
@@ -15,7 +15,7 @@
 
            <div class="row p-2" >
 
-           <div class="form-group col  pl-0 pr-0 mt-2 mb-1">
+           <div class="form-group col-12  pl-0 pr-0 mt-2 mb-1">
      <label for="formClient-Name"><i class="fas fa-lira-sign text-orange"></i> Siparis Kodu</label>
      <input type="text"   class="form-control"     value="<?=$siparis_data->siparis_kodu?>"   required=""  >
     </div>
