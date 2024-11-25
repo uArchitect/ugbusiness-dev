3e8b7925223323846341fc919f89bd9dc20b61ca
ugyazilim <ugyazilim@gmail.com>
2024-11-25 10:31:16 +0300

    sf

--- application/views/rehber/main_content.php
+++ application/views/rehber/main_content.php
@@ -59,9 +59,12 @@ text-align: center;
 <div class="name text-bold"><?=$kullanici->kullanici_ad_soyad?></div>
   <div class="job"><?=$kullanici->kullanici_unvan?></div>
   </div>
+  <div class="f-lex">
   <div onclick="location.href='tel:<?=$kullanici->kullanici_bireysel_iletisim_no?>';" class="media-icons text-primary" style="background: #ebebeb; color: black !important; border-radius: 5px; padding: 5px 5px;">
 <i class="fa fa-phone"></i> <?=$kullanici->kullanici_bireysel_iletisim_no?>
   </div>  
+  <div href="https://wa.me/9<?=str_replace(" ","",$kullanici->kullanici_bireysel_iletisim_no)?>" class="media-icons text-primary" style="background: #22b100;color: #ffffff !important;border-radius: 5px;padding: 5px 5px;padding: 5px 10px;margin-right: 5px;">
+<i class="fab fa-whatsapp"></i></div></div>
 </div>
 </div>
 
