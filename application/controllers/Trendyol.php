12213d1ea3c62902dec966131c8f6b77130444ee
ugyazilim <ugyazilim@gmail.com>
2024-12-02 14:45:01 +0300

    s

--- application/controllers/Trendyol.php
+++ application/controllers/Trendyol.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Sehir extends CI_Controller {
+class Trendyol extends CI_Controller {
 	function __construct(){
         parent::__construct();
         session_control();
