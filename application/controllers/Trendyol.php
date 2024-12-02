ed1f8952d92450e8f94f2d4dbfb9df8a80b55a7d
ugyazilim <ugyazilim@gmail.com>
2024-12-02 14:44:34 +0300

    asf

---
+++ application/controllers/Trendyol.php
@@ -0,0 +1,61 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Sehir extends CI_Controller {
+	function __construct(){
+        parent::__construct();
+        session_control();
+        date_default_timezone_set('Europe/Istanbul');
+    }
+ 
+	public function index()
+	{
+        yetki_kontrol("trendyol_siparislerini_goruntule");
+       
+  
+$username = 'KRgGB8YfCyHNgTp1vu5N';  
+$password = 'leVtgjJK3JE6Upeu8oEO';  
+$api_url = 'https://api.trendyol.com/sapigw/suppliers/534419/orders';  
+
+  
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $api_url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'Authorization: Basic ' . base64_encode("$username:$password"),
+    'Content-Type: application/json'
+]);
+ 
+$response = curl_exec($ch);
+ 
+if (curl_errno($ch)) {
+    echo 'Hata: ' . curl_error($ch);
+    curl_close($ch);
+    exit;
+}
+
+curl_close($ch);
+ 
+$data = json_decode($response, true);
+ 
+if (isset($data['content'])) {
+  
+
+    
+    $viewData["siparis_data"] = $data;
+    $viewData["page"] = "trendyol/siparis";
+    $this->load->view('base_view',$viewData);
+} else {
+    echo "Sipariş bulunamadı veya API'den bir hata döndü." . PHP_EOL;
+}
+
+
+
+
+
+
+
+	}
+
+	
+}
---
+++ application/views/trendyol/siparis/main_content.php
@@ -0,0 +1,69 @@
+foreach ($data['content'] as $order) {
+        echo "Sipariş ID: " . $order['id'] . PHP_EOL;
+        echo "Müşteri Adı: " . $order['customerFirstName'] .' '.$order['customerLastName']. PHP_EOL;
+        echo "Toplam Tutar: " . $order['totalPrice'] . PHP_EOL;
+        echo "Durum: " . $order['status'] . PHP_EOL;
+           echo "Adres: " . $order['invoiceAddress']['fullAddress'] . PHP_EOL;
+            echo "Ürün: " . $order['lines'][0]['productName'] . PHP_EOL;
+            echo "Sipariş Tarihi: " . date("d.m.Y H:i", $order['orderDate'] / 1000) . PHP_EOL;
+           echo "Fatura: " .$order['invoiceLink']  . PHP_EOL;
+           
+        echo str_repeat('-', 20) . PHP_EOL;
+        
+    }
+
+
+
+
+
+
+
+
+     
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper" style="padding-top:8px">
+ <style>.dataTables_wrapper th, td { white-space: nowrap; }</style>
+<section class="content text-md">
+<div class="card card-dark">
+              <div class="card-header">
+                <h3 class="card-title"><strong>UG Business</strong> - Trendyol Son 1 Hafta Siparişler</h3>
+                <a href="<?=base_url("kullanici/ekle")?>" type="button" class="btn btn-success btn-xs" style="float: right!important;padding: 0px;padding-left: 5px;padding-right: 5px;"><i class="fa fa-plus" style="font-size:12px" aria-hidden="true"></i> Yeni Kayıt Ekle</a>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <table id="examplekullanicilar" class="table table-bordered table-striped"    >
+                  <thead>
+                  <tr>
+               
+                    <th>Siparis</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                    <?php  
+                    
+                    foreach ($data['content'] as $order) {
+                        echo "Sipariş ID: " . $order['id'] . PHP_EOL;
+                        echo "Müşteri Adı: " . $order['customerFirstName'] .' '.$order['customerLastName']. PHP_EOL;
+                        echo "Toplam Tutar: " . $order['totalPrice'] . PHP_EOL;
+                        echo "Durum: " . $order['status'] . PHP_EOL;
+                           echo "Adres: " . $order['invoiceAddress']['fullAddress'] . PHP_EOL;
+                            echo "Ürün: " . $order['lines'][0]['productName'] . PHP_EOL;
+                            echo "Sipariş Tarihi: " . date("d.m.Y H:i", $order['orderDate'] / 1000) . PHP_EOL;
+                           echo "Fatura: " .$order['invoiceLink']  . PHP_EOL;
+                           
+                        echo str_repeat('-', 20) . PHP_EOL;
+                        
+                    }
+                    
+                    
+                    
+                    ?>
+                  </tbody>
+ 
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+</section>
+            </div>
