857f20a08727504362f6a8d5bb6c1ce50a26eb7e
ugyazilim <ugyazilim@gmail.com>
2024-12-03 10:07:49 +0300

    asf

--- application/views/trendyol/siparis/main_content.php
+++ application/views/trendyol/siparis/main_content.php
@@ -19,7 +19,7 @@
 ">
     
     </h3>
-        <div class="d-flex mt-3" style="margin: auto;">
+        <div class="d-flex mt-3 mb-3" style="margin: auto;">
             <button class="btn btn-default" style="    background: #ee6433;
     color: white;" onclick="showHide('siparis_div','btn-siparis');" id="btn-siparis">TRENDYOL SİPARİŞLERİ</button>
             <button class="btn btn-default mr-2 ml-2" onclick="showHide('urun_div','btn-urun');" id="btn-urun">TRENDYOL ÜRÜNLERİ</button>
