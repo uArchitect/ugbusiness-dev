bca420ea40a5d95de063c1e29584dd942c548cfb
ugyazilim <ugyazilim@gmail.com>
2024-12-10 17:16:54 +0300

    sf

--- application/views/teslimat/form/main_content.php
+++ application/views/teslimat/form/main_content.php
@@ -1,10 +1,5 @@
 <style>
-    body {
-        font-family: 'Arial', sans-serif;
-        background-color: #f8f9fa;
-        margin: 0;
-        padding: 20px;
-    }
+    
     .content-wrapper {
         max-width: 800px;
         margin: 0 auto;
