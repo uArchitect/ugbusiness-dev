7a17502768aecd403e3a0193ddd4f6570a39842c
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2024-12-11 22:01:18 +0300

    1

--- application/views/arvento/main_content.php
+++ application/views/arvento/main_content.php
@@ -7,7 +7,9 @@
         } 
     </style>
 <div class="content-wrapper p-1 pr-2" style="margin-left: 235px;padding-top:15px">
+
   <div class="row">
+      <div class="col-md-12" style="height:100px"></div>
     <div class="col-md-3"></div>
     <div class="col-md-9"><div id="map" style="height: 804px !important;"></div></div>
   </div>
