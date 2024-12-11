f7f3413343f2a732cf747c709fdc6f1b402bb628
ugyazilim <ugyazilim@gmail.com>
2024-12-11 18:09:38 +0300

    asf

--- application/controllers/Arvento.php
+++ application/controllers/Arvento.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Ariza extends CI_Controller {
+class Arvento extends CI_Controller {
 	function __construct(){
         parent::__construct();
         session_control();
