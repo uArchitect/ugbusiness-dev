a2144541a594e50955ac35b79095d6026adc359b
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2024-12-11 21:59:06 +0300

    1

--- application/views/arvento/main_content.php
+++ application/views/arvento/main_content.php
@@ -7,7 +7,11 @@
         } 
     </style>
 <div class="content-wrapper p-1 pr-2" style="margin-left: 235px;padding-top:15px">
-<div id="map" style="height: 804px !important;"></div>
+  <div class="row">
+    <div class="col-md-3"></div>
+    <div class="col-md-9"><div id="map" style="height: 804px !important;"></div></div>
+  </div>
+
 <div class="row"> 
   <?php
   foreach ($driverdata as $d) {
