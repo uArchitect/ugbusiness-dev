929bb19cbdb9a2322bd9ccb9e0946100407aa699
ugyazilim <ugyazilim@gmail.com>
2024-12-11 18:15:52 +0300

    asf

--- application/controllers/Anasayfa.php
+++ application/controllers/Anasayfa.php
@@ -62,8 +62,7 @@ $pin1 = "Umexapi.2425";
 $pin2 = "Umexapi.2425";
 $language = "tr";
 // SOAP isteği
-$soapRequest = '
-<?xml version="1.0" encoding="utf-8"?>
+$soapRequest = '<?xml version="1.0" encoding="utf-8"?>
 <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
   <soap:Body>
     <GetVehicleStatus xmlns="https://www.arvento.com/">
