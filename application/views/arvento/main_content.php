00e540b030e95c7c6d97300bfb2faa54251a6fdb
ugyazilim <ugyazilim@gmail.com>
2024-12-12 10:16:52 +0300

    s

--- application/views/arvento/main_content.php
+++ application/views/arvento/main_content.php
@@ -78,7 +78,7 @@
         button.addEventListener('click', () => {
        
             const node = button.getAttribute('data-node'); // Butonun node değerini al
-            
+            alert(markers[node].getLatLng());
             if (markers[node]) {
                 map.setView(markers[node].getLatLng(), 13); // Marker konumuna zoom yap
                 markers[node].openPopup(); // Popup'ı aç
