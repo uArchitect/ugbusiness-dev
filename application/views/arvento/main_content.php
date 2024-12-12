8af1903e341f21f192500366af6abde7cb34d050
ugyazilim <ugyazilim@gmail.com>
2024-12-12 10:47:23 +0300

    sf

--- application/views/arvento/main_content.php
+++ application/views/arvento/main_content.php
@@ -9,8 +9,8 @@
 <div class="content-wrapper p-1 pr-2" style="margin-left: 235px;padding-top:15px">
 
   <div class="row">
-      <div class="col-md-12" style="height: 100px; background: #000951b3; margin-bottom: -100px; z-index: 999;">
-        <img src="<?=base_url("uploads/arventoumex.png")?>" style="height: 100px; margin: auto; display: block;" alt="">
+  <div class="col-md-12" style="height: 62px;background: #000951b3;margin-bottom: -100px;z-index: 999;">
+        <img src="https://ugbusiness.com.tr/uploads/arventoumex.png" style="height: 83px;margin: auto;display: block;margin-top: -13px;" alt="">
       </div>
     <div class="col-md-3 d-none"></div>
     <div class="col-md-12"><div id="map" style="height: 904px !important;"></div></div>
