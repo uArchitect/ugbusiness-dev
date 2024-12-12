0da5403e3a36c217eb628f4edbee0e7fd1b8b5b4
ugyazilim <ugyazilim@gmail.com>
2024-12-12 16:55:31 +0300

    asf

--- application/views/arvento/main_content.php
+++ application/views/arvento/main_content.php
@@ -167,11 +167,11 @@ function updateMarkers() {
                     if(pin.speed > 0){
                       document.getElementById("durum-"+pin.node+"-1").style.display = "none";
                       document.getElementById("durum-"+pin.node+"-2").style.display = "block";
-                      document.getElementById("button-"+pin.node).style.border = "2px solid #04f100;";
+                      document.getElementById("button-"+pin.node).style.borderColor = "#04f100";
                     }else{
                       document.getElementById("durum-"+pin.node+"-2").style.display = "none";
                       document.getElementById("durum-"+pin.node+"-1").style.display = "block";
-                       document.getElementById("button-"+pin.node).style.border = "2px solid #003b64;";
+                       document.getElementById("button-"+pin.node).style.borderColor = "#003b64";
                     }
 
 
