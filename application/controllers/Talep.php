2a6e34531be7ed4205124bbd073e3ea731114497
ugyazilim <ugyazilim@gmail.com>
2025-01-20 11:14:43 +0300

    asf

--- application/controllers/Talep.php
+++ application/controllers/Talep.php
@@ -880,7 +880,7 @@ LEFT JOIN talepler t ON t.talep_kaynak_no = tk.talep_kaynak_id
 
         $controlmusteriad = $this->input->post('talep_musteri_ad_soyad');
         if (str_word_count($controlmusteriad) === 1) {
-            $this->session->set_flashdata('flashDanger','Ad Soyad Geçersiz. Bilgileri Kontrol Edip Tekrar Deneyiniz.');
+            $this->session->set_flashdata('flashDanger','Ad Soyad Geçersiz. Soyad Bilgisi Zorunludur. Bilgileri Kontrol Edip Tekrar Deneyiniz.');
             redirect($_SERVER['HTTP_REFERER']); 
         }
         $kucukMetin = mb_strtolower($controlmusteriad, 'UTF-8');
