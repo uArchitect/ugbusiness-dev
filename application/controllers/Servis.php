47153689c584f8a0e46b441966efafbd8eb19d0b
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-01-25 17:09:30 +0300

    asf

--- application/controllers/Servis.php
+++ application/controllers/Servis.php
@@ -1008,6 +1008,7 @@ public function servis_bildirim_guncelle($servis_id = 0,$guncellenecek_bildirim 
 
 
 					  $musterimerkezdata =  $this->db
+					  ->where("musteri_id",$row->merkez_yetkili_id)
 					  ->join('musteriler', 'musteriler.musteri_id = merkez_yetkili_id')
 					   ->join('sehirler', 'sehirler.sehir_id = merkez_il_id','left')
 					   ->join('ilceler', 'ilceler.ilce_id = merkez_ilce_id','left')
