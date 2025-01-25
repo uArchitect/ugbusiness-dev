02bb85444d618a86344280e5d967a24316c949b5
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-01-25 17:25:53 +0300

    asf

--- application/controllers/Cihaz.php
+++ application/controllers/Cihaz.php
@@ -773,6 +773,7 @@ if($search != null)
         ;
 
         $query = $this->db
+        ->where("musteri_aktif",1)
         ->join('musteriler', 'musteriler.musteri_id = merkez_yetkili_id')
          ->join('sehirler', 'sehirler.sehir_id = merkez_il_id','left')
          ->join('ilceler', 'ilceler.ilce_id = merkez_ilce_id','left')
