0caed537bec9cddac4f11a59db8c08134c00c9b5
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-01-28 17:19:50 +0300

    asf

--- application/views/siparis/merkez_bilgi_form/main_content.php
+++ application/views/siparis/merkez_bilgi_form/main_content.php
@@ -77,8 +77,9 @@
       
       <div class="row mb-2">
         <div class="col">
+          
         <i class="fas fa-tint text-primary"></i> Ürün Bilgisi
-        <select id="urun_no<?=$urun->siparis_urun_id?>" class="form-control" >
+        <select name="urun_no<?=$urun->siparis_urun_id?>" class="form-control" >
     <option <?=($urun->urun_no == 1 ? "selected" : "")?> value="1">Umex Lazer</option>
     <option <?=($urun->urun_no == 2 ? "selected" : "")?> value="2">Umex Diode</option>
     <option <?=($urun->urun_no == 3 ? "selected" : "")?> value="3">Umex EMS</option>
