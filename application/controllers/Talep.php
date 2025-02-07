62eb2ed511e6c9bd46761d76ba6d571490fa129e
ugyazilim <ugyazilim@gmail.com>
2025-02-07 10:13:05 +0300

    asf

--- application/controllers/Talep.php
+++ application/controllers/Talep.php
@@ -307,7 +307,7 @@ $this->db->where('kullanici_aktif', 1);
         $viewData["talepler"] = $tquery;
 
 
-        $kullanicilar = $this->db->order_by('kullanici_adi', 'ASC')->where("kullanici_aktif"=>1)->or_where(["kullanici_departman_id"=>12,"kullanici_departman_id"=>17])
+        $kullanicilar = $this->db->order_by('kullanici_adi', 'ASC')->where("kullanici_aktif",1)->or_where(["kullanici_departman_id"=>12,"kullanici_departman_id"=>17])
         ->join('departmanlar', 'departmanlar.departman_id = kullanicilar.kullanici_departman_id')
         ->join('kullanici_gruplari', 'kullanici_gruplari.kullanici_grup_id = kullanicilar.kullanici_grup_no')
         ->get("kullanicilar")->result();
