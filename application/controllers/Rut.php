572907254420d32ecec7b21d7bee7f2b963a3476
ugyazilim <ugyazilim@gmail.com>
2025-02-11 17:29:40 +0300

    asf

--- application/controllers/Rut.php
+++ application/controllers/Rut.php
@@ -102,42 +102,6 @@ class Rut extends CI_Controller {
 
 
 
-   $rkul = $this->db->where("kullanici_id",$this->input->post("kullanici_id"))->get("kullanicilar")->result()[0];
-   $rarac = $this->db->where("arac_id",$arac_id)->get("araclar")->result()[0];
-
-   $rilce = "";
-   if(json_encode($this->input->post("rut_ilce_id")) != "[]" && json_encode($this->input->post("rut_ilce_id")) != "null" && json_encode($this->input->post("rut_ilce_id")) != null) {
-
-	 
-	$ilcelers = json_decode(json_encode($this->input->post("rut_ilce_id")));
-	$totalIlceler = count($ilcelers);
-
-	foreach ($ilcelers as $key => $secilen_ilce) {
-	
-	  foreach ($ilceler as $ilce) {
-	   if($ilce->ilce_id == $secilen_ilce){
-		$rilce .= $ilce->ilce_adi;
-	   }
-	  }
-	  $count++;
-	if ($key != $totalIlceler - 1) {
-		$rilce .= ", ";
-	  }
-  }
- 
-
-  $ril = $this->db->where("sehir_id",$this->input->post("sehir_id"))->get("sehirler")->result()[0];
-   
-
-  echo "Sn. $rkul->kullanici_ad_soyad, size yeni rut tanımlanması yapılmıştır. Rut detayları aşağıda yer almaktadır:\n\n
-  
-  Başlangıç : ".date('Y-m-d',strtotime($this->input->post('rut_baslangic_tarihi')))
-  ."\nBitiş : ".date('Y-m-d',strtotime($this->input->post('rut_bitis_tarihi')))
-  ."Araç : $rarac->arac_plaka\n".
-  "İl - İlçe : ".$rilce ."/". $ril->sehir_adi
-  
-  ;
-}
 
 		redirect(base_url("rut/form/".$this->input->post("sehir_id")));
 	}
