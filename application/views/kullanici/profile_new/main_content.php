6fd15f5e29318bda2b215b2cd15cfd4e23bb465f
ugyazilim <ugyazilim@gmail.com>
2025-02-17 15:53:23 +0300

    asf

--- application/views/kullanici/profile_new/main_content.php
+++ application/views/kullanici/profile_new/main_content.php
@@ -2,6 +2,11 @@
     <section class="content text-md">
 
     <div class="row">
+
+    <div class="col" style="background:white;height:50px" >
+        
+    </div>
+
         <div class="col" style="max-width:350px">
         <div class="card" style="background:white;">
 asfasf
