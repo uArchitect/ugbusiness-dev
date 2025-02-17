e8e6b196c5f90b56ae5605f9de4bbee4f0a73087
ugyazilim <ugyazilim@gmail.com>
2025-02-17 15:58:53 +0300

    asf

--- application/views/kullanici/profile_new/main_content.php
+++ application/views/kullanici/profile_new/main_content.php
@@ -1,21 +1,6 @@
-<div class="content-wrapper" style="min-height: 1604.44px;">
+<div class="content-wrapper"  >
     <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Profile</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">User Profile</li>
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
-
+     
     <!-- Main content -->
     <section class="content">
       <div class="container-fluid">
