89723af4e92f65e2dc63b4c5b84831983cdf7a14
ugyazilim <ugyazilim@gmail.com>
2025-02-17 14:31:53 +0300

    asf

--- application/models/Cihaz_model.php
+++ application/models/Cihaz_model.php
@@ -179,6 +179,8 @@ return $query->result();
     if($sehir_id != 0){
       $this->db->where(["sehirler.sehir_id"=>$sehir_id]);
     }
+    if($urun_id != 0){
+      
     $query = $this->db
     ->select("sehirler.*,count(*) as toplam")
     ->order_by('toplam', 'desc')
@@ -188,6 +190,17 @@ return $query->result();
     ->join("sehirler","sehirler.sehir_id = merkezler.merkez_il_id","left")
     ->group_by("sehirler.sehir_adi,urunler.urun_adi")
     ->get("siparis_urunleri");
+    }else{
+      $query = $this->db
+      ->select("sehirler.*,count(*) as toplam")
+      ->order_by('toplam', 'desc')
+      ->join("urunler","urunler.urun_id = siparis_urunleri.urun_no")
+      ->join("siparisler","siparisler.siparis_id = siparis_urunleri.siparis_kodu")
+      ->join("merkezler","merkezler.merkez_id = siparisler.merkez_no")
+      ->join("sehirler","sehirler.sehir_id = merkezler.merkez_il_id","left")
+      ->group_by("sehirler.sehir_adi")
+      ->get("siparis_urunleri");
+    }
 
 return $query->result();
 	}
