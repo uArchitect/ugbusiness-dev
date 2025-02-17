e3c2f7b1856fa45894250edc8535738bbfc1d90b
ugyazilim <ugyazilim@gmail.com>
2025-02-17 15:54:38 +0300

    asf

--- application/views/kullanici/profile_new/main_content.php
+++ application/views/kullanici/profile_new/main_content.php
@@ -3,16 +3,18 @@
 
     <div class="row">
 
-    <div class="col-12" style="background:white;height:50px" >
-        
-    </div>
+    
 
         <div class="col" style="max-width:350px">
         <div class="card" style="background:white;">
 asfasf
         </div>
         </div>
-        <div class="col"></div>
+        <div class="col">
+        <div class="col-12" style="background:white;height:50px" >
+        
+        </div>
+        </div>
     </div>
 
 
