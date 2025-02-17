34700c3269b7deb930986f73d08a37370d4051ce
ugyazilim <ugyazilim@gmail.com>
2025-02-17 16:28:04 +0300

    asf

--- application/views/kullanici/profile_new/main_content.php
+++ application/views/kullanici/profile_new/main_content.php
@@ -107,7 +107,7 @@
                   <div class="tab-pane active" id="activity">
 
                     <!-- Post -->
-                    <div class="col-lg-4 col-6" onclick="marka_model_guncelle();" style="cursor:pointer;padding: 0px;padding-left: 2px;">
+                    <div class="col-lg-12 col-6" onclick="marka_model_guncelle();" style="cursor:pointer;padding: 0px;padding-left: 2px;">
             <div class="small-box bg-success" style="    height: 99%; margin-bottom: 2px !important;border-radius: 0px;background: #f3f0f1!important;">
               <div class="inner">
                 <h3>FIAT FIORINO                </h3>
