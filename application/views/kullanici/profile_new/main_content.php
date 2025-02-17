f1d09a286f3e38aec18583f2c17a8a45a553bc78
ugyazilim <ugyazilim@gmail.com>
2025-02-17 15:56:38 +0300

    asf

--- application/views/kullanici/profile_new/main_content.php
+++ application/views/kullanici/profile_new/main_content.php
@@ -1,17 +1,17 @@
 <div class="content-wrapper"   style="background:#e2e8f0;">
-    <section class="content text-md">
+    <section class="content text-md p-0">
 
     <div class="row">
 
     
 
-        <div class="col" style="max-width:350px">
-        <div  style="background:white;">
-asfasf
-        </div>
+        <div class="col" style="max-width:350px;    padding: 2px;">
+            <div  style="background:white;">
+            asfasf
+            </div>
         </div>
         <div class="col">
-        <div class="col-12" style="background:white;height:50px" >
+        <div class="col-12" style="background:white;height:50px;padding: 2px;" >
         
         </div>
         </div>
