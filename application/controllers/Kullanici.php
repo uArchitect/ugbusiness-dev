cdf5c4c506f64da431a0ff828ec818bcd7fc8de6
ugyazilim <ugyazilim@gmail.com>
2025-02-18 13:23:50 +0300

    asf

--- application/controllers/Kullanici.php
+++ application/controllers/Kullanici.php
@@ -571,16 +571,21 @@ $query = $this->db->query($sql);
 
 public function profil_new($kullanici_id){
     
+    $filter = $_GET["subpage"];
+    if($filter == "arac-bilgisi"){
+        $arac = $this->db->where("arac_surucu_id",$kullanici_id)->get("araclar")->result();
+        $viewData["data_arac"] = (count(arac)>0 ? $arac[0] : null);
+        $viewData["driverdata"] = get_arvento_arac_detay(); 
+        $viewData["data_kullanici"] = get_yonlendiren_kullanici($kullanici_id); 
+        $viewData["page"] = "kullanici/profile_new";
+        $viewData["subpage"] = "kullanici/profile_new/arac_bilgi";
+        $this->load->view('base_view',$viewData);
+    }
+    
 
 
 
-    $arac = $this->db->where("arac_surucu_id",$kullanici_id)->get("araclar")->result();
-    $viewData["data_arac"] = (count(arac)>0 ? $arac[0] : null);
-    $viewData["driverdata"] = get_arvento_arac_detay(); 
-    $viewData["data_kullanici"] = get_yonlendiren_kullanici($kullanici_id); 
-    $viewData["page"] = "kullanici/profile_new";
-    $viewData["subpage"] = "kullanici/profile_new/arac_bilgi";
-    $this->load->view('base_view',$viewData);
+    
 
 }
 
