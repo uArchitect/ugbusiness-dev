7928beadfc4d4216a8db3e5f25e15b06042b8c1a
ugyazilim <ugyazilim@gmail.com>
2025-02-18 16:16:16 +0300

    asf

--- application/views/kullanici/profile_new/ozluk_dosyasi.php
+++ application/views/kullanici/profile_new/ozluk_dosyasi.php
@@ -8,7 +8,7 @@
                 <li><a href="#work-experience"><i class="fas fa-briefcase"></i> İş Deneyimi</a></li>
                 <li><a href="#health-info"><i class="fas fa-heartbeat"></i> Sağlık Bilgileri</a></li>
                 <li><a href="#health-info"><i class="fas fa-certificate"></i> Sertifika & Eğitim Bilgileri</a></li>
-                <li><a href="#health-info"><i class="fas fa-certificate"></i> Dil Bilgisi</a></li>
+                <li><a href="#health-info"><i class="fas fa-globe"></i> Dil Bilgisi</a></li>
             </ul>
         </div>
     </div>
