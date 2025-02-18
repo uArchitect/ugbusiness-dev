d9ac07b224a91926611bb745b074993c72487642
ugyazilim <ugyazilim@gmail.com>
2025-02-18 15:40:04 +0300

    asf

--- application/views/kullanici/profile_new/ozluk_dosyasi.php
+++ application/views/kullanici/profile_new/ozluk_dosyasi.php
@@ -39,6 +39,10 @@
                     <td><?=date("d.m.Y",strtotime($data_kullanici->kullanici_dogum_tarihi))?></td>
                 </tr>
                 <tr>
+                    <th>Medeni Durum</th>
+                    <td><?=$data_kullanici->kullanici_medeni_durum == 0 ? "BİLİNMİYOR" : ($data_kullanici->kullanici_medeni_durum == 1 ? "EVLİ" : "BEKAR")?></td>
+                </tr>
+                <tr>
                     <th>Adres</th>
                     <td><?=$data_kullanici->kullanici_adres?></td>
                 </tr>
@@ -46,10 +50,7 @@
                     <th>Uyruk</th>
                     <td><?=$data_kullanici->kullanici_uyruk?></td>
                 </tr>
-                <tr>
-                    <th>Medeni Durum</th>
-                    <td><?=$data_kullanici->kullanici_medeni_durum == 0 ? "BİLİNMİYOR" : ($data_kullanici->kullanici_medeni_durum == 1 ? "EVLİ" : "BEKAR")?></td>
-                </tr>
+               
             </table>
         </section>
 
