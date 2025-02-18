363ae862f4e411ce2e2969a690fad8ec6a8f081c
ugyazilim <ugyazilim@gmail.com>
2025-02-18 13:21:30 +0300

    asf

--- application/controllers/Kullanici.php
+++ application/controllers/Kullanici.php
@@ -577,7 +577,7 @@ public function profil_new($kullanici_id){
 
     $viewData["data_arac"] = $this->db->where("arac_surucu_id",$kullanici_id)->get("araclar")->result()[0];
     $viewData["driverdata"] = get_arvento_arac_detay(); 
-    $viewData["data_kullanici"] = get_yonlendiren_kullanici($kullanici_id)[0]; 
+    $viewData["data_kullanici"] = get_yonlendiren_kullanici($kullanici_id); 
     $viewData["page"] = "kullanici/profile_new";
     $viewData["subpage"] = "kullanici/profile_new/arac_bilgi";
     $this->load->view('base_view',$viewData);
