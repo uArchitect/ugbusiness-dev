aa2a556a7135b2399f7d9be4a0d00790b13bd98d
ugyazilim <ugyazilim@gmail.com>
2025-02-18 11:33:45 +0300

    asf

--- application/views/kullanici/profile_new/main_content.php
+++ application/views/kullanici/profile_new/main_content.php
@@ -5,7 +5,7 @@
      
     <!-- Main content -->
     <section class="content">
-      <div class="container-fluid">
+      <div class="container-fluid" style="padding:0px!important;">
         <div class="row">
           <div class="col-md-3">
 
