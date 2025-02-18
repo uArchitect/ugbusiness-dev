d1a77f6d5ac4c44fe1015234770257771f2e99d5
ugyazilim <ugyazilim@gmail.com>
2025-02-18 11:49:15 +0300

    asf

--- application/views/kullanici/profile_new/main_content.php
+++ application/views/kullanici/profile_new/main_content.php
@@ -41,7 +41,7 @@
             <!-- About Me Box -->
             <div class="card card-primary">
               <div class="card-header">
-                <h3 class="card-title">Kişisel Bilgiler</h3>
+                <h3 class="card-title">Kullanıcı Hakkında Genel Bilgiler </h3>
               </div>
               <!-- /.card-header -->
               <div class="card-body">
