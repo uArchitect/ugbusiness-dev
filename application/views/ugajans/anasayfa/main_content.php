8c2970654e9368c24a12860f7331890206f9b083
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-02-20 14:36:08 +0300

    asf

--- application/views/ugajans/anasayfa/main_content.php
+++ application/views/ugajans/anasayfa/main_content.php
@@ -144,6 +144,8 @@
       <div class="menu-item"> 
         <h3> <svg aria-label="currently running: " width="17px" height="17px" fill="none" viewBox="0 0 16 16" class="anim-rotate"  xmlns="http://www.w3.org/2000/svg"> <path fill="none" stroke="#DBAB0A" stroke-width="2" d="M3.05 3.05a7 7 0 1 1 9.9 9.9 7 7 0 0 1-9.9-9.9Z" opacity=".5"></path> <path fill="#eda705" fill-rule="evenodd" d="M8 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Z" clip-rule="evenodd"></path> <path fill="#eda705" d="M14 8a6 6 0 0 0-6-6V0a8 8 0 0 1 8 8h-2Z"></path> </svg> Bekleyen Görev ( Gamze Oranbaş )</h3>
         <p>Bu görev 20.02.2025 14:34 (1 saat önce) tarihinde Gamze Oranbaş tarafından oluşturulmuştur.</p>
+        <p>Görev Detayları</p>
+        <p>Bu görev 20.02.2025 14:34 (1 saat önce) tarihinde Gamze Oranbaş tarafından oluşturulmuştur.</p>
 
         
  <div class="d-flex">
