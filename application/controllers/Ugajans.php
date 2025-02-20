fc01acb14fc2541aa2b419efb2377c60628aa93c
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-02-20 15:55:53 +0300

    asf

--- application/controllers/Ugajans.php
+++ application/controllers/Ugajans.php
@@ -12,6 +12,15 @@ class Ugajans extends CI_Controller {
 	{   
         $this->load->model('Yemek_model');
 		$viewData["yemek"] = $this->Yemek_model->get_by_id(date("d"))[0];
+
+
+        $gorev_filter = (!isset($_GET["gorev_filter"]) ? "1" : $_GET["gorev_filter"] );
+        $viewData["gorevler"] = $this->db->where("gorev_durum",$gorev_filter)->get("ug_ajans_gorevler")->result();
+
+        $viewData["yemek"] = $this->Yemek_model->get_by_id(date("d"))[0];
+
+
+
         $viewData["page"] = "ugajans/anasayfa";
         $this->load->view('ug_ajans_base_view',$viewData);
  
