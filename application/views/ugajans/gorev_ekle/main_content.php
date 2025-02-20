d46e86a59dd7983382ab513eecb6053ff6438899
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-02-20 16:16:12 +0300

    asf

--- application/views/ugajans/gorev_ekle/main_content.php
+++ application/views/ugajans/gorev_ekle/main_content.php
@@ -135,7 +135,7 @@
     
     <div class="menu">
       <div class="menu-item"> 
-        <h3> <svg aria-label="currently running: " width="17px" height="17px" fill="none" viewBox="0 0 16 16" class="anim-rotate"  xmlns="http://www.w3.org/2000/svg"> <path fill="none" stroke="#DBAB0A" stroke-width="2" d="M3.05 3.05a7 7 0 1 1 9.9 9.9 7 7 0 0 1-9.9-9.9Z" opacity=".5"></path> <path fill="#eda705" fill-rule="evenodd" d="M8 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Z" clip-rule="evenodd"></path> <path fill="#eda705" d="M14 8a6 6 0 0 0-6-6V0a8 8 0 0 1 8 8h-2Z"></path> </svg> Yeni Görev Ekleme Formu</h3>
+        <h3>   Yeni Görev Ekleme Formu</h3>
         <p style="text-align:left;">
         <span style="opacity:0.8!important;"><b> <i class="fas fa-arrow-circle-right text-warning"></i> Görev Tanımlanacak Kullanıcı</b> </span> <br>
  
