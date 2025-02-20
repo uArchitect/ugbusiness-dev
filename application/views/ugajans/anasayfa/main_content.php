052b84b88bedd7644996d58d3facb90129dd8841
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-02-20 15:33:40 +0300

    saf

--- application/views/ugajans/anasayfa/main_content.php
+++ application/views/ugajans/anasayfa/main_content.php
@@ -156,12 +156,12 @@
     
  <div class="d-flex" style="margin-bottom: 10px;">
  <a class="btn mobile-nav-btn btn-logout d-block d-lg-none" style="border: 1px dashed #028c1c;/* border-left: 0px; *//* border-top: 0; */flex:1;background: #202034;color: #02a102;font-size: 14px!important;!importan;!importa;!import;!impor;!impo;!imp;!im;!i;!;!ş;!;" href="https://ugbusiness.com.tr/logout">
-   <i class="fa fa-eye-circle"></i>  Yeni Görev Tanımla
+   <i class="fa fa-plus-circle"></i>  Yeni Görev Tanımla
 </a>
  </div>
  <div class="d-flex" style="margin-bottom: 10px;">
  <a class="btn mobile-nav-btn btn-logout d-block d-lg-none" style="border: 1px dashed rgb(140, 2, 2);/* border-left: 0px; *//* border-top: 0; */flex:1;background: #202034;color:rgb(161, 2, 2);font-size: 14px!important;!importan;!importa;!import;!impor;!impo;!imp;!im;!i;!;!ş;!;" href="https://ugbusiness.com.tr/logout">
-   <i class="fa fa-plus-circle"></i>  İptal Edilen Görevleri Görüntüle
+   <i class="fa fa-eye"></i>  İptal Edilen Görevleri Görüntüle
 </a>
  </div>
       <section class="menu-section" style="    margin-top: 5px;">
