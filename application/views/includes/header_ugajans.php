7987f727c96204dd8a837b4e5ced8c8869a34b98
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-02-20 17:26:03 +0300

    asf

--- application/views/includes/header_ugajans.php
+++ application/views/includes/header_ugajans.php
@@ -64,15 +64,15 @@
     <i class="fa fa-user-circle"></i> <?=aktif_kullanici()->kullanici_ad_soyad?> / <?=aktif_kullanici()->kullanici_unvan?>
 </div>
  <div class="d-flex">
- <a class="btn mobile-nav-btn btn-logout d-block " style="border: 1px solid #3a3a7f;border-left: 0px;border-top: 0;flex:1;background:#252547" href="https://ugbusiness.com.tr/ugajans">
+ <a class="btn mobile-nav-btn   d-block " style="border: 1px solid #3a3a7f;border-left: 0px;border-top: 0;flex:1;background:#252547" href="https://ugbusiness.com.tr/ugajans">
     <i class="fas fa-home"></i> Anasayfa
 </a>
 
-<a class="btn mobile-nav-btn btn-logout d-block " style="border: 1px solid #3a3a7f;border-left: 0px;border-top: 0;flex:1;background:#252547" style="flex:1;background:#252547" href="https://ugbusiness.com.tr/ugajans/rehber">
+<a class="btn mobile-nav-btn  d-block " style="border: 1px solid #3a3a7f;border-left: 0px;border-top: 0;flex:1;background:#252547" style="flex:1;background:#252547" href="https://ugbusiness.com.tr/ugajans/rehber">
     <i class="fas fa-users"></i> Rehber
 </a>
 
-<a class="btn mobile-nav-btn btn-logout d-block " style="border: 1px solid #3a3a7f;border-left: 0px;border-top: 0;flex:1;background:rgb(138, 14, 14)" style="flex:1;" href="https://ugbusiness.com.tr/logout">
+<a class="btn mobile-nav-btn   d-block " style="border: 1px solid #3a3a7f;border-left: 0px;border-top: 0;flex:1;background:rgb(138, 14, 14)" style="flex:1;" href="https://ugbusiness.com.tr/logout">
     <i class="fas fa-sign-out-alt"></i> Çıkış
 </a>
  </div>
