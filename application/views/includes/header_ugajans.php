d371f1adf3a1da4164325ecb0617e5c0cad1574f
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-02-20 13:23:28 +0300

    asf

--- application/views/includes/header_ugajans.php
+++ application/views/includes/header_ugajans.php
@@ -54,76 +54,7 @@
 
 
 
-
-
- 
-<?php 
-if(aktif_kullanici()->kullanici_id == 4){
-  ?>
-  <a href="https://ugbusiness.com.tr/talep/talep_hizli_yonlendirme_save_view" class="btn btn-warning">Hızlı Talep Oluştur</a>
-  <?php
-}
-?>
-    <!-- Left navbar links -->
-    <ul class="navbar-nav  col-md-6">
-      <li class="nav-item" style="display: flex;">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars text-white"></i></a>
-
-
-
-        
-        
-
-
-        <?php 
-        
-        if(get_talep_uyari()){
-          ?>
-          <a href="<?=base_url("talep/bekleyen_rapor_list")?>" class="btn btn-danger text-white yanipsonenyazinew mr-2">Bekleyen Talep Uyarısı</a>
-          <?php
-        }
-        ?>
- 
-
-<?php 
-if(aktif_kullanici()->kullanici_id == 9 || aktif_kullanici()->kullanici_id == 7 || aktif_kullanici()->kullanici_id == 8 || aktif_kullanici()->kullanici_id == 37){
- 
-  if(kritik_stok_varmi()){
-    ?>
-    <a href="<?=base_url("api/kritik_stoklar")?>" class="btn btn-danger text-white yanipsonenyazinew mr-2">Kritik Stok Uyarısı</a>
-    <?php
-  } 
-  if(get_arac_bildirim()){
-    ?>
-    <a href="<?=base_url("arac")?>" class="btn btn-danger text-white yanipsonenyazinew">Kasko Sigorta Uyarısı</a>
-    <?php
-  }else{
-
   
-
-        for ($i=1; $i < 15; $i++) { 
-          if($i == 9 || $i == 10 || $i == 11){
-            continue;
-          }
-          $kmlastdata2 = get_arac_km_son_kayit($i);
-          if($kmlastdata2){
-            $gun2 = gunSayisiHesapla(date("d.m.Y"),date("d.m.Y",strtotime($kmlastdata2->arac_km_kayit_tarihi)));
-            if($gun2 >= 7){
-             ?>
-             <a href="<?=base_url("arac")?>" class="btn btn-danger text-white yanipsonenyazis2">Km Giriş Uyarısı</a>
-             <?php
-             break;
-            }  
-          }
-        
-        }
-      }
-    }
-        ?>
-      </li>
-      
-       
-    </ul>
    
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto   d-none d-sm-flex">
