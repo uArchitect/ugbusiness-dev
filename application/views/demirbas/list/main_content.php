03fe9f3e96c8f8f36ea2f00098214ee6058bcc55
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-02-24 10:59:14 +0300

    asf

--- application/views/demirbas/list/main_content.php
+++ application/views/demirbas/list/main_content.php
@@ -4,9 +4,13 @@
     <div class="card card-dark" style="border-radius:0px !important;">
       <div class="card-header">
         <h3 class="card-title"><strong>UG Business</strong> - Parametreler - Demirbaş Yönetimi</h3>
-        <a href="<?=base_url("demirbas/ekle/1")?>" type="button" class="btn btn-primary btn-sm" style="float: right!important;padding: 0px;padding-left: 5px;padding-right: 5px;"><i class="fa fa-plus" style="font-size:12px" aria-hidden="true"></i> Yeni Kayıt Ekle</a>
+        <a href="<?=base_url("demirbas/ekle/1")?>" type="button" class="btn btn-primary btn-sm" style="float: right!important;padding: 0px;padding-left: 5px;padding-right: 5px;">
+          <i class="fa fa-plus" style="font-size:12px" aria-hidden="true"></i> Yeni Kayıt Ekle
+        </a>
         <?php if(!empty($kategori_kontrol)) { ?>
-          <a href="<?=base_url("demirbas")?>" type="button" class="btn btn-danger btn-sm mr-2" style="float: right!important;padding: 0px;padding-left: 5px;padding-right: 5px;"><i class="fa fa-times text-white" style="font-size:12px" aria-hidden="true"></i> Filtrelemeyi kaldır, tüm kayıtları göster</a>
+          <a href="<?=base_url("demirbas")?>" type="button" class="btn btn-danger btn-sm mr-2" style="float: right!important;padding: 0px;padding-left: 5px;padding-right: 5px;">
+            <i class="fa fa-times text-white" style="font-size:12px" aria-hidden="true"></i> Filtrelemeyi kaldır, tüm kayıtları göster
+          </a>
         <?php } ?>
       </div>
       <!-- /.card-header -->
@@ -14,7 +18,7 @@
         <table id="example1" class="table table-bordered table-striped text-sm">
           <thead>
             <tr>
-              <th style="width: 42px;">Kod</th> 
+              <th style="width: 42px;">Kod</th>
               <th>Envanter Bilgisi</th>
               <th>Envanter Kullanıcısı</th>
               <th style="width: 130px;">Kayıt Tarihi</th>
@@ -22,16 +26,29 @@
             </tr>
           </thead>
           <tbody>
-            <?php $count=0; foreach ($demirbaslar as $demirbas) : ?>
+            <?php foreach ($demirbaslar as $demirbas): ?>
               <tr>
                 <td>
                   <?php
-                    // Your image logic here for different categories
+                    // Kategoriye göre resim gösterimi
+                    if($demirbas->kategori_id == 1) {
+                        echo '<img style="width:40px" src="https://m.media-amazon.com/images/I/71s72QE+voL.jpg">';
+                    } elseif($demirbas->kategori_id == 2) {
+                        echo '<img style="width:40px" src="https://cdn.vatanbilgisayar.com/Upload/PRODUCT/lenovo/thumb/147559-1_large.jpg">';
+                    } elseif($demirbas->kategori_id == 3) {
+                        echo '<img style="width:40px" src="https://yemekkarti.co/sites/yemekkarti.co/files/inline-images/MN_dikey_erkek.png">';
+                    } elseif($demirbas->kategori_id == 4) {
+                        echo '<img style="width:40px" src="https://cdn.qukasoft.com/f/752658/bzR6WmFtNG0vcUp3ZUdGdEg4MXZKZWxESUE9PQ/p/intel-i3-4n-8gb-120gb-ssd-19-mon-masaustu-bilgisayar-195154728-sw1000sh1000.webp">';
+                    }
                   ?>
-                </td> 
-                <td> 
-                  <?=$demirbas->demirbas_adi?> 
-                  <span style="margin-top:9px" class="d-block"><?=$demirbas->demirbas_marka?></span>
+                </td>
+                <td>
+                  <?=$demirbas->demirbas_adi?>
+                  <?php if($demirbas->kategori_id == 3): ?>
+                    <span style="margin-top:9px" class="d-block">Multinet Kart</span>
+                  <?php else: ?>
+                    <span style="margin-top:9px" class="d-block"><?=$demirbas->demirbas_marka?></span>
+                  <?php endif; ?>
                 </td>
                 <td>
                   <span style="margin-top:9px;display:block">
@@ -41,25 +58,41 @@
                 <td>
                   <span style="margin-top:9px;display:block">
                     <i class="far fa-calendar-plus" style="margin-right:5px;opacity:1"></i>
-                    <?=date('d.m.Y H:i',strtotime($demirbas->demirbas_kayit_tarihi));?>
+                    <?=date('d.m.Y H:i', strtotime($demirbas->demirbas_kayit_tarihi));?>
                   </span>
                 </td>
                 <td>
-                  <a href="<?=site_url("demirbas/duzenle/$demirbas->demirbas_id")?>" type="button" class="btn btn-warning btn-xs"><i class="fa fa-pen" style="font-size:12px" aria-hidden="true"></i> Düzenle</a>
-                  <a type="button" onclick="confirm_action('Silme İşlemini Onayla','Seçilen bu kaydı silmek istediğinize emin misiniz ? Bu işlem geri alınamaz.','Onayla','<?=base_url('demirbas/sil/').$demirbas->demirbas_id?>');" class="btn btn-danger btn-xs"><i class="fa fa-times" style="font-size:12px" aria-hidden="true"></i> Kayıt Sil</a>
-                  <!-- Add button to toggle inventory list visibility -->
+                  <a href="<?=site_url("demirbas/duzenle/$demirbas->demirbas_id")?>" type="button" class="btn btn-warning btn-xs">
+                    <i class="fa fa-pen" style="font-size:12px" aria-hidden="true"></i> Düzenle
+                  </a>
+                  <a type="button" onclick="confirm_action('Silme İşlemini Onayla', 'Seçilen bu kaydı silmek istediğinize emin misiniz ? Bu işlem geri alınamaz.', 'Onayla', '<?=base_url('demirbas/sil/').$demirbas->demirbas_id?>');" class="btn btn-danger btn-xs">
+                    <i class="fa fa-times" style="font-size:12px" aria-hidden="true"></i> Kayıt Sil
+                  </a>
+                  <!-- Buton ile envanter detaylarını göster/gizle -->
                   <button class="btn btn-info btn-xs" onclick="toggleInventory(<?=$demirbas->demirbas_id?>)">
                     <i class="fa fa-plus"></i> Envanteri Göster
                   </button>
                 </td>
               </tr>
-              <!-- Expandable row to show the inventory -->
+              <!-- Kullanıcıya ait envanterleri filtreleyerek listeleme -->
               <tr id="inventory-row-<?=$demirbas->demirbas_id?>" style="display:none;">
                 <td colspan="5">
-                  <!-- Here, display the inventory details related to the user -->
+                  <?php 
+                    // Eğer her $demirbas için doğrudan ilgili envanterler gelmiyorsa,
+                    // tüm envanter listesinden kullanıcıya ait olanları filtreleyin.
+                    // Aşağıdaki örnekte $allInventories dizisinden filtreleme yapılmaktadır.
+                    // Eğer zaten $demirbas->inventory mevcutsa, bunu kullanın.
+                    if(isset($demirbas->inventory)) {
+                      $userInventories = $demirbas->inventory;
+                    } else {
+                      // Örnek filtreleme: $allInventories tüm envanterleri içeriyor
+                      $userInventories = array_filter($allInventories, function($item) use ($demirbas) {
+                          return $item->kullanici_id == $demirbas->kullanici_id;
+                      });
+                    }
+                  ?>
                   <ul>
-                    <!-- Loop through inventory items assigned to this user -->
-                    <?php foreach ($demirbas->inventory as $item) : ?>
+                    <?php foreach ($userInventories as $item): ?>
                       <li><?=$item->item_name?> - <?=$item->quantity?></li>
                     <?php endforeach; ?>
                   </ul>
@@ -76,10 +109,10 @@
 </div>
 
 <script>
-  // Function to toggle inventory visibility
+  // JavaScript fonksiyonu: Envanter satırını göster/gizle
   function toggleInventory(demirbasId) {
     var row = document.getElementById('inventory-row-' + demirbasId);
-    if (row.style.display === "none") {
+    if (row.style.display === "none" || row.style.display === "") {
       row.style.display = "table-row";
     } else {
       row.style.display = "none";
