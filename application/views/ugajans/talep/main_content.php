2ba9e9baa3c9ae531422b46dca20359d42de9200
ugyazilim <ugyazilim@gmail.com>
2025-02-25 14:11:23 +0300

    sdg

--- application/views/ugajans/talep/main_content.php
+++ application/views/ugajans/talep/main_content.php
@@ -231,7 +231,8 @@ foreach ($talepler as $talep) :
   <span style="opacity:0.8!important;"><span style="    color: #07ed07;"> <i class="fa fa-check"></i> Talep Sonlandırma Notu</span> </span>
   <br>
   <textarea name="ugajans_talep_sonlandirma_notu" class="form-control" placeholder="Bu bölüme talep ile ilgili sonlandırma veya görüşme notunuzu girebilirsiniz.." id="" style="
-   background: #24243c; border: 1px solid #003671;
+    background: #24243c; border: 1px solid #003671; color:white;
+   
     margin-top: 5px;
 "><?=$talep->ugajans_talep_sonlandirma_notu?></textarea> 
 <br>
