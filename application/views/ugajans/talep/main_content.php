08bdf5829363ea9a075351a168642bf639f56344
ugyazilim <ugyazilim@gmail.com>
2025-02-25 14:14:46 +0300

    asf

--- application/views/ugajans/talep/main_content.php
+++ application/views/ugajans/talep/main_content.php
@@ -211,24 +211,24 @@ foreach ($talepler as $talep) :
 
 
   <div class="row">
-    <div class="col">
+    <div class="col-md-3">
     <a class="btn mobile-nav-btn btn-logout d-block p-3" style="background: #24243c; border: 1px solid #003671;border-left: 2px;flex:1; " href="https://ugbusiness.com.tr/ugajans/talep?talep_filter=1">
  <span style="opacity:0.8!important;"><b> <i class="fas fa-user text-warning" style="display: block; margin-bottom: 7px;"></i> Ad Soyad : </b> <?=$talep->ugajans_talep_ad_soyad?> </span>
 </a>
     </div>
-    <div class="col">
+    <div class="col-md-3">
       
 <a class="btn mobile-nav-btn btn-logout d-block p-3 " style="background: #24243c; border: 1px solid #003671;border-left: 0px;flex:1;  " href="https://ugbusiness.com.tr/ugajans/talep?talep_filter=2">
 <span style="opacity:0.8!important;"><b> <i class="fas fa-phone text-warning" style="display: block; margin-bottom: 7px;"></i> İletişim Numarası : </b> <?=$talep->ugajans_talep_iletisim_numarasi?> </span> 
 </a>
     </div>
-    <div class="col">
+    <div class="col-md-3">
       
 <a class="btn mobile-nav-btn btn-logout d-block p-3 " style="background: #24243c; border: 1px solid #003671;border-left: 0px;flex:1; " href="https://ugbusiness.com.tr/ugajans/talep?talep_filter=3">
 <span style="opacity:0.8!important;"><b> <i class="fas fa-envelope text-warning" style="display: block; margin-bottom: 7px;"></i> Email : </b> <?=$talep->ugajans_talep_email_adres?> </span>
 </a>
     </div>
-    <div class="col">
+    <div class="col-md-3">
       
 <a class="btn mobile-nav-btn btn-logout d-block p-3" style="background: #24243c; border: 1px solid #003671;border-left: 0px;flex:1; " href="https://ugbusiness.com.tr/ugajans/talep?talep_filter=4">
 <span style="opacity:0.8!important;"><b> <i class="fas fa-info-circle text-warning" style="display: block; margin-bottom: 7px;"></i> Detay : </b> <?=$talep->ugajans_talep_detay?> </span>
