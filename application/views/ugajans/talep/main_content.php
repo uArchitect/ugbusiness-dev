fbb407f3258f19024420ba5a7b7e1aa06a1b8a8d
ugyazilim <ugyazilim@gmail.com>
2025-02-25 14:08:50 +0300

    asf

--- application/views/ugajans/talep/main_content.php
+++ application/views/ugajans/talep/main_content.php
@@ -212,17 +212,17 @@ foreach ($talepler as $talep) :
 
 
 
-  <div class="d-flex" style="margin-bottom: -8px!important;margin-top: -18px!important;border-left: 1px solid #1f62bf;">
- <a class="btn mobile-nav-btn btn-logout d-block " style="background: #24243c; border: 1px solid #0060c7;border-left: 2px;flex:1; " href="https://ugbusiness.com.tr/ugajans/talep?talep_filter=1">
+  <div class="d-flex" style="margin-bottom: -8px!important;margin-top: -18px!important;border-left: 1px solid #003671;">
+ <a class="btn mobile-nav-btn btn-logout d-block " style="background: #24243c; border: 1px solid #003671;border-left: 2px;flex:1; " href="https://ugbusiness.com.tr/ugajans/talep?talep_filter=1">
  <span style="opacity:0.8!important;"><b> <i class="fas fa-user text-warning"></i> Ad Soyad : </b> <?=$talep->ugajans_talep_ad_soyad?> </span>
 </a>
-<a class="btn mobile-nav-btn btn-logout d-block " style="background: #24243c; border: 1px solid #0060c7;border-left: 0px;flex:1;  " href="https://ugbusiness.com.tr/ugajans/talep?talep_filter=2">
+<a class="btn mobile-nav-btn btn-logout d-block " style="background: #24243c; border: 1px solid #003671;border-left: 0px;flex:1;  " href="https://ugbusiness.com.tr/ugajans/talep?talep_filter=2">
 <span style="opacity:0.8!important;"><b> <i class="fas fa-phone text-warning"></i> İletişim Numarası : </b> <?=$talep->ugajans_talep_iletisim_numarasi?> </span> 
 </a>
-<a class="btn mobile-nav-btn btn-logout d-block " style="background: #24243c; border: 1px solid #0060c7;border-left: 0px;flex:1; " href="https://ugbusiness.com.tr/ugajans/talep?talep_filter=3">
+<a class="btn mobile-nav-btn btn-logout d-block " style="background: #24243c; border: 1px solid #003671;border-left: 0px;flex:1; " href="https://ugbusiness.com.tr/ugajans/talep?talep_filter=3">
 <span style="opacity:0.8!important;"><b> <i class="fas fa-envelope text-warning"></i> Email : </b> <?=$talep->ugajans_talep_email_adres?> </span>
 </a>
-<a class="btn mobile-nav-btn btn-logout d-block" style="background: #24243c; border: 1px solid #0060c7;border-left: 0px;flex:1; " href="https://ugbusiness.com.tr/ugajans/talep?talep_filter=4">
+<a class="btn mobile-nav-btn btn-logout d-block" style="background: #24243c; border: 1px solid #003671;border-left: 0px;flex:1; " href="https://ugbusiness.com.tr/ugajans/talep?talep_filter=4">
 <span style="opacity:0.8!important;"><b> <i class="fas fa-info-circle text-warning"></i> Detay : </b> <?=$talep->ugajans_talep_detay?> </span>
 </a>  
  </div>
