c533d38a7e66528b6d16dd7f20828358ef5a6695
ugyazilim <ugyazilim@gmail.com>
2025-02-25 11:49:30 +0300

    asf

--- application/views/ugajans/talep/main_content.php
+++ application/views/ugajans/talep/main_content.php
@@ -160,21 +160,22 @@ foreach ($talepler as $talep) :
         <h3> <svg style="<?=$talep->ugajans_talep_durum > 1 ? "display:none" : ""?>" aria-label="currently running: " width="17px" height="17px" fill="none" viewBox="0 0 16 16" class="anim-rotate"  xmlns="http://www.w3.org/2000/svg"> <path fill="none" stroke="#DBAB0A" stroke-width="2" d="M3.05 3.05a7 7 0 1 1 9.9 9.9 7 7 0 0 1-9.9-9.9Z" opacity=".5"></path> <path fill="#eda705" fill-rule="evenodd" d="M8 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Z" clip-rule="evenodd"></path> <path fill="#eda705" d="M14 8a6 6 0 0 0-6-6V0a8 8 0 0 1 8 8h-2Z"></path> </svg> 
         
         <?php
+        $durum = "";
         if($talep->ugajans_talep_durum == 1){
-          echo "Bekleyen";
+          $durum = "Bekleyen";
         }
         if($talep->ugajans_talep_durum == 2){
-          echo "İşleme Alınan";
+          $durum = "İşleme Alınan";
         }
         if($talep->ugajans_talep_durum == 3){
-          echo "Tamamlanan";
+          $durum = "Tamamlanan";
         }
         if($talep->ugajans_talep_durum == 4){
-          echo "İptal";
+          $durum = "İptal";
         }
         ?>
         
-        <?=$talep->ugajans_hizmet_adi?> Talebi </h3>
+        <?=$talep->ugajans_hizmet_adi?> Talebi (<?=$durum?>)</h3>
         <p style="text-align:left;">
         <span style="opacity:0.4!important;padding-left:">
         <i class="fa fa-info-circle"></i> Bu talep <?=date("d.m.Y H:i",strtotime($talep->ugajans_talep_kayit_tarihi))?> tarihinde ugajans.com websitesi üzerinden oluşturulmuştur.
