057ba112eb5d928d2024741ef4e6f638685fca47
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-02-26 17:44:39 +0300

    as

--- application/views/kullanici/profile_new/arac_bilgisi.php
+++ application/views/kullanici/profile_new/arac_bilgisi.php
@@ -102,19 +102,19 @@
  
      <b><?=(!empty($bakim_kayitlari) && count($bakim_kayitlari)>0) ? date("d.m.Y",strtotime($bakim_kayitlari[count($bakim_kayitlari)-1]->arac_bakim_baslangic_tarihi)) : "#"?></b>  </span>
                 </div>
-                <div class="info-row">
-                    <i class="fas fa-shield-alt"></i> <strong>Sigorta Tarihi : </strong> <span style="font-size:15px!important;"> 
- 
- <b><?=(!empty($sigorta_kayitlari) && count($sigorta_kayitlari)>0) ? date("d.m.Y",strtotime($sigorta_kayitlari[count($sigorta_kayitlari)-1]->arac_sigorta_baslangic_tarihi)) : "#"?></b>  </span>
-                </div>
-                
+            
             </div>
 
 
 
 
             <div class="vehicle-info">
-                 
+            <div class="info-row">
+                    <i class="fas fa-shield-alt"></i> <strong>Sigorta Tarihi : </strong> <span style="font-size:15px!important;"> 
+ 
+ <b><?=(!empty($sigorta_kayitlari) && count($sigorta_kayitlari)>0) ? date("d.m.Y",strtotime($sigorta_kayitlari[count($sigorta_kayitlari)-1]->arac_sigorta_baslangic_tarihi)) : "#"?></b>  </span>
+                </div>
+                
                 
                 <div class="info-row">
                     <i class="fas fa-tools"></i> <strong>Muayene Tarihi : </strong> <span style="font-size:15px!important;"> 
