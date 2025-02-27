2b087b401f5d886ab8c8e7e9d2475189b02206e2
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-02-27 18:11:11 +0300

    asf

--- application/views/kullanici/profile_new/satis.php
+++ application/views/kullanici/profile_new/satis.php
@@ -122,7 +122,7 @@
                          <?php 
                          $urlcustom = base_url("siparis/report/").urlencode(base64_encode("Gg3TGGUcv29CpA8aUcpwV2KdjCz8aE".$kullanici->siparis_id."Gg3TGGUcv29CpA8aUcpwV2KdjCz8aE"));
                          ?>
-                          
+                          <a href="<?=$urlcustom?>" target="_blank"><?=$kullanici->siparis_kodu?></a>
                       </td>
                        
                       <td>
