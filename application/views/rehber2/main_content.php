fd96ea933f2f54de215798eb980e45b4dd09349d
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-03-03 11:46:35 +0300

    asf

--- application/views/rehber2/main_content.php
+++ application/views/rehber2/main_content.php
@@ -78,6 +78,7 @@
             <div class="details">
               <div class="name text-bold"><?= $kullanici->kullanici_ad_soyad ?></div>
               <div class="job"><?= $kullanici->kullanici_unvan ?></div>
+              <a href="<?=base_url("kullanici/profil_new/$kullanici->kullanici_id?subpage=ozluk-dosyasi")?>" class="btn btn-primary">Profili Görüntüle</a>
             </div>
           </div>
         </div>
