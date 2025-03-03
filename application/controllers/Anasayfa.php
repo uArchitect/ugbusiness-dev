9d1c96c91617461f676837b2b7bd0ba57a11a120
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-03-03 11:04:58 +0300

    asf

--- application/controllers/Anasayfa.php
+++ application/controllers/Anasayfa.php
@@ -222,7 +222,7 @@ echo json_encode($pins);
 		$kullanicilar = $this->db->order_by("kullanicilar.rehber_sira_no","asc")->where("kullanici_departman_id !=",19)->where("kullanici_aktif",1)
         ->join('departmanlar', 'departmanlar.departman_id = kullanicilar.kullanici_departman_id')
         ->join('kullanici_gruplari', 'kullanici_gruplari.kullanici_grup_id = kullanicilar.kullanici_grup_no')
-		 
+		 ->order_by("siralama","asc")
         ->get("kullanicilar")->result();
  
 		$viewData["kullanicilar"] = $kullanicilar;
