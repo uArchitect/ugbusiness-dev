b87e2c23ded2bb21f994f6d9a866722c338b2032
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-03-03 14:26:10 +0300

    asf

--- application/views/rehber2/main_content.php
+++ application/views/rehber2/main_content.php
@@ -59,7 +59,7 @@
   position: absolute;
   top: 5px;
   right: 5px;
-  font-size: 31px;
+  font-size: 16px;
   color:rgb(151, 5, 5);
   opacity: 0; /* Varsayılan olarak görünmez */
   transition: opacity 0.3s ease;
