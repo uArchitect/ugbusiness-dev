e5787fced697718573ea82033a3ebb1515376556
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-03-03 11:51:02 +0300

    asf

--- application/views/kullanici/profile_new/ozluk_dosyasi.php
+++ application/views/kullanici/profile_new/ozluk_dosyasi.php
@@ -11,7 +11,7 @@
                 <li><a href="#health-info"><i class="fas fa-certificate"></i> Sertifika Bilgileri</a></li>
                 <li><a href="#health-info"><i class="fas fa-globe"></i> Dil Bilgisi</a></li>
                 <li><a href="#work-experience"><i class="fas fa-briefcase"></i> İş Deneyimi</a></li>
-                <li><a href="#health-info"><i class="fas fa-heartbeat"></i> Sağlık Bilgileri</a></li>
+                <li><a href="#saglik-bilgileri"><i class="fas fa-heartbeat"></i> Sağlık Bilgileri</a></li>
                 <li><a href="#health-info"  style="color:red!important;"><i  style="color:red!important;" class="fas fa-user-shield"></i> Acil Durum</a></li>
             </ul>
         </div>
