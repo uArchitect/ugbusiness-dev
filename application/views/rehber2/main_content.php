0369afce577fbe78ffbb4ef9fc4cd063ff78aabb
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-03-03 15:48:57 +0300

    asf

--- application/views/rehber2/main_content.php
+++ application/views/rehber2/main_content.php
@@ -117,7 +117,7 @@
               <img style="border: 3px solid #ffffff; outline: 2px solid #393c3721;width:70px;height:70px;border-radius:50%; object-fit:cover" src="<?= $kullanici->kullanici_resim != "" ? base_url("uploads/$kullanici->kullanici_resim") : "https://ugbusiness.com.tr/uploads/1710857373145.jpg" ?>">
             </div>
             <div class="details">
-              <div class="name text-bold"><?= $kullanici->kullanici_ad_soyad ?></div>
+              <a href="<?=base_url("kullanici/profil_new/$kullanici->kullanici_id?subpage=ozluk-dosyasi")?>"><div class="name text-bold"><?= $kullanici->kullanici_ad_soyad ?></div></a>
               <div class="job"><?= $kullanici->kullanici_unvan != "" ? $kullanici->kullanici_unvan : "-" ?></div>
               <a href="<?=base_url("kullanici/profil_new/$kullanici->kullanici_id?subpage=ozluk-dosyasi")?>" class="btn btn-primary mt-2"><i class="fa fa-user"></i> Profili Görüntüle</a>
             </div>
