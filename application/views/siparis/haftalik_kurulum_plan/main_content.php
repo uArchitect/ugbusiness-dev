9e5a631120642eeae5a1e7569d2bd6f836b9799b
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-03-04 17:50:38 +0300

    asf

--- application/views/siparis/haftalik_kurulum_plan/main_content.php
+++ application/views/siparis/haftalik_kurulum_plan/main_content.php
@@ -267,14 +267,14 @@ width: -webkit-fill-available;width: -webkit-fill-available; color: #3b3e41; }
 
           
           <div class="timeline-footer" style="padding: 0px; padding-left: 7px; padding-right: 7px; padding-bottom: 7px;">
-          <a href="<?=base_url("kullanici/profil_new/$value->kullanici_id"."?subpage=ozluk-dosyasi")?>" class="btn btn-outline-primary btn-sm mb-2" style="
+          <a  target="_blank"  href="<?=base_url("kullanici/profil_new/$value->kullanici_id"."?subpage=ozluk-dosyasi")?>" class="btn btn-outline-primary btn-sm mb-2" style="
 width: -webkit-fill-available;width: -webkit-fill-available; color: #3b3e41; }
 "><i class="fa fa-user"></i> <?=$value->kullanici_ad_soyad?></a>
  <a href="<?=base_url('siparis/report/'.urlencode(base64_encode("Gg3TGGUcv29CpA8aUcpwV2KdjCz8aE".$value->siparis_id."Gg3TGGUcv29CpA8aUcpwV2KdjCz8aE")))?>" class="btn btn-outline-primary btn-sm" style="
 width: -webkit-fill-available;width: -webkit-fill-available; color: #3b3e41; }
 "><i class="fas fa-eye"></i> Sipariş Detayı</a>
 
-<a  target="_blank"  href="<?=base_url('siparis/save_kurulum_programlama_view/'.$value->siparis_id)?>" class="btn mt-1 btn-outline-warning btn-sm" style="
+<a  href="<?=base_url('siparis/save_kurulum_programlama_view/'.$value->siparis_id)?>" class="btn mt-1 btn-outline-warning btn-sm" style="
 width: -webkit-fill-available;width: -webkit-fill-available; color: #3b3e41; }
 "><i class="fas fa-pen"></i> Tarih Düzenle</a>
 
