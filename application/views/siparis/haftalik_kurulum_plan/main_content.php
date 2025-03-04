d4d86737901c595d17dd2883f7a76e65afa7f1cc
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-03-04 17:47:24 +0300

    asf

--- application/views/siparis/haftalik_kurulum_plan/main_content.php
+++ application/views/siparis/haftalik_kurulum_plan/main_content.php
@@ -147,7 +147,9 @@ width: -webkit-fill-available;width: -webkit-fill-available; color: #3b3e41; }
 
 
           <div class="timeline-footer" style="padding: 0px; padding-left: 7px; padding-right: 7px; padding-bottom: 7px;">
- 
+          <a href="<?=base_url("kullanici/profil_new/$value->kullanici_id"."?subpage=ozluk-dosyasi")?>" class="btn btn-outline-primary btn-sm" style="
+width: -webkit-fill-available;width: -webkit-fill-available; color: #3b3e41; }
+"><i class="fas fa-eye"></i> <?=$value->kullanici_ad_soyad?></a>
  <a href="<?=base_url('siparis/report/'.urlencode(base64_encode("Gg3TGGUcv29CpA8aUcpwV2KdjCz8aE".$value->siparis_id."Gg3TGGUcv29CpA8aUcpwV2KdjCz8aE")))?>" class="btn btn-outline-primary btn-sm" style="
 width: -webkit-fill-available;width: -webkit-fill-available; color: #3b3e41; }
 "><i class="fas fa-eye"></i> Sipariş Detayı</a>
@@ -204,7 +206,9 @@ width: -webkit-fill-available;width: -webkit-fill-available; color: #3b3e41; }
 
           
           <div class="timeline-footer" style="padding: 0px; padding-left: 7px; padding-right: 7px; padding-bottom: 7px;">
- 
+          <a href="<?=base_url("kullanici/profil_new/$value->kullanici_id"."?subpage=ozluk-dosyasi")?>" class="btn btn-outline-primary btn-sm" style="
+width: -webkit-fill-available;width: -webkit-fill-available; color: #3b3e41; }
+"><i class="fas fa-eye"></i> <?=$value->kullanici_ad_soyad?></a>
  <a href="<?=base_url('siparis/report/'.urlencode(base64_encode("Gg3TGGUcv29CpA8aUcpwV2KdjCz8aE".$value->siparis_id."Gg3TGGUcv29CpA8aUcpwV2KdjCz8aE")))?>" class="btn btn-outline-primary btn-sm" style="
 width: -webkit-fill-available;width: -webkit-fill-available; color: #3b3e41; }
 "><i class="fas fa-eye"></i> Sipariş Detayı</a>
@@ -263,7 +267,9 @@ width: -webkit-fill-available;width: -webkit-fill-available; color: #3b3e41; }
 
           
           <div class="timeline-footer" style="padding: 0px; padding-left: 7px; padding-right: 7px; padding-bottom: 7px;">
- 
+          <a href="<?=base_url("kullanici/profil_new/$value->kullanici_id"."?subpage=ozluk-dosyasi")?>" class="btn btn-outline-primary btn-sm" style="
+width: -webkit-fill-available;width: -webkit-fill-available; color: #3b3e41; }
+"><i class="fas fa-eye"></i> <?=$value->kullanici_ad_soyad?></a>
  <a href="<?=base_url('siparis/report/'.urlencode(base64_encode("Gg3TGGUcv29CpA8aUcpwV2KdjCz8aE".$value->siparis_id."Gg3TGGUcv29CpA8aUcpwV2KdjCz8aE")))?>" class="btn btn-outline-primary btn-sm" style="
 width: -webkit-fill-available;width: -webkit-fill-available; color: #3b3e41; }
 "><i class="fas fa-eye"></i> Sipariş Detayı</a>
@@ -322,7 +328,9 @@ width: -webkit-fill-available;width: -webkit-fill-available; color: #3b3e41; }
 
           
           <div class="timeline-footer" style="padding: 0px; padding-left: 7px; padding-right: 7px; padding-bottom: 7px;">
- 
+          <a href="<?=base_url("kullanici/profil_new/$value->kullanici_id"."?subpage=ozluk-dosyasi")?>" class="btn btn-outline-primary btn-sm" style="
+width: -webkit-fill-available;width: -webkit-fill-available; color: #3b3e41; }
+"><i class="fas fa-eye"></i> <?=$value->kullanici_ad_soyad?></a>
  <a href="<?=base_url('siparis/report/'.urlencode(base64_encode("Gg3TGGUcv29CpA8aUcpwV2KdjCz8aE".$value->siparis_id."Gg3TGGUcv29CpA8aUcpwV2KdjCz8aE")))?>" class="btn btn-outline-primary btn-sm" style="
 width: -webkit-fill-available;width: -webkit-fill-available; color: #3b3e41; }
 "><i class="fas fa-eye"></i> Sipariş Detayı</a>
@@ -382,7 +390,9 @@ width: -webkit-fill-available;width: -webkit-fill-available; color: #3b3e41; }
 
           
           <div class="timeline-footer" style="padding: 0px; padding-left: 7px; padding-right: 7px; padding-bottom: 7px;">
- 
+          <a href="<?=base_url("kullanici/profil_new/$value->kullanici_id"."?subpage=ozluk-dosyasi")?>" class="btn btn-outline-primary btn-sm" style="
+width: -webkit-fill-available;width: -webkit-fill-available; color: #3b3e41; }
+"><i class="fas fa-eye"></i> <?=$value->kullanici_ad_soyad?></a>
  <a href="<?=base_url('siparis/report/'.urlencode(base64_encode("Gg3TGGUcv29CpA8aUcpwV2KdjCz8aE".$value->siparis_id."Gg3TGGUcv29CpA8aUcpwV2KdjCz8aE")))?>" class="btn btn-outline-primary btn-sm" style="
 width: -webkit-fill-available;width: -webkit-fill-available; color: #3b3e41; }
 "><i class="fas fa-eye"></i> Sipariş Detayı</a>
@@ -440,7 +450,9 @@ width: -webkit-fill-available;width: -webkit-fill-available; color: #3b3e41; }
 
 
           <div class="timeline-footer" style="padding: 0px; padding-left: 7px; padding-right: 7px; padding-bottom: 7px;">
- 
+          <a href="<?=base_url("kullanici/profil_new/$value->kullanici_id"."?subpage=ozluk-dosyasi")?>" class="btn btn-outline-primary btn-sm" style="
+width: -webkit-fill-available;width: -webkit-fill-available; color: #3b3e41; }
+"><i class="fas fa-eye"></i> <?=$value->kullanici_ad_soyad?></a>
  <a href="<?=base_url('siparis/report/'.urlencode(base64_encode("Gg3TGGUcv29CpA8aUcpwV2KdjCz8aE".$value->siparis_id."Gg3TGGUcv29CpA8aUcpwV2KdjCz8aE")))?>" class="btn btn-outline-primary btn-sm" style="
 width: -webkit-fill-available;width: -webkit-fill-available; color: #3b3e41; }
 "><i class="fas fa-eye"></i> Sipariş Detayı</a>
