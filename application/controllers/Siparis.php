d0bbbec3f1ac99327d531139ec1c8bd00e16141a
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-03-06 11:54:33 +0300

    asf

--- application/controllers/Siparis.php
+++ application/controllers/Siparis.php
@@ -1441,7 +1441,7 @@ class Siparis extends CI_Controller {
 			
 			$data[] = [
 				"<b style='cursor: pointer;' onclick='showWindow(\"$urlcustom\");'>$row->siparis_kodu</b><br><span style='font-weight:normal'>" . date('d.m.Y H:i', strtotime($row->kayit_tarihi)) . "</span><br><a class='btn btn-dark' target='_blank' href='".base_url("kullanici/profil_new/$row->kullanici_id")."?subpage=ozluk-dosyasi'><i class='fa fa-user'></i> $row->kullanici_ad_soyad</a>",
-				"<b>$musteri</b><br><span style='font-weight:normal'>İletişim : " . formatTelephoneNumber($row->musteri_iletisim_numarasi) . "</span><br>$durum".'<span class="text-orange">$row->siparis_gorusme_aciklama</span>',
+				"<b>$musteri</b><br><span style='font-weight:normal'>İletişim : " . formatTelephoneNumber($row->musteri_iletisim_numarasi) . "</span><br>$durum".'<span class="text-orange">'.$row->siparis_gorusme_aciklama.'</span>',
 				"<b>$row->merkez_adi</b> / $row->sehir_adi ($row->ilce_adi)<br>$adres",
 				"<a type='button' onclick='showWindow(\"$urlcustom\");' class='btn btn-warning btn-xs'><i class='fa fa-pen'></i> Düzenle</a>"
 			];
