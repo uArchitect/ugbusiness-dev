74a5a3b8805074af1175781557cf7227c7f89de5
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-03-11 13:29:55 +0300

    asf

---
+++ application/views/musteri/tumcihazlar/main_content.php
@@ -0,0 +1,72 @@
+<div class="content-wrapper" style="padding-top:10px">
+ 
+<section class="content text-md" style="    margin-top: -5px;">
+<div class="card card-dark" style="border-radius:0px !important;">
+              <div class="card-header" style="background:#00264f!important">
+              <h3 class="card-title"><strong>UG Business</strong> - Müşteri Bilgileri</h3>
+  
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body" style="    min-height: 790px !important;">
+
+
+              <input type="date" name="filter_garanti_bitis_tarihi" value="<?=(!empty($garanti_bitis_data) ? date("Y-m-d",strtotime($garanti_bitis_data)) : "")?>">
+
+
+              <table id="example1yonlendirilentablo"   class="table table-bordered table-striped nowrap" style="width:100%;">
+        <thead>
+            <tr>
+                 <th>Garanti Bitiş</th>
+                 <th>Cihaz</th>
+                <th>Müşteri Adı</th>
+                <th>Merkez Bilgisi</th> 
+                <th>Adres</th>
+                <th>İl</th>
+                <th>İlçe</th>
+                <th>İletişim Numarası</th> 
+              
+                
+            </tr>
+        </thead>
+        <tbody>
+     <?php 
+     foreach ($data as $musteri) {
+       ?>
+       <tr>
+       <td><?=date("d.m.Y",strtotime($musteri->garanti_bitis_tarihi))?></td>
+        <td><?=$musteri->urun_adi?></td>
+        <td><?=$musteri->musteri_ad?></td>
+        <td><?=$musteri->merkez_adi?></td>
+        <td><?=$musteri->merkez_adresi?></td>
+        <td><?=$musteri->sehir_adi?></td>
+        <td><?=$musteri->ilce_adi?></td>
+        <td><?=$musteri->musteri_iletisim_numarasi?></td>
+     </tr>
+       <?php
+     }
+     ?>
+    </tbody>
+    </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            </div>
+
+
