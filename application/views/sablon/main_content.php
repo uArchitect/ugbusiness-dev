6e2129466e5028be6939ff34693d1b439013673e
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-03-20 13:08:33 +0300

    asf

--- application/views/sablon/main_content.php
+++ application/views/sablon/main_content.php
@@ -7,7 +7,7 @@
         <div class="col">
             <div class="card card-primary">
             <div class="card-header">
-            <h3 class="card-title">Quick Example <small>jQuery Validation</small></h3>
+            <h3 class="card-title"><?=$sablon?></h3>
             </div>
             <!-- /.card-header -->
             <!-- form start -->
