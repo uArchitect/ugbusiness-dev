058cd15a2fab3ba466b03213adbb1534f2e09eeb
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-03-20 13:10:17 +0300

    asf

--- application/views/sablon/main_content.php
+++ application/views/sablon/main_content.php
@@ -22,10 +22,7 @@
                 <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                 </div>
                 <div class="form-group mb-0">
-                <div class="custom-control custom-checkbox">
-                    <input type="checkbox" name="terms" class="custom-control-input" id="exampleCheck1">
-                    <label class="custom-control-label" for="exampleCheck1">I agree to the <a href="#">terms of service</a>.</label>
-                </div>
+                
                 </div>
             </div>
             <!-- /.card-body -->
