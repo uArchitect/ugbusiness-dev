3f74743671127a3b56f81b52d13028d553b8898d
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-03-20 13:04:57 +0300

    asfasf

---
+++ application/controllers/Sablon.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Ariza extends CI_Controller {
+	function __construct(){
+        parent::__construct();
+        session_control();
+        
+        date_default_timezone_set('Europe/Istanbul');
+    }
+ 
+	public function index()
+	{
+      
+		$viewData["page"] = "sablon";
+		$this->load->view('base_view',$viewData);
+	}
+
+	 
+}
