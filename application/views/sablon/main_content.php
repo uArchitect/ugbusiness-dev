6a07575882b473024d49aeabd927e07f5d0cc07f
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-03-20 13:07:02 +0300

    asf

--- application/views/sablon/main_content.php
+++ application/views/sablon/main_content.php
@@ -11,7 +11,7 @@
             <div class="card-body">
                 <div class="form-group">
                 <label for="exampleInputEmail1">Email address</label>
-                <input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                <textarea></textarea>
                 </div>
                 <div class="form-group">
                 <label for="exampleInputPassword1">Password</label>
