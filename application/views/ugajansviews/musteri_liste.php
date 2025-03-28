62da7b368c82691e2dca1a170c90909636f9490a
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-03-28 12:13:22 +0300

    asf

--- application/views/ugajansviews/musteri_liste.php
+++ application/views/ugajansviews/musteri_liste.php
@@ -142,7 +142,7 @@
                
               <td>
 
-              <a href="<?=base_url("ugajans_musteri/profil/$musteri->musteri_id")?>" class="menu-toggle btn btn-sm btn-icon btn-warning  ">
+              <a href="<?=base_url("ugajans_musteri/profil/$musteri->musteri_id")?>" class="  btn btn-sm   btn-warning  ">
                  <i class="ki-filled ki-notepad-edit">
                </i> Profili Görüntüle
                  </a>
