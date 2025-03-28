d77c5a308fcf5557701821d035f84a7a470c1854
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-03-28 09:31:16 +0300

    asf

--- ugajans/application/config/config.php
+++ ugajans/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ugajans/';
+$config['base_url'] = 'https://ugbusiness.com/ugajans/';
 
 /*
 |--------------------------------------------------------------------------
