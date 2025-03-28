6c1c28757e4968aec33789da0a07ea0ac1e7bc4d
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-03-28 09:32:12 +0300

    asf

--- ugajans/application/config/database.php
+++ ugajans/application/config/database.php
@@ -72,13 +72,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $active_group = 'default';
 $query_builder = TRUE;
-
+ 
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'ugajansdb',
+	'username' => 'ugbusine_root',
+	'password' => 'mqEwwy5aFGo',
+	'database' => 'ugbusine_erpdatabase',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
