3531d91d84c6cdcc37eefadfa3e2528187980f20
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-03-28 10:58:22 +0300

    saf

--- application/views/ugajansviews/musteri_liste.php
+++ application/views/ugajansviews/musteri_liste.php
@@ -202,11 +202,12 @@
             const musteriAd = row.querySelector("td:nth-child(1) a")?.textContent.toLowerCase() || "";
             const isletmeAd = row.querySelector("td:nth-child(1) span")?.textContent.toLowerCase() || "";
             const iletisimNumarasi = row.querySelector("td:nth-child(2)")?.textContent.toLowerCase() || "";
-            
+           
             if (musteriAd.includes(searchValue) || isletmeAd.includes(searchValue) || iletisimNumarasi.includes(searchValue)) {
                 row.style.display = "";
             } else {
                 row.style.display = "none";
+                alert(musteriAd);
             }
         });
     });
