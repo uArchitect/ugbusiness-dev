b5082e790001dcfdbf46a891aa2db16c6bc97918
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-04-07 18:29:48 +0300

    asf

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -20,28 +20,28 @@ class Login extends CI_Controller {
 
            $viewData["pazartesi_uretim"] = $this->Siparis_model->get_all(
             "adim_no >"=>3,
-            "kurulum_tarihi >=" =>  date('Y-m-d 00:00:00', strtotime('+1 day')),
-            "kurulum_tarihi <=" =>  date('Y-m-d 23:59:59', strtotime('+1 day'))
+            "kurulum_tarihi >=" =>  date('Y-m-d 00:00:00', strtotime('+1 day', strtotime(date("Y-m-d")))),
+            "kurulum_tarihi <=" =>  date('Y-m-d 23:59:59', strtotime('+1 day', strtotime(date("Y-m-d"))))
             );
             $viewData["sali_uretim"] = $this->Siparis_model->get_all(
                 "adim_no >"=>3,
-                "kurulum_tarihi >=" =>  date('Y-m-d 00:00:00', strtotime('+2 day')),
-                "kurulum_tarihi <=" =>  date('Y-m-d 23:59:59', strtotime('+2 day'))
+                "kurulum_tarihi >=" =>  date('Y-m-d 00:00:00', strtotime('+2 day', strtotime(date("Y-m-d")))),
+                "kurulum_tarihi <=" =>  date('Y-m-d 23:59:59', strtotime('+2 day', strtotime(date("Y-m-d"))))
                 );
                 $viewData["carsamba_uretim"] = $this->Siparis_model->get_all(
                     "adim_no >"=>3,
-                    "kurulum_tarihi >=" =>  date('Y-m-d 00:00:00', strtotime('+3 day')),
-                    "kurulum_tarihi <=" =>  date('Y-m-d 23:59:59', strtotime('+3 day'))
+                    "kurulum_tarihi >=" =>  date('Y-m-d 00:00:00', strtotime('+3 day', strtotime(date("Y-m-d")))),
+                    "kurulum_tarihi <=" =>  date('Y-m-d 23:59:59', strtotime('+3 day', strtotime(date("Y-m-d"))))
                     );
                     $viewData["persembe_uretim"] = $this->Siparis_model->get_all(
                         "adim_no >"=>3,
-                        "kurulum_tarihi >=" =>  date('Y-m-d 00:00:00', strtotime('+4 day')),
-                        "kurulum_tarihi <=" =>  date('Y-m-d 23:59:59', strtotime('+4 day'))
+                        "kurulum_tarihi >=" =>  date('Y-m-d 00:00:00', strtotime('+4 day', strtotime(date("Y-m-d")))),
+                        "kurulum_tarihi <=" =>  date('Y-m-d 23:59:59', strtotime('+4 day', strtotime(date("Y-m-d"))))
                         );
                         $viewData["cuma_uretim"] = $this->Siparis_model->get_all(
                             "adim_no >"=>3,
-                            "kurulum_tarihi >=" =>  date('Y-m-d 00:00:00', strtotime('+5 day')),
-                            "kurulum_tarihi <=" =>  date('Y-m-d 23:59:59', strtotime('+5 day'))
+                            "kurulum_tarihi >=" =>  date('Y-m-d 00:00:00', strtotime('+5 day', strtotime(date("Y-m-d")))),
+                            "kurulum_tarihi <=" =>  date('Y-m-d 23:59:59', strtotime('+5 day', strtotime(date("Y-m-d"))))
                             );
 
 		   
