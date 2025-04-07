add611959fa39aee56921dc215cb157964b863e9
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-04-07 18:44:10 +0300

    asf

--- application/views/siparis/haftalik_kurulum_plan_tv/main_content.php
+++ application/views/siparis/haftalik_kurulum_plan_tv/main_content.php
@@ -21,7 +21,7 @@
   ];
   foreach ($days as $dayName => $dayData): ?>
     <div class="col mb-4">
-      <div class="card shadow-lg border-0 rounded-3">
+      <div class="card border-0 rounded-3">
         <div class="card-header bg-primary text-white text-center">
           <h5 class="mb-0"><?= $dayName ?></h5>
         </div>
