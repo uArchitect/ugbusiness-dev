4f1db3560cc625a3a5d6aac4c0cf2b61505c99e5
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-04-08 10:44:18 +0300

    sf

--- application/views/siparis/haftalik_kurulum_plan_tv/main_content.php
+++ application/views/siparis/haftalik_kurulum_plan_tv/main_content.php
@@ -115,7 +115,7 @@
 <div class="row">
   <div class="col">
     <div class="card" style=" background-color: #0018a4 !important;">
-      <div class="card-body">
+      <div class="card-body" style="color:white">
         YEMEK LİSTESİ
       </div>
     </div>
