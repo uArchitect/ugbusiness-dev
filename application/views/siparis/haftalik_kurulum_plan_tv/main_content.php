3149ae2b5172be5133de551889f22280634e2e35
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-04-08 10:33:56 +0300

    sd

--- application/views/siparis/haftalik_kurulum_plan_tv/main_content.php
+++ application/views/siparis/haftalik_kurulum_plan_tv/main_content.php
@@ -102,7 +102,7 @@
 <script>
   setInterval(function() {
     location.reload();  goFullScreen();
-}, 300000);  // 60000 milisaniye = 1 dakika
+}, 5000);  // 60000 milisaniye = 1 dakika
 function goFullScreen() {
             if (document.documentElement.requestFullscreen) {
                 document.documentElement.requestFullscreen();
