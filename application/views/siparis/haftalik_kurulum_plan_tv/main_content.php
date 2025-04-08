04ae701c49b59c84cae59b3c980ace1b6fc5bec1
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-04-08 15:37:41 +0300

    asf

--- application/views/siparis/haftalik_kurulum_plan_tv/main_content.php
+++ application/views/siparis/haftalik_kurulum_plan_tv/main_content.php
@@ -40,7 +40,7 @@ UMEX ÜRETİM LİSTESİ
 
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper pt-2" style="display: grid
-;background:#e9e9e9">
+;background:#e9e9e9;margin-left:0px">
     <!-- Content Header (Page header) -->
     
     <!-- /.content-header -->
