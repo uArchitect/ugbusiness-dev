a7a96b95f673aec3bd0f448dd97f56bc4ead1dfd
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-04-08 15:35:33 +0300

    asf

--- application/views/siparis/haftalik_kurulum_plan_tv/main_content.php
+++ application/views/siparis/haftalik_kurulum_plan_tv/main_content.php
@@ -137,14 +137,7 @@ UMEX ÜRETİM LİSTESİ
 			<div class="card-header text-bold">SALI
 			</div>
               <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up the bulk of the card's
-                  content.
-                </p>
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
+                 
               </div>
 			  <div class="card-footer">PAZARTESİ
 			</div>
