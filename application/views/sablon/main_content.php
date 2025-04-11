21d1500d9c480b18ff2be14f0ed48834f1c5f8ac
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-04-11 17:59:45 +0300

    asf

--- application/views/sablon/main_content.php
+++ application/views/sablon/main_content.php
@@ -13,7 +13,7 @@
         <?php 
         foreach ($veriler as $veri) {
             ?>
-            <div class="card">
+            <div class="card card-primary col-md-3">
                 <div class="card-header">
                     <?=$veri->sablon_veri_adi?>
                 </div>
