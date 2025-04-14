5b8a6d2d68a5ecb11a6676b03732ae472d11ac7e
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-04-14 15:49:27 +0300

    asf

--- application/views/siparis/haftalik_kurulum_plan_tv/main_content.php
+++ application/views/siparis/haftalik_kurulum_plan_tv/main_content.php
@@ -18,215 +18,164 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <link rel="stylesheet" href="<?=base_url("assets")?>/dist/css/adminlte.min.css">
 </head>
 <body class="hold-transition layout-top-nav">
+ 
+<!-- ./wrapper -->
 <div class="wrapper">
-
-  <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand-md navbar-light navbar-white" style="background:#383E42;margin-left:0px;">
-   <div class="container" style="
-    text-align: center;color:white;
+      <!-- Navbar -->
+      <nav class="main-header navbar navbar-expand-md navbar-light navbar-white" style="background-color: #0003bd; color: #ffffff; padding: 0;">
+        <div class="container" style="
+    text-align: center;
     display: block;
-    font-size: 29px;
+    font-size: 22px;
     font-weight: bolder;
     padding: 13px;
-">
-    
-UMEX ÜRETİM LİSTESİ (<span style="color:white;"> TEST EKRANI</span> )
-      
-       
- 
-    </div>
-  </nav>
-  <!-- /.navbar -->
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper pt-2" style="display: grid
-;background:#e9e9e9;margin-left:0px">
-    <!-- Content Header (Page header) -->
-    
-    <!-- /.content-header -->
-
-    <!-- Main content -->
-    <div class="content" style="    height: 100%!important;">
-      <div class="container" style="     height: 100%!important;       max-width: 100%!important;">
-        <div class="row pb-3 pt-2" style="    height: 100%!important;">
-          
-		    
-		   
-
-
-        <?php 
-  $days = [
-    'PAZARTESİ' => $day1, 
-    'SALI' => $day2, 
-    'ÇARŞAMBA' => $day3, 
-    'PERŞEMBE' => $day4, 
-    'CUMA' => $day5
-  ];
-
-
-
-
-  $gun = date('N');
-  $count = 0;
-  foreach ($days as $dayName => $dayData): ?>
-
-
-
-
-<div class="col" style="    height: 100%!important;margin-right 2px !important;">
-             
-
-            <div class="card 
-             <?php
-          $count++;
-          if($count==$gun){
-            echo "card-success";
-          }else{
-            echo " card-dark";
-          }
-          
-          
-          ?>
-            
-             card-outline" style="    height: 100%!important;">
-			<div class="card-header  text-center
-       <?php 
-          if($count==$gun){
-            echo "text-bold text-success";
-          } 
-          
-          ?>
-      
-      "><?= $dayName ?>
-			</div>
-              <div class="card-body " style="height:345px;padding:5px">
-              <button style="   padding-right: 6px;
-    width: 100%!important;
-    border: 1px dashed #002355;
-    padding-left: 6px;"   type="button" class="btn btn-default text-left pb-2">   
- <b>Umex Plus</b> / U. Grisi<br>
-                             <span>Buzlanan, Soğuk Hava</span>  
-                              </button>
-							  
-                
-              </div>
-               
-            </div><!-- /.card -->
-          </div>
-
- 
-  <?php endforeach; ?>
-
-
-
-  <div class="col" style="    height: 100%;">
-             
-
-             <div class="card card-warning card-outline" style="    height: 100%;">
-       <div class="card-header text-center">PAZARTESİ
-       </div>
-               <div class="card-body" style="height:345px;padding:5px">
-               <button style="   padding-right: 6px;
-    width: 100%!important;
-    border: 1px dashed #002355;
-    padding-left: 6px;"   type="button" class="btn btn-default text-left pb-2">   
- <b>Umex Plus</b><br>
-                             <span>Buzlanan, Soğuk Hava</span>  
-                              </button>
-                 
-                 
-               </div>
-              
-             </div><!-- /.card -->
-           </div>
- 
- 
-          <!-- /.col-md-6 -->
-           
-          <!-- /.col-md-6 -->
-        </div>
-
-
- <!-- Navbar -->
- <nav class="main-header navbar navbar-expand-md navbar-light navbar-white" style="background:#6e696a;margin-left:0px;margin-top:-15px">
-   <div class="container" style="
-    text-align: center;color:white;
-    display: block;
-    font-size: 17px;
-    font-weight: bolder;
-    padding: 5px;
-">
-    
-YEMEK LİSTESİ
-      
-       
- 
-    </div>
-  </nav>
-<div class="row pt-1">
-
-            <?php 
-            $yemek = [
-              "Karnabahar Kızartması, Mercimek Çorbası, Tatlı, Turşu",
-              "Karnabahar Kızartması, Mercimek Çorbası, Tatlı, Turşu",
-              "Karnabahar Kızartması, Mercimek Çorbası, Tatlı, Turşu",
-              "Karnabahar Kızartması, Mercimek Çorbası, Tatlı, Turşu",
-              "Karnabahar Kızartması, Mercimek Çorbası, Tatlı, Turşu",
-              "Karnabahar Kızartması, Mercimek Çorbası, Tatlı, Turşu"
-            ];
-            foreach ($yemek as $y) {
-
-              ?>
-              <div class="col p-2">
-                <div class="card text-center">
-                
-
-                  <button style="   padding-right: 6px;
-    width: 100%!important;text-align:center
-  ;
-    border: 1px dashed #002355;
-    padding-left: 6px;"   type="button" class="btn btn-default text-left pb-2">   
-   <?=$y?>  
-                              </button>
-                 
+"> UMEX ÜRETİM LİSTESİ </div>
+      </nav>
+      <!-- /.navbar -->
+      <!-- Content Wrapper. Contains page content -->
+      <div class="content-wrapper " style="display: grid
+;background:#f7f7f7">
+        <!-- Content Header (Page header) -->
+        <!-- /.content-header -->
+        <!-- Main content -->
+        <div class="content" style="    height: 100%;padding:0">
+          <div class="container" style="     height: 100%;       max-width: 100%;">
+            <div class="row  " style="    height: 100%;">
+              <div class="col" style="    height: 100%;">
+                <div class="card card-dark" style="    height: 100%;">
+                  <div class="card-header text-bold" style="background:#000589">
+                    <span style="font-weight:400;font-size:13px">14.04.2025</span>
+                    <br>PAZARTESİ
+                  </div>
+                  <div class="card-body"></div>
                 </div>
+                <!-- /.card -->
               </div>
-
-              <?php
-              # code...
-            }
-            ?>
-
+              <div class="col" style="    height: 100%;">
+                <div class="card card-dark  " style="    height: 100%;">
+                  <div class="card-header text-bold" style="background:#000589">
+                    <span style="font-weight:400;font-size:13px">14.04.2025</span>
+                    <br>SALI
+                  </div>
+                  <div class="card-body"></div>
+                </div>
+                <!-- /.card -->
+              </div>
+              <div class="col" style="    height: 100%;">
+                <div class="card card-dark  " style="    height: 100%;">
+                  <div class="card-header text-bold" style="background:#000589">
+                    <span style="font-weight:400;font-size:13px">14.04.2025</span>
+                    <br>ÇARŞAMBA
+                  </div>
+                  <div class="card-body"></div>
+                </div>
+                <!-- /.card -->
+              </div>
+              <div class="col" style="    height: 100%;">
+                <div class="card card-dark" style="    height: 100%;">
+                  <div class="card-header text-bold" style="background:#000589">
+                    <span style="font-weight:400;font-size:13px">14.04.2025</span>
+                    <br>PERŞEMBE
+                  </div>
+                  <div class="card-body"></div>
+                </div>
+                <!-- /.card -->
+              </div>
+              <div class="col" style="    height: 100%;">
+                <div class="card card-dark" style="    height: 100%;">
+                  <div class="card-header text-bold" style="background:#000589">
+                    <span style="font-weight:400;font-size:13px">14.04.2025</span>
+                    <br>CUMA
+                  </div>
+                  <div class="card-body"></div>
+                </div>
+                <!-- /.card -->
+              </div>
+              <div class="col" style="    height: 100%;">
+                <div class="card card-warning  " style="    height: 100%;">
+                  <div class="card-header text-bold">
+                    <span style="font-weight:400;font-size:13px">14.04.2025</span>
+                    <br>PAZARTESİ
+                  </div>
+                  <div class="card-body">
+                    <div class="row">
+                      <div class="col" style="padding-left: 0px;">
+                        <span style="display: block;background: #dbdbdb87;padding: 5px;color: white;border-radius: 5px;border-radius: 3px 3px 0 0;">
+                          <span style="min-width: 230px; width: 230px;   margin-left:5px">
+                            <b style="color:#0f3979">Umex Plus / Gri</b>
+                          </span>
+                        </span>
+                        <span style="
+    height: 11px;
+"></span>
+                        <div style="padding-left:10px;background:white;border:1px solid;border-top:0px;border: 1px solid #dbdbdb; border-top: 0px; border-radius: 0px 0px 3px 3px;"> 1 Buz, 1 Soğuk </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- /.card -->
+              </div>
+              <div class="col" style="    height: 100%;">
+                <div class="card card-success mb-0">
+                  <div class="card-header text-bold">
+                    <span style="font-weight:400;font-size:13px">12:30</span>
+                    <br>YEMEK MENÜSÜ
+                  </div>
+                  <div class="card-body">
+                    <div class="row">
+                      <div class="col-12 text-center" style="padding-left: 0px;">
+                        <span style="font-weight: 400; font-size: sm;">Kıymalı Makarna <br>Yoğurt Çorbası <br>Tatlı <br>Turşu </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- /.card -->
+                <div class="card  card-danger  ">
+                  <div class="card-header text-bold  " style="font-size:13px">KART OKUTMAYANLAR </div>
+                  <div class="card-body">
+                    <div class="row">
+                      <div class="col-12" style="padding-left: 0px;">
+                        <span style="display: block;background: #dbdbdb87;padding: 5px;color: white;border-radius: 5px;border-radius: 3px 3px 0 0;">
+                          <span style="min-width: 230px; width: 230px;   margin-left:5px">
+                            <b style="color:#0f3979">Ergül Kızılkaya</b>
+                          </span>
+                        </span>
+                        <span style="
+    height: 11px;
+"></span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- /.card -->
+              </div>
+              <!-- /.col-md-6 -->
+              <!-- /.col-md-6 -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.container-fluid -->
+        </div>
+        <!-- /.content -->
+      </div>
+      <!-- /.content-wrapper -->
+      <!-- Control Sidebar -->
+      <aside class="control-sidebar control-sidebar-dark">
+        <!-- Control sidebar content goes here -->
+      </aside>
+      <!-- /.control-sidebar -->
+      <!-- Main Footer -->
+      <footer class="main-footer" style="color:#2b4883;">
+        <!-- To the right -->
+        <div class="text-center  " style="margin-bottom:-30px">
+          <img src="umexlogo.svg"></img>
+        </div>
+        <div class="float-right d-none d-sm-inline">
+          <b>Son Güncelleme : </b>14:44
+        </div>
+        <!-- Default to the left --> ÜRETİM DEPARTMANI
+      </footer>
 </div>
-
-
-
-
-
-        <!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-
- 
- 
-  <!-- Main Footer -->
-  <footer class="main-footer" style="margin-left:0px;margin-top:-30px">
-    <!-- To the right -->
-	<div class="text-center  " style="margin-bottom:-30px">
-      <img src="<?=base_url("uploads")?>/umexlogo.svg"></img>
-    </div>
-    <div class="float-right d-none d-sm-inline" style="margin-bottom:-10px">
-      Son Güncelleme : <?=date("d.m.Y H:i")?>
-    </div>
-    <!-- Default to the left -->
-   ÜRETİM DEPARTMANI
-  </footer>
-</div>
-<!-- ./wrapper -->
-
 <!-- REQUIRED SCRIPTS -->
 
 <!-- jQuery -->
