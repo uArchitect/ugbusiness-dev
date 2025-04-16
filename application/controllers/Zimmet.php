9ff17eeb92cc3f05ba12f2118d729955efe1f5d1
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-04-16 11:34:33 +0300

    asf

--- application/controllers/Zimmet.php
+++ application/controllers/Zimmet.php
@@ -47,6 +47,7 @@ public function departmana_stok_tanimla($departman_id)
         $this->db->insert("zimmet_hareketler",$insertData);
 
         $this->session->set_flashdata('insertedID', $this->input->post("zimmet_stok_no") );
+ $this->session->set_flashdata('departmanID', $departman_id );
 
 
         redirect($_SERVER['HTTP_REFERER']);
