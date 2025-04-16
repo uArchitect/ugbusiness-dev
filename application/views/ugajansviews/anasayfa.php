7e1edb14138ab37367184134350da2eaefa9229c
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-04-16 10:10:03 +0300

    asf

--- application/views/ugajansviews/anasayfa.php
+++ application/views/ugajansviews/anasayfa.php
@@ -166,28 +166,28 @@
            <div class="grid grid-cols-1 w-full gap-1.5 border-[0.5px] border-dashed border-gray-400 rounded-md px-2.5 py-2 shrink-0 min-w-24 max-w-auto">
             
              <span class="text-gray-700 text-xs">
-              Kuru Fasülye
+             Yoğurt çorbası
              </span>
             </div>
 
             <div class="grid grid-cols-1 w-full gap-1.5 border-[0.5px] border-dashed border-gray-400 rounded-md px-2.5 py-2 shrink-0 min-w-24 max-w-auto">
             
              <span class="text-gray-700 text-xs">
-              Pirinç Pilavı
+             Kıymalı Makarna
              </span>
             </div>
 
             <div class="grid grid-cols-1 w-full gap-1.5 border-[0.5px] border-dashed border-gray-400 rounded-md px-2.5 py-2 shrink-0 min-w-24 max-w-auto">
             
              <span class="text-gray-700 text-xs">
-              Cacık
+             Tatlı 
              </span>
             </div>
 
             <div class="grid grid-cols-1 w-full gap-1.5 border-[0.5px] border-dashed border-gray-400 rounded-md px-2.5 py-2 shrink-0 min-w-24 max-w-auto">
            
              <span class="text-gray-700 text-xs">
-              Meyve
+             Salata  
              </span>
             </div>
              
