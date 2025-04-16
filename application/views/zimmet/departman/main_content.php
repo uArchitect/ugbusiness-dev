446b9e048393434b470200622d42c21f45c251a6
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-04-16 10:37:21 +0300

    asf

--- application/views/zimmet/departman/main_content.php
+++ application/views/zimmet/departman/main_content.php
@@ -45,7 +45,7 @@
 
             <div class="card card-danger card-outline">
               <div class="card-header">
-                <h3 class="card-title">Yeni Stok Tanımla</h3>
+                <h3 class="card-title">Üretim Departmanı <small>(Tanımlanan Stoklar)</small></h3>
               </div>
               <div class="card-body">
               <table class="table table-bordered">
