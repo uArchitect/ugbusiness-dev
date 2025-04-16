bbe31d05bbdf26524c1102d53c9b6c8a72b5f035
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-04-16 13:57:36 +0300

    asf

--- application/controllers/Zimmet.php
+++ application/controllers/Zimmet.php
@@ -41,7 +41,20 @@ $this->db->order_by('zs.zimmet_stok_adi', 'ASC');
 
         $viewData["kullanicilar"] =  $this->db->get()->result();
 
-
+        $this->db->select('
+        zs.*,
+        zh.*,
+        d.*,
+        SUM(zh.zimmet_hareket_giris_miktar) AS toplam_giris,
+        SUM(zh.zimmet_hareket_cikis_miktar) AS toplam_cikis,
+        (SUM(zh.zimmet_hareket_giris_miktar) - SUM(zh.zimmet_hareket_cikis_miktar)) AS kalan
+    ');
+    $this->db->from('zimmet_hareketler zh');
+    $this->db->join('zimmet_stoklar zs', 'zh.zimmet_stok_no = zs.zimmet_stok_id', 'left');
+    $this->db->join('zimmet_departmanlar d', 'zh.zimmet_departman_no = d.zimmet_departman_id', 'left');
+    $this->db->group_by(['zh.zimmet_stok_no', 'zh.zimmet_departman_no']);
+    $this->db->order_by('zs.zimmet_stok_adi', 'ASC');
+    $viewData["hareketler"] =  $this->db->get()->result();
 
 
 		$viewData["page"] = "zimmet/dagitim";
