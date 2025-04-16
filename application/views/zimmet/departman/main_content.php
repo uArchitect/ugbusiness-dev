f394409fa59e33d9dbae0c657bd96ed6c7bfbd13
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-04-16 10:36:19 +0300

    asf

--- application/views/zimmet/departman/main_content.php
+++ application/views/zimmet/departman/main_content.php
@@ -48,27 +48,58 @@
                 <h3 class="card-title">Yeni Stok Tanımla</h3>
               </div>
               <div class="card-body">
-                <div class="row">
-                  <div class="col-5">
-                    <select name="" class="select2 form-control" id="">
-                      <?php 
-                      foreach ($stoklar as $s) {
-                       ?>
-                       <option value=""><?=$s->zimmet_stok_adi?></option>
-                       <?php
-                      }
-                      ?>
-                    </select>
-                  </div>
-                  <div class="col-5">
-                    <input type="number" class="form-control" min="1" placeholder="Stok Miktarı Giriniz">
-                  </div>
-                  <div class="col-2">
-                    <button class="btn btn-success" style="    width: -webkit-fill-available;">
-                      KAYDET
-                    </button>
-                  </div>
-                </div>
+              <table class="table table-bordered">
+                  <thead>
+                    <tr>
+                      <th style="width: 10px">#</th>
+                      <th>Task</th>
+                      <th>Progress</th>
+                      <th style="width: 40px">Label</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1.</td>
+                      <td>Update software</td>
+                      <td>
+                        <div class="progress progress-xs">
+                          <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
+                        </div>
+                      </td>
+                      <td><span class="badge bg-danger">55%</span></td>
+                    </tr>
+                    <tr>
+                      <td>2.</td>
+                      <td>Clean database</td>
+                      <td>
+                        <div class="progress progress-xs">
+                          <div class="progress-bar bg-warning" style="width: 70%"></div>
+                        </div>
+                      </td>
+                      <td><span class="badge bg-warning">70%</span></td>
+                    </tr>
+                    <tr>
+                      <td>3.</td>
+                      <td>Cron job running</td>
+                      <td>
+                        <div class="progress progress-xs progress-striped active">
+                          <div class="progress-bar bg-primary" style="width: 30%"></div>
+                        </div>
+                      </td>
+                      <td><span class="badge bg-primary">30%</span></td>
+                    </tr>
+                    <tr>
+                      <td>4.</td>
+                      <td>Fix and squish bugs</td>
+                      <td>
+                        <div class="progress progress-xs progress-striped active">
+                          <div class="progress-bar bg-success" style="width: 90%"></div>
+                        </div>
+                      </td>
+                      <td><span class="badge bg-success">90%</span></td>
+                    </tr>
+                  </tbody>
+                </table>
               </div>
               <!-- /.card-body -->
             </div>
