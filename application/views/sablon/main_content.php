fa5ba0c10fa71dfc73bbb3f2d4f8f817627811ec
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-04-17 12:48:52 +0300

    asf

--- application/views/sablon/main_content.php
+++ application/views/sablon/main_content.php
@@ -28,9 +28,9 @@
 
 <a class="dropdown-item editKategoriBtn" 
 data-id="<?=$altsablon->sablon_kategori_id?>" 
-data-ad="<?=$altsablon->sablon_kategori_adi?>"  href="#"><fa class="fas fa-pencil"></fa></a>
+data-ad="<?=$altsablon->sablon_kategori_adi?>"  href="#"><fa class="fas fa-pen"></fa></a>
 
-<a class="dropdown-item deleteKategoriBtn"  data-id="<?=$altsablon->sablon_kategori_id?>"  href="#"><i class="fa-fa-trash"></i></a>
+<a class="dropdown-item deleteKategoriBtn"  data-id="<?=$altsablon->sablon_kategori_id?>"  href="#"><i class="fa fa-trash"></i></a>
 
                     </div>
 
