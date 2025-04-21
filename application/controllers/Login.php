de84e0d6ceb7c7275678b25b315906c03fb586c0
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-04-21 18:32:55 +0300

    asf

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -47,7 +47,7 @@ class Login extends CI_Controller {
     
 
         $this->load->model('Yemek_model');
-        if(date("h")>14){
+        if(date("h")>2){
             $viewData["yemek"] = $this->Yemek_model->get_by_id(date("d")+1)[0];
         
         }else{
