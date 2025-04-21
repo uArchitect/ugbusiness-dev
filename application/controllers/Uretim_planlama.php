661b6421a9c77ed870be1f3a49ea45684943c1fd
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-04-21 09:37:15 +0300

    asf

--- application/controllers/Uretim_planlama.php
+++ application/controllers/Uretim_planlama.php
@@ -29,29 +29,29 @@ class Uretim_planlama extends CI_Controller {
         $viewData["urunler"] = $query->result();
 
 
-		$viewData["page"] = "departman/form";
+		$viewData["page"] = "uretim/form";
 		$this->load->view('base_view',$viewData);
 	}
 
 	public function edit($id = '')
 	{  
        // yetki_kontrol("uretim_plan_duzenle");
-		$check_id = $this->Departman_model->get_by_id($id); 
+		$check_id = $this->db->where("uretim_planlama_id",$id)->get("uretim_planlama")->result()[0]; 
         if($check_id){  
-            $viewData['departman'] = $check_id[0];
-			$viewData["page"] = "departman/form"; 
+            $viewData['uplan'] = $check_id;
+			$viewData["page"] = "uretim/form"; 
 			$this->load->view('base_view',$viewData);
         }else{
-            redirect(site_url('departman'));
+            redirect(site_url('uretim'));
         }
  
 	}
 
     public function delete($id)
 	{     
-        yetki_kontrol("departman_sil");
-		$this->Departman_model->delete($id);  
-        $viewData["page"] = "departman/list";
+       // yetki_kontrol("uretim_plan_sil");
+       $this->db->where("uretim_planlama_id",$id)->delete("uretim_planlama");
+        $viewData["page"] = "uretim/list";
 		$this->load->view('base_view',$viewData);
 	}
 
@@ -60,30 +60,28 @@ class Uretim_planlama extends CI_Controller {
 	public function save($id = '')
 	{   
         if(empty($id)){
-            yetki_kontrol("departman_ekle");
+          //  yetki_kontrol("uretim_plan_ekle");
         }else{
-            yetki_kontrol("departman_duzenle");
+      //      yetki_kontrol("uretim_plan_duzenle");
         }
-        $this->form_validation->set_rules('departman_adi',  'Departman Adı',  'required'); 
+        $this->form_validation->set_rules('urun_fg_id',  'Cihaz',  'required'); 
         
-        $data['departman_adi']  = escape($this->input->post('departman_adi'));
-        $data['departman_aciklama']  = escape($this->input->post('departman_aciklama'));
-        $data['departman_guncelleme_tarihi'] = date('Y-m-d H:i:s');
+        $data['urun_fg_id']  = escape($this->input->post('departman_adi'));
+        $data['baslik_bilgisi']  = escape($this->input->post('baslik_bilgisi'));
+        $data['uretim_tarihi'] = escape($this->input->post('uretim_tarihi'));
 
         if ($this->form_validation->run() != FALSE && !empty($id)) {
-            $check_id = $this->Departman_model->get_by_id($id);
+            $check_id = $this->db->where("uretim_planlama_id",$id)->get("uretim_planlama")->result()[0];
             if($check_id){
                 unset($data['id']);
-                $this->Departman_model->update($id,$data);
+                $this->db->where("uretim_planlama_id",$id)->update("uretim_planlama",$data);
             }
         }elseif($this->form_validation->run() != FALSE && empty($id)){
-            $data['departman_sorumlu_kullanici_id']  = escape($this->session->userdata('aktif_kullanici_id'));
-       
-            $this->Departman_model->insert($data);
+            $this->db->insert("uretim_planlama",$data); 
         }else{
             $this->session->set_flashdata('form_errors', json_encode($this->form_validation->error_array()));
-            redirect(site_url('departman/ekle'));
+            redirect(base_url('uretim/form'));
         }
-		redirect(site_url('departman'));
+		redirect(base_url('uretim_planlama'));
 	}
 }
