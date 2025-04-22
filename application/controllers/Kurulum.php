aa7f793d142e3ea7efca0d59a6ee85ed01ecd844
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-04-22 16:12:17 +0300

    asf

--- application/controllers/Kurulum.php
+++ application/controllers/Kurulum.php
@@ -16,12 +16,12 @@ class Kurulum extends CI_Controller {
         $viewData["tag"] =     $tag;
       
 
-        $query = $this->db->where("kurulum_data.kurulum_data_id",$id)
+        $query = $this->db
         ->join('siparis_urunleri', 'siparis_urunleri.siparis_urun_id  = kurulum_data.kurulum_data_siparis_urun_no')
         ->join('urunler', 'urunler.urun_id = siparis_urunleri.urun_no')
         ->join('siparisler', 'siparisler.siparis_id = siparis_urunleri.siparis_kodu')
         ->join('merkezler', 'merkezler.merkez_id = siparisler.merkez_no')
-        ->get("kurulum_data");
+        ->where("kurulum_data.kurulum_data_id",$id)->get("kurulum_data");
         $viewData["kdata"] = $query->result()[0];
  
         $viewData["page"] = "kurulum/tarama";
