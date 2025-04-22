b4fb20f72c5b096901da19fb2967b38987bcc169
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-04-22 16:17:41 +0300

    asf

--- application/controllers/Kurulum.php
+++ application/controllers/Kurulum.php
@@ -8,7 +8,7 @@ class Kurulum extends CI_Controller {
         date_default_timezone_set('Europe/Istanbul'); 
     } 
 
-    public function index($id = 0,$tag="c1") {
+    public function index($id = 0,$tag="C1") {
       if($tag == "0"){
  echo "hata";
       }else{
