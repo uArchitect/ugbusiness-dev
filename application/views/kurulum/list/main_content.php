c1269081814e8adee6137d2726ab3fe4c0bdb50c
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-04-22 11:57:48 +0300

    asf

--- application/views/kurulum/list/main_content.php
+++ application/views/kurulum/list/main_content.php
@@ -7,20 +7,17 @@
     <div class="col-md-3">
     <div class="card card-primary">
               <div class="card-header">
-                <h3 class="card-title">Quick Example</h3>
+                <h3 class="card-title">Yeni Kayıt Oluştur</h3>
               </div>
               <!-- /.card-header -->
               <!-- form start -->
               <form>
                 <div class="card-body">
                   <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
+                    <label for="exampleInputEmail1">Cihaz Seri Numarası</label>
                     <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
                   </div>
-                  <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                  </div>
+                  
                   <div class="form-group">
                     <label for="exampleInputFile">File input</label>
                     <div class="input-group">
