173dee0231238e3be1cee5e80a2c5445db9bf77a
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-04-22 12:00:21 +0300

    asf

--- application/views/kurulum/list/main_content.php
+++ application/views/kurulum/list/main_content.php
@@ -15,7 +15,7 @@
                 <div class="card-body">
                   <div class="form-group">
                     <label for="exampleInputEmail1">Cihaz Seri Numarası</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Seri No Giriniz">
+                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Cihaz Seri No Giriniz">
                   </div>
                    
                 </div>
