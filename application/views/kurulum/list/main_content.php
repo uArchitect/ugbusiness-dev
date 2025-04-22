c9863f207fce9109bf5e7cba3b7aa8ebe7c5de63
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-04-22 12:01:48 +0300

    asf

--- application/views/kurulum/list/main_content.php
+++ application/views/kurulum/list/main_content.php
@@ -22,7 +22,7 @@
                 <!-- /.card-body -->
 
                 <div class="card-footer">
-                  <button type="submit" class="btn btn-primary">Submit</button>
+                  <button type="submit" class="btn btn-success">Kayıt Oluştur</button>
                 </div>
               </form>
             </div>
