28751a53281d347cbb8f3ed97d41859b2e5a1a6d
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-04-22 11:58:33 +0300

    asf

--- application/views/kurulum/list/main_content.php
+++ application/views/kurulum/list/main_content.php
@@ -17,23 +17,7 @@
                     <label for="exampleInputEmail1">Cihaz Seri Numarası</label>
                     <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
                   </div>
-                  
-                  <div class="form-group">
-                    <label for="exampleInputFile">File input</label>
-                    <div class="input-group">
-                      <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="exampleInputFile">
-                        <label class="custom-file-label" for="exampleInputFile">Choose file</label>
-                      </div>
-                      <div class="input-group-append">
-                        <span class="input-group-text">Upload</span>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                  </div>
+                   
                 </div>
                 <!-- /.card-body -->
 
