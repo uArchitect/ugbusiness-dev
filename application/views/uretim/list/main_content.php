307393f647c34a841bb29e61ea17003a75c13e0e
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-04-24 13:38:35 +0300

    asf

--- application/views/uretim/list/main_content.php
+++ application/views/uretim/list/main_content.php
@@ -81,7 +81,9 @@ foreach ($gunler as $g) {
                         continue;
                       }
                       ?>
- <div class="row" onclick="<?=base_url("uretim_planlama/edit/$d->uretim_planlama_id")?>">
+ <a href="<?=base_url("uretim_planlama/edit/$d->uretim_planlama_id")?>">
+ 
+ <div class="row" >
                        <div class="col mb-2" style="border:1px solid gray;border-radius:3px;padding-left: 0px;margin-right: -1px;    padding-right: 0; margin-top: -1px;">
                        <span style="display: block;background:#070db9;  padding: 5px;color: white;border-radius: 5px;border-radius: 3px 3px 0 0;">
                          <span style="min-width: 230px; width: 230px;    margin-left:5px">
@@ -122,7 +124,7 @@ foreach ($gunler as $g) {
                         
                         </div>
                        </div>
-                       </div>
+                       </div></a>
                       <?php
                     }
                     ?>
