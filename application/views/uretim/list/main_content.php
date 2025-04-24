b777be84b1d799dc158d2ad77623bea44eacb2d6
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-04-24 13:42:46 +0300

    asf

--- application/views/uretim/list/main_content.php
+++ application/views/uretim/list/main_content.php
@@ -84,8 +84,8 @@ foreach ($gunler as $g) {
  
  <div class="row" >
                        <div class="col mb-2" style="border:1px solid gray;border-radius:3px;padding-left: 0px;margin-right: -1px;    padding-right: 0; margin-top: -1px;">
-                       <span style="display: block;background:#070db9;  padding: 5px;color: white;border-radius: 5px;border-radius: 3px 3px 0 0;">
-                         <span style="min-width: 230px; width: 230px;    margin-left:5px">
+                       <span style="display: block;background:#070db9; text-align: center; padding: 5px;color: white;border-radius: 5px;border-radius: 3px 3px 0 0;">
+                         <span style="min-width: 230px; width: 230px;   text-align: center; margin-left:5px">
                             
                           
 
