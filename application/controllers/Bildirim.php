f30026d6f16c851096b3208922139c6ce25d1bc9
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-04-25 10:59:11 +0300

    asf

--- application/controllers/Bildirim.php
+++ application/controllers/Bildirim.php
@@ -17,64 +17,7 @@ class Bildirim extends CI_Controller {
     }
 
 
-
-
-
-
-
-   public function sendFirebaseNotification($deviceToken, $title, $body)
-    {
-        $projectId = 'umexcomtr'; // Firebase projenin ID'si
-        $credentialsPath = __DIR__ . '/service-account.json'; // İndirdiğin hizmet hesabı JSON dosyası
-    echo  $credentialsPath;
-        // Access Token al
-        $oauth = new OAuth2([
-            'audience' => 'https://oauth2.googleapis.com/token',
-            'issuer' => json_decode(file_get_contents($credentialsPath))->client_email,
-            'signingAlgorithm' => 'RS256',
-            'signingKey' => json_decode(file_get_contents($credentialsPath))->private_key,
-            'tokenCredentialUri' => 'https://oauth2.googleapis.com/token',
-            'scope' => 'https://www.googleapis.com/auth/firebase.messaging',
-        ]);
-    
-        $authToken = $oauth->fetchAuthToken();
-        $accessToken = $authToken['access_token'];
-    
-        $url = "https://fcm.googleapis.com/v1/projects/{$projectId}/messages:send";
-    
-        $message = [
-            "message" => [
-                "token" => $deviceToken,
-                "notification" => [
-                    "title" => $title,
-                    "body" => $body,
-                ],
-                "android" => [
-                    "priority" => "high"
-                ],
-                "apns" => [
-                    "headers" => [
-                        "apns-priority" => "10"
-                    ]
-                ]
-            ]
-        ];
-    
-        $headers = [
-            "Authorization: Bearer " . $accessToken,
-            "Content-Type: application/json"
-        ];
-    
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($message));
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    
-        $result = curl_exec($ch);
-        curl_close($ch);
-    
-        return $result;
-    }
+ 
 
 
     public function gonder()
