f00c4e799fa5a906b40d0c5bf5760a57f9619267
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-04-25 10:59:29 +0300

    asf

--- application/controllers/Bildirim.php
+++ application/controllers/Bildirim.php
@@ -1,9 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-require 'vendor/autoload.php';
 
-use Google\Auth\OAuth2;
 
 class Bildirim extends CI_Controller {
 	function __construct(){
