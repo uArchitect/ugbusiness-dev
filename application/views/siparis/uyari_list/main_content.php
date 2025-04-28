17201136a6793facf5f29aabee51a9e25eac6a0d
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-04-28 15:50:37 +0300

    asf

--- application/views/siparis/uyari_list/main_content.php
+++ application/views/siparis/uyari_list/main_content.php
@@ -131,7 +131,7 @@
                     { "data": 3 } 
                 ],
             "createdRow": function (row, data, dataIndex) {
-                if (Object.values(data).some(val => String(val).includes("1 x"))) {
+                if (Object.values(data).some(val => String(val).includes(" x"))) {
                     $(row).css("background-color", "#9beaae"); // Bootstrap success yeşili
                 }
             }
