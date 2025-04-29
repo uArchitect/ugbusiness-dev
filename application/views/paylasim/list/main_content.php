f69e0f7f6e4f2a0292c5ff86144a17c9125a9c4e
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-04-29 11:36:17 +0300

    asf

--- application/views/paylasim/list/main_content.php
+++ application/views/paylasim/list/main_content.php
@@ -41,31 +41,31 @@
                     <td>
                       <div class="custom-control custom-checkbox">
                           <input class="custom-control-input custom-control-input-danger" type="checkbox" id="customCheckbox4" checked="">
-                         
+                          <label for="customCheckbox4" class="custom-control-label"> </label>
                         </div>
                       </td>
                     <td>
                       <div class="custom-control custom-checkbox">
                           <input class="custom-control-input custom-control-input-danger" type="checkbox" id="customCheckbox4" checked="">
-                      
+                          <label for="customCheckbox4" class="custom-control-label"> </label>
                         </div>
                       </td>
                     <td>
                       <div class="custom-control custom-checkbox">
                           <input class="custom-control-input custom-control-input-danger" type="checkbox" id="customCheckbox4" checked="">
-                          
+                          <label for="customCheckbox4" class="custom-control-label"> </label>
                         </div>
                       </td>
                     <td>
                       <div class="custom-control custom-checkbox">
                           <input class="custom-control-input custom-control-input-danger" type="checkbox" id="customCheckbox4" checked="">
-                         
+                          <label for="customCheckbox4" class="custom-control-label">  </label>
                         </div>
                       </td>
                     <td>
                       <div class="custom-control custom-checkbox">
                           <input class="custom-control-input custom-control-input-danger" type="checkbox" id="customCheckbox4" checked="">
-                          
+                          <label for="customCheckbox4" class="custom-control-label"> </label>
                         </div>
                       </td>
                   </tr>   </tbody>
