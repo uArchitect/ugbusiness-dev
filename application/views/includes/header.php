00bc1b569ab1ee01297da5a1a04b0c28c9d2ec26
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-05-06 13:21:07 +0300

    asf

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -103,7 +103,7 @@ if(aktif_kullanici()->kullanici_id == 9 || aktif_kullanici()->kullanici_id == 7 
          
        if(kmuyari()){
         ?>
-        <a href="<?=base_url("arac")?>" class="btn btn-danger text-white yanipsonenyazis2"><?= $kmlastdata2->arac_km_kayit_tarihi?> Km Giriş Uyarısı</a>
+        <a href="<?=base_url("arac")?>" class="btn btn-danger text-white yanipsonenyazis2"> Km Giriş Uyarısı</a>
           
         <?php
        }
