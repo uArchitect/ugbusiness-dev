8597353e67fd2661469f69bc21cb0b70f367f399
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-05-12 11:26:23 +0300

    asf

--- application/views/includes/left_side_bar.php
+++ application/views/includes/left_side_bar.php
@@ -247,19 +247,17 @@
           
 <?php } ?>
 
-
-
 <?php
-  if($this->session->userdata('aktif_kullanici_id') == 1 || $this->session->userdata('aktif_kullanici_id') == 9 ){
+  if($this->session->userdata('aktif_kullanici_id') == 1 || $this->session->userdata('aktif_kullanici_id') == 9 || $this->session->userdata('aktif_kullanici_id') == 4){
       
 
 ?>
  <li class="nav-item">
-     <a href="<?=base_url("sablon/index/24")?>" class="nav-link">
+     <a href="<?=base_url("yazilim")?>" class="nav-link">
      <i class="fa fa-contact nav-icon" style="font-size:13px"></i>
      <p style="font-size:15px">
       <?php 
-      echo "ŞİRKET KURALLARI";
+      echo "YAPILACAK İŞLER";
         
       ?>
      
