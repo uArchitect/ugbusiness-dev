d148e66c437096b1a3a6821caadbd80c2a2dd458
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-05-21 10:39:23 +0300

    asf

--- application/controllers/Cihaz.php
+++ application/controllers/Cihaz.php
@@ -1012,8 +1012,8 @@ if($search != null)
         ;
 
         $query = $this->db
-        ->where("musteri_aktif",1)
-        ->where("rg_medikal",1)
+        ->where("musteri_aktif",0)
+        ->where("rg_medikal",$rg_mi)
         ->join('musteriler', 'musteriler.musteri_id = merkez_yetkili_id')
          ->join('sehirler', 'sehirler.sehir_id = merkez_il_id','left')
          ->join('ilceler', 'ilceler.ilce_id = merkez_ilce_id','left')
