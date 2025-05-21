ab16dbc70520ef3fa16565f03ca64f27a1fde9c0
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-05-21 11:09:05 +0300

    asf

--- application/views/musteri/profil/main_content.php
+++ application/views/musteri/profil/main_content.php
@@ -76,7 +76,7 @@ body {
                     </div>
                     <div class="media-body text-white">
                       <h4 class="mt-0 mb-0" style="font-size: 30px;"><?=$musteri->musteri_ad?></h4>
-                      <p class="small mb-4" style="font-size: 15px;">
+                      <p class="small mb-4 <?=($musteri->rg_medikal	== 1)?"bg-success":""?>" style="font-size: 15px;">
                   
                         <?=($musteri->musteri_doktor_mu == 1) ? "DOKTOR" : (($musteri->rg_medikal	== 1) ? "RG MEDİKAL MÜŞTERİSİ" : "UMEX BİREYSEL MÜŞTERİ")?>
                       </p>
