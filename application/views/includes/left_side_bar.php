646052602f32b7b19a80069c541d8d7308d91852
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-05-21 11:07:43 +0300

    asf

--- application/views/includes/left_side_bar.php
+++ application/views/includes/left_side_bar.php
@@ -401,8 +401,11 @@
 
 ?>
  <li class="nav-item">
-     <a href="<?=base_url("cihaz/rgmedikalindex")?>" class="nav-link">
+     <a href="<?=base_url("cihaz/rgmedikalindex")?>" style="background: #004e0f;" class="nav-link">
      <i class="fa fa-contact nav-icon" style="font-size:13px"></i>
+    
+     <i class="far fa-circle nav-icon " style="font-size:13px"></i>
+    
      <p style="font-size:15px">
       <?php 
       echo "RG MEDİKAL";
