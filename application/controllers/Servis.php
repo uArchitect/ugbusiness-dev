82c2147ab1e22463cc651608e97637ba3895752b
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-05-23 16:27:00 +0300

    asf

--- application/controllers/Servis.php
+++ application/controllers/Servis.php
@@ -1239,8 +1239,7 @@ sendSmsData("05453950049","SERVİS KAYDI AÇILDI ".date("d.m.Y H:i")."\n".base_u
     urunler.urun_adi,
     siparis_urunleri.seri_numarasi,
     
-    urun_renkleri.renk_adi,servis_islem_kategorileri.servis_islem_kategori_id,
-    GROUP_CONCAT(servis_islem_kategorileri.servis_islem_kategori_adi SEPARATOR '<br> ') AS yapilan_islemler
+    urun_renkleri.renk_adi,servis_islem_kategorileri.servis_islem_kategori_id
 FROM servis_islemleri
 INNER JOIN servis_islem_kategorileri ON servis_islemleri.servis_islem_tanim_id = servis_islem_kategorileri.servis_islem_kategori_id
 INNER JOIN servisler ON servisler.servis_id = servis_islemleri.servis_tanim_id
@@ -1319,7 +1318,7 @@ GROUP BY servisler.servis_id
 
             $data[] = [
                 
-                '<a style="cursor:pointer"  class="custom-href" onclick="showWindow(`'.base_url("servis/servis_detay/".$row->servis_id).'`);"><b>'.$row->servis_kod.'</b></a><br>'.$row->yapilan_islemler, 
+                '<a style="cursor:pointer"  class="custom-href" onclick="showWindow(`'.base_url("servis/servis_detay/".$row->servis_id).'`);"><b>'.$row->servis_kod.'</b></a>', 
 			  '<span style="color:green"><b>S. Açılış : </b>'.date("d.m.Y H:i",strtotime($row->servis_kayit_tarihi)).'</span><br>'. $date_close,
 			  
 			  "<b>".strtoupper($row->urun_adi)." (".$row->renk_adi.")</b><br>".$row->seri_numarasi
