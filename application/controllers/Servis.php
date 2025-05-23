24c91521bbdf3a1dd0a1ae543977a2858971e54c
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-05-23 14:35:03 +0300

    asf

--- application/controllers/Servis.php
+++ application/controllers/Servis.php
@@ -1197,10 +1197,7 @@ sendSmsData("05453950049","SERVİS KAYDI AÇILDI ".date("d.m.Y H:i")."\n".base_u
 
 
 		$kategoriler = $this->input->get('kategoriler'); // dizi olarak gelir
-
-if ($kategoriler && is_array($kategoriler)) {
-    $this->db->where_in('servis_islem_kategori_id', $kategoriler);
-}
+ 
 
 
 
@@ -1231,7 +1228,7 @@ INNER JOIN merkezler ON merkezler.merkez_id = siparisler.merkez_no
 INNER JOIN musteriler ON musteriler.musteri_id = merkezler.merkez_yetkili_id
 INNER JOIN sehirler ON sehirler.sehir_id = merkezler.merkez_il_id
 INNER JOIN ilceler ON ilceler.ilce_id = merkezler.merkez_ilce_id
-WHERE servis_islem_kategorileri.servis_islem_kategori_id IN (8,5,7)
+WHERE servis_islem_kategorileri.servis_islem_kategori_id IN ($kategoriler)
 GROUP BY servisler.servis_id
 ");
 		
