d643cb885073e43ba78042066d6c02b587b8ca9a
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-05-23 12:07:48 +0300

    asf

--- application/views/includes/map-config-rg.php
+++ application/views/includes/map-config-rg.php
@@ -18,7 +18,7 @@ foreach ($sehirler as $sehir) {
       "hover" => $sehir->sehir_adi,
       "url" => base_url("cihaz/rg_medikal_cihaz_harita_il_detay/".$sehir->sehir_id."/".$secilen_urun),
       "target" => "same_window",
-      "upColor" =>"#008814" ,
+      "upColor" =>"rgb(17, 0, 92)" ,
       "overColor" => "#3535358f",
       "downColor" => "#0c0c0c",
       "active" => true
