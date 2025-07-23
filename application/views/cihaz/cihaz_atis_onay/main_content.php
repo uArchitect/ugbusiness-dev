1f007f83c0bd0d4dcb5b241822aa774ac4e254e4
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-07-23 14:29:48 +0300

    asf

--- application/views/cihaz/cihaz_atis_onay/main_content.php
+++ application/views/cihaz/cihaz_atis_onay/main_content.php
@@ -17,11 +17,11 @@
           <h4 class="m-0" style="font-size: 18px; font-weight: 600;"><?=$cihaz->merkez_adi?></h4> 
            <p class="m-0" style="font-size: 18px; line-height: 1.5;"><?=$cihaz->merkez_adresi?></p>
            <div class="row">
-            <div class="col">
-              <a class="btn btn-default btn-xs" href="tel:<?=$cihaz->musteri_iletisim_numarasi?>"><i class="fa fa-phone"></i> TELEFON ARA </a>
+            <div class="col p-0">
+              <a class="btn btn-default btn-xs" style="width: -webkit-fill-available;" href="tel:<?=$cihaz->musteri_iletisim_numarasi?>"><i class="fa fa-phone"></i> TELEFON ARA </a>
             </div>
-             <div class="col">
-              <a class="btn btn-default btn-xs" href="https://wa.me/9<?=$cihaz->musteri_iletisim_numarasi?>"><i class="fab fa-whatsapp"></i> WHATSAPP </a>
+             <div class="col p-0">
+              <a class="btn btn-default btn-xs" style="width: -webkit-fill-available;" href="https://wa.me/9<?=$cihaz->musteri_iletisim_numarasi?>"><i class="fab fa-whatsapp"></i> WHATSAPP </a>
             </div>
            </div>
         </div>
