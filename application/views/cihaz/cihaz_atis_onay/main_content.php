ff853e814a9769fdd3509d92631ead798f6e3321
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-07-23 15:06:59 +0300

    asf

--- application/views/cihaz/cihaz_atis_onay/main_content.php
+++ application/views/cihaz/cihaz_atis_onay/main_content.php
@@ -87,7 +87,7 @@
 
         <div class="mt-0">
          <?php 
-         if($onaylandi == true){
+         if($onaylandi == false){
           ?>
           <a href="<?=base_url("api/cihaz_atis_genel_mudur_onay/$serino/1000?securitykey=9cdd1a22ab314caa8515393cb6b93938")?>" class="btn btn-warning btn-lg py-3  shadow-sm" style="width: -webkit-fill-available;font-size: 22px; font-weight: 600; letter-spacing: 1px; transition: all 0.3s ease; background-color: #28a745; border-color: #28a745; border-radius: 5px 5px 10px 10px;">
             GEÇİCİ ONAY VER
@@ -109,6 +109,6 @@
 
 
 
-    
+
   </section>
 </div>
