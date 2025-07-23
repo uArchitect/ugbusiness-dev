b84255684897d37ff3bb200c446f32c92df094d9
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-07-23 15:11:52 +0300

    asf

--- application/views/cihaz/cihaz_atis_onay/main_content.php
+++ application/views/cihaz/cihaz_atis_onay/main_content.php
@@ -95,7 +95,7 @@
           <?php
          }else{
 ?>
-    <a href="<?=base_url()?>" class="btn btn-success btn-lg py-3  shadow-sm" style="width: -webkit-fill-available;font-size: 22px; font-weight: 600; letter-spacing: 1px; transition: all 0.3s ease; background-color: #28a745; border-color: #28a745; border-radius: 5px 5px 10px 10px;">
+    <a  class="btn btn-success btn-lg py-3  shadow-sm" style="width: -webkit-fill-available;font-size: 22px; font-weight: 600; letter-spacing: 1px; transition: all 0.3s ease; background-color: #28a745; border-color: #28a745; border-radius: 5px 5px 10px 10px;">
                 ATIŞ YÜKLEME ONAYI VERİLDİ
               </a>
               <a href="<?=base_url("api/cihaz_atis_genel_mudur_onay/$serino/2?securitykey=9cdd1a22ab314caa8515393cb6b93938")?>" class="btn btn-danger btn-lg py-3  shadow-sm mt-2" style="width: -webkit-fill-available;font-size: 22px; font-weight: 600; letter-spacing: 1px; transition: all 0.3s ease;   border-radius: 5px 5px 10px 10px;">
