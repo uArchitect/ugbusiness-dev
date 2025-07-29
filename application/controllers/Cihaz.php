c05912b7948bbea8020a32496ebd19b9a99066d1
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-07-29 13:41:37 +0300

    asf

--- application/controllers/Cihaz.php
+++ application/controllers/Cihaz.php
@@ -1094,9 +1094,14 @@ if(empty($this->input->post('il_id'))){
 } else{
       $filter_il_id = $this->input->post('il_id');
 }
+
+if($this->input->post('cihaz_id')!=9999){
+   
+  $this->db->where(["merkezler.merkez_il_id"=> $filter_il_id]);
+}
  
     $query = $this->db->where(["siparis_urun_aktif"=>1])
-    ->where(["merkezler.merkez_il_id"=> $filter_il_id])
+    
     ->where(["urunler.urun_id"=> $filter_cihaz_id])
     ->where(["seri_numarasi !="=> ""])
     ->select("musteriler.musteri_kayit_tarihi,kullanicilar.kullanici_ad_soyad,merkezler.merkez_kayit_guncelleme_notu,musteriler.musteri_kayit_guncelleme_notu,musteriler.musteri_ad,borclu_cihazlar.borc_durum as cihaz_borc_uyarisi,musteriler.musteri_id,musteriler.musteri_kod,musteriler.musteri_iletisim_numarasi,
