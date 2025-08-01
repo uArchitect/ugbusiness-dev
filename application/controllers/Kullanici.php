99770ab76d3b79a492cc50286896d5293d946762
Ergül Kızılkaya <kizilkayaergul@gmail.com>
2025-08-01 15:23:21 +0300

    asf

--- application/controllers/Kullanici.php
+++ application/controllers/Kullanici.php
@@ -959,10 +959,72 @@ JOIN siparis_onay_adimlari
           kullanicilar.kullanici_bolge";
 
             $query6 = $this->db->query($sql6);
+
+if ($ay_filtre != 0) {
+    $baslangic = date('dmY0000', strtotime("01-" . $ay_filtre . "-" . $secilen_yil)); 
+    $bitis = date('dmY2359', strtotime("last day of " . $secilen_yil . "-" . $ay_filtre)); 
+} else {
+    $baslangic = date('dmY0000', strtotime("first day of this month"));
+    $bitis = date('dmY2359', strtotime("last day of this month"));
+}
+
+ function curlitjson($url,$content) {
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER,
+        array("Content-type: application/json"));
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $content);				
+        $json_response = curl_exec($curl);
+        $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_close($curl);				
+        return($json_response);
+        }
+        
+        try {
+           
+            date_default_timezone_set('Europe/Istanbul');
+            $one_day_before = strtotime('-10 day', strtotime(date("Y-m-d")));
+            $start_date =   $baslangic; 
+            $end_date = $bitis;
+           
+             
+            $ayar = $this->Ayar_model->get_by_id(1); 
+            $arr_acc = array('usercode' => $ayar[0]->netgsm_kullanici_ad, 'password' => base64_decode($ayar[0]->netgsm_kullanici_sifre), 'startdate' => $start_date, 'stopdate' => $end_date);				
+            $url_acc = "https://api.netgsm.com.tr/netsantral/report";  
+            $content_acc = json_encode($arr_acc);				  
+            $send_acc = curlitjson($url_acc,$content_acc);
+            $send_acc = json_decode($send_acc,true);
+            $viewData["santral_kayitlar"] = $send_acc;
+        } catch (Exception $exc)
+          {  
+          echo $exc->getMessage();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
             $viewData["satis_bolge_adet_reports"] = $query6->result(); 
             
             $viewData["secilen_yil"] = $secilen_yil;
             $viewData["current_month"] = $ay_filtre;
+
+
+
+
+
             $viewData["page"] = "kullanici/muhasebe_rapor";
             $this->load->view('base_view',$viewData);
 
