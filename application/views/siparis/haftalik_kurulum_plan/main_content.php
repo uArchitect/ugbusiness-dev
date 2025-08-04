75853dd57f85580abdaab00e3a33832ce11ee06a
ugyazilim <ugyazilim@gmail.com>
2025-08-04 15:39:59 +0300

    asf

--- application/views/siparis/haftalik_kurulum_plan/main_content.php
+++ application/views/siparis/haftalik_kurulum_plan/main_content.php
@@ -73,6 +73,7 @@
           $urunlerdata = get_siparis_urunleri($value->siparis_id);
           foreach ($urunlerdata as $ur) {
             echo "<b>".$ur->urun_adi." (".$ur->renk_adi.")</b>".($ur->yenilenmis_cihaz_mi == 1 ? "<span class='bg-success  ' style='display: block; padding: 15px; text-align:center;  border-radius: 7px;'>Yenilenmiş Cihaz</span>" : "")."<br>".$ur->seri_numarasi."<br>";
+         echo "<br><span class='bg-warning'>$takas_alinan_seri_kod</span>";
           }
           ?>
           </div>
