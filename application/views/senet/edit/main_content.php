d3cd57ef04a6d2b5c8901c11a4f1c69ae20ddaaa
ugyazilim <ugyazilim@gmail.com>
2025-08-04 19:09:07 +0300

    asf

--- application/views/senet/edit/main_content.php
+++ application/views/senet/edit/main_content.php
@@ -7,16 +7,16 @@
     <section class="content">
         <div class="row">
             <div class="col-md-6">
-                <div class="box box-primary">
-                    <div class="box-header with-border">
-                        <h3 class="box-title">Senet Bilgileri</h3>
+                <div class="card card-primary">
+                    <div class="card-header with-border">
+                        <h3 class="card-title">Senet Bilgileri</h3>
                     </div>
                     <?php echo validation_errors('<div class="alert alert-danger">', '</div>'); ?>
                     <?php if($this->session->flashdata('error')): ?>
                         <div class="alert alert-danger"><?php echo $this->session->flashdata('error'); ?></div>
                     <?php endif; ?>
                     <form role="form" action="<?php echo base_url('senet/duzenle/' . $senet->id); ?>" method="post">
-                        <div class="box-body">
+                        <div class="card-body">
                             <div class="form-group">
                                 <label for="musteri_adsoyad">Müşteri Ad Soyad</label>
                                 <input type="text" class="form-control" id="musteri_adsoyad" name="musteri_adsoyad" placeholder="Müşteri adını ve soyadını giriniz" value="<?php echo $senet->musteri_adsoyad; ?>">
@@ -30,7 +30,7 @@
                                 <input type="date" class="form-control" id="senet_tarihi" name="senet_tarihi" value="<?php echo $senet->senet_tarihi; ?>">
                             </div>
                         </div>
-                        <div class="box-footer">
+                        <div class="card-footer">
                             <button type="submit" class="btn btn-primary">Güncelle</button>
                             <a href="<?php echo base_url('senet'); ?>" class="btn btn-default">İptal</a>
                         </div>
