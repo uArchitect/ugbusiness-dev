b35c623879b28b216e568eda8bbf6e60e3c8c4d8
ugyazilim <ugyazilim@gmail.com>
2025-08-08 10:11:33 +0300

    asf

--- application/controllers/Kullanici.php
+++ application/controllers/Kullanici.php
@@ -781,25 +781,6 @@ public function siralama_guncelle() {
 
 
 
-
-         $CI = get_instance();
-  $CI->load->model('Talep_model');
- 
-      $CI->db->select('talepler.*,talep_kaynaklari.*', false);
-      $CI->db->from('talepler');
-      $CI->db->join('talep_kaynaklari', 'talep_kaynaklari.talep_kaynak_id = talep_kaynak_no');
-      $CI->db->where("(talep_sorumlu_kullanici_id = 1 OR talep_sorumlu_kullanici_id = 4) AND talep_cep_telefon = '05320510678'");
-      
-      $CI->db->order_by('talepler.talep_id', "DESC");
-      
-      $query = $CI->db->get()->result();
-
-echo json_encode($query);    return;
-  if(count($query) > 0 || count($query) > 0){
-    return 1;
-  }else{
-    return 0;
-  }
  
 
   
