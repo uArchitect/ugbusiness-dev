0a0b3746dfb1cc5710ae350bd97bb98d3a4b7a9d
ugyazilim <ugyazilim@gmail.com>
2025-09-04 10:32:24 +0300

    asf

--- application/views/teklif_form/rapor/report.php
+++ application/views/teklif_form/rapor/report.php
@@ -151,7 +151,7 @@
     </tbody>
   </table>
   <p class="content"><?=$teklif_form->teklif_form_birinci_not?></p>
-    <p class="content" style="color:red"><?=$teklif_form->teklif_form_ucuncu_not?></p><br>
+    <p class="content"><?=$teklif_form->teklif_form_ucuncu_not?></p>
   <p class="content" style="color:red"><?=$teklif_form->teklif_form_ikinci_not?></p><br>
   <p class="" style="display:none;color:red;text-align:right;    margin-right: 35px;">*Fiyatlarımızda KDV hariçtir.</p>
 </div>
