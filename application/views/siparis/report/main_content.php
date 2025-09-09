d17bd33b54bd81248b3e29ffae837531aa9d0349
ugyazilim <ugyazilim@gmail.com>
2025-09-09 18:38:06 +0300

    asf

--- application/views/siparis/report/main_content.php
+++ application/views/siparis/report/main_content.php
@@ -1461,21 +1461,10 @@ margin-bottom: 20px;">
                <?php 
                $cihazlarhavuz = get_havuz($urun->urun_id,$urun->renk_id,$urun->yenilenmis_cihaz_mi);
                foreach ($cihazlarhavuz as $value) {
-
-                 if($value->anakart_onayi == false){
-                  ?>
-                    <option disabled><?=$value->cihaz_havuz_seri_numarasi?> (ANAKART ONAYI VERİLMEMİŞ)</option>
-   
-                  <?php
-               }else{
-                ?>
-                  <option value="<?=$value->cihaz_havuz_seri_numarasi?>" <?=($urun->seri_numarasi==$value->cihaz_havuz_seri_numarasi)?"selected":""?>><?=$value->cihaz_havuz_seri_numarasi?></option>
-   
-                <?php
-               }
+ 
                ?>
-              
-              
+               <option value="<?=$value->cihaz_havuz_seri_numarasi?>" <?=($urun->seri_numarasi==$value->cihaz_havuz_seri_numarasi)?"selected":""?>><?=$value->cihaz_havuz_seri_numarasi?></option>
+   
                <?php
                }
                
