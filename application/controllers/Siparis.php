7868209c823757d42a82a4bb159e978c24084b84
ugyazilim <ugyazilim@gmail.com>
2025-09-12 15:15:59 +0300

    asf

--- application/controllers/Siparis.php
+++ application/controllers/Siparis.php
@@ -281,7 +281,7 @@ class Siparis extends CI_Controller {
 
 	public function report($id = '',$modal_format = 0)
 	{ 	echo $id."<br><br>";
-		$id = urldecode(str_replace("Gg3TGGUcv29CpA8aUcpwV2KdjCz8aE","",base64_decode($id)));
+		$id =str_replace("Gg3TGGUcv29CpA8aUcpwV2KdjCz8aE","",urldecode(base64_decode($id)));
 		$check_id = $this->Siparis_model->get_by_id($id); 
 		//echo json_encode($id);return;
         if($check_id){  
