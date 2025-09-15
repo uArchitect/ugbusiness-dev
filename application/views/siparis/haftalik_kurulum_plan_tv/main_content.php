85d437f1e7dd62a7a47c63cc245d78a3132c07ea
ugyazilim <ugyazilim@gmail.com>
2025-09-15 13:38:58 +0300

    asf

--- application/views/siparis/haftalik_kurulum_plan_tv/main_content.php
+++ application/views/siparis/haftalik_kurulum_plan_tv/main_content.php
@@ -509,18 +509,16 @@ setInterval(saatiGuncelle, 1000);
                    <?php 
                    foreach ($kartokutmayanlar as $k) {
                     ?>
-
-
-<div class="widget-user-header bg-dark">
-                <div class="widget-user-image">
-                  <img class="img-circle elevation-2" src="<?=base_url("uploads/$k->kullanici_resim")?>" alt="User Avatar">
-                </div>
-                <!-- /.widget-user-image -->
-                <h3 class="widget-user-username"><?=$k->kullanici_ad_soyad?></h3>
-                <h5 class="widget-user-desc">Üretim</h5>
-              </div>
-
- 
+                    
+                     <div class="row">
+                       <div class="col-12" style="padding-left: 0px;">
+                         <span style="display: block;background: #fd3838;padding: 5px;color: #ffffff;border-radius: 5px;border-radius: 3px 3px 0 0;">
+                           <span style="min-width: 230px; width: 230px;   margin-left:5px">
+                             <b style="color: #ffffff;font-size:10px;"><?=$k->kullanici_ad_soyad?></b>
+                           </span>
+                         </span>         
+                       </div>
+                     </div><br>
                     <?php
                    }
                    
