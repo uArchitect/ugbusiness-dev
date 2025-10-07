df1cc5edcc882e73408d5c9e8ba67cdd2f635d0c
ugyazilim@gmail.com <ugyazilim@gmail.com>
2025-10-07 11:19:59 +0300

    as

--- application/views/kullanici/mesai_genel_bakis/main_content.php
+++ application/views/kullanici/mesai_genel_bakis/main_content.php
@@ -147,7 +147,7 @@
 
 if (user.servis_var_mi == 1) {
 
-  cardClass = 'bg-gradient-to-br from-blue-600 from-blue-700 text-white';
+  cardClass = 'bg-gradient-to-br from-blue-500 from-blue-700 text-white';
            extraContent = `<p class="text-sm font-semibold">SERVİSTE</p>`;
 }
 else{
