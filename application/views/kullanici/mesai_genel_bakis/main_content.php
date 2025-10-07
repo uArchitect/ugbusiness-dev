f92322418167e8c42082fcde5cb134c3adb53f55
ugyazilim@gmail.com <ugyazilim@gmail.com>
2025-10-07 10:51:50 +0300

    test

--- application/views/kullanici/mesai_genel_bakis/main_content.php
+++ application/views/kullanici/mesai_genel_bakis/main_content.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Mesai Takip Kartları</title>
+   <meta http-equiv="refresh" content="10">
   <script src="https://cdn.tailwindcss.com"></script>
   <style>
     html, body {
