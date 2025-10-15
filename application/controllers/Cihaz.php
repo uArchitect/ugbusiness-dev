f5c9ac61a6b010ab0f122ffe10d90837d71a8d79
ugyazilim@gmail.com <ugyazilim@gmail.com>
2025-10-15 14:29:14 +0300

    s

--- application/controllers/Cihaz.php
+++ application/controllers/Cihaz.php
@@ -1085,8 +1085,7 @@ function cihaz_havuz_stok_sil($stok_id = 0) {
      
     yetki_kontrol("cihazlari_goruntule");
     $query = $this->db->where(["siparis_urun_aktif"=>1])
-    ->where(["merkezler.merkez_il_id"=> 1])
-     ->or_where(["merkezler.merkez_il_id"=> 58])
+    ->where(["merkezler.merkez_il_id = 1 or merkezler.merkez_il_id = 58"])
     ->where(["seri_numarasi !="=> ""])
     ->select("musteriler.musteri_kayit_tarihi,kullanicilar.kullanici_ad_soyad,merkezler.merkez_kayit_guncelleme_notu,musteriler.musteri_kayit_guncelleme_notu,musteriler.musteri_ad,borclu_cihazlar.borc_durum as cihaz_borc_uyarisi,musteriler.musteri_id,musteriler.musteri_kod,musteriler.musteri_iletisim_numarasi,
     merkezler.merkez_adi,merkezler.merkez_adresi,merkezler.merkez_yetkili_id,  merkezler.merkez_id,
