12660c3191c620958f32099a46d84683fbad243d
ugyazilim@gmail.com <ugyazilim@gmail.com>
2025-10-23 17:56:50 +0300

    afs

--- application/views/kullanici/mesai_page/main_content.php
+++ application/views/kullanici/mesai_page/main_content.php
@@ -115,6 +115,24 @@ function createPersonnelCard(array $data, int $departmanId, string $baslik, stri
         echo '          </span>';
         echo '        </a>';
     }
+
+
+    if($departmanId == 11){
+        for ($i=0; $i < 5 ; $i++) { 
+               echo '        <a   target="_blank" type="button" ';
+                echo '          style="flex-grow: 1; ' . $flexBasis . ' height:65px; line-height:12px; padding:5px; border-radius:3px;';
+                echo '          border:2px solid black; background-color:' . $renk . '; color:' . $yaziRenk . ';">';
+                echo '          <span style="font-size:7px">';
+                echo '            <b style="font-size:10px">';
+                echo                  htmlspecialchars($personel["kullanici_ad_soyad"]) . '<br>';
+                echo '              <span style="opacity:1;font-weight:400;font-size:11px">';
+                echo                  $personel["mesai_baslama_saati"];
+                echo '              </span>';
+                echo '            </b>';
+                echo '          </span>';
+                echo '        </a>';
+        }
+    }
     
     // HTML Bitişi
     echo '      </div>'; // .d-flex.flex-wrap
