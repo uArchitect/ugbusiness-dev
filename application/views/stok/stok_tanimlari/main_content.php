93c81c3f34cae1e450371998aa510f216f8f32c2
ugyazilim@gmail.com <ugyazilim@gmail.com>
2025-11-11 18:35:12 +0300

    afs

--- application/views/stok/stok_tanimlari/main_content.php
+++ application/views/stok/stok_tanimlari/main_content.php
@@ -367,7 +367,7 @@ oninput="this.value = this.value.toUpperCase();">
 
 
 
-  <div class="card d-none <?=($_GET["filter"] == "stok-lamba-giris") ? "" : ""?>" id="kayitform" style="border-radius:0px;margin-bottom: 5px !important;">
+  <div class="card   <?=($_GET["filter"] == "stok-lamba-giris") ? "" : ""?>" id="kayitform" style="display:none;border-radius:0px;margin-bottom: 5px !important;">
     <div class="card-header" style="background: #181818;color:white;padding-top: 8px;padding-bottom: 4px;">
       <h3 class="card-title" style="font-weight: 700;">Lamba Stok Giriş</h3>
       <div class="card-tools">
