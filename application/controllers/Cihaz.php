59811de9af0814fe1292d24bd0059f3c1e2c1f3f
ugyazilim@gmail.com <ugyazilim@gmail.com>
2025-11-12 14:46:52 +0300

    clean empty

--- application/controllers/Cihaz.php
+++ application/controllers/Cihaz.php
@@ -1466,10 +1466,8 @@ $filter_merkez_adresi = ((strlen($row->merkez_adresi) > 50) ? mb_substr($row->me
 
     public function showroom_urun_sil($id)
     {
-
         $this->db->delete('showroom_cihazlar', array('showroom_cihaz_id' => $id));
         redirect($_SERVER['HTTP_REFERER']); 
-            
     }
 
 
