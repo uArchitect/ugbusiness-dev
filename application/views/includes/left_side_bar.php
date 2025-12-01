090a159f0747b49fa6bfe1a37e4d0c0732700c23
ugyazilim@gmail.com <141937429+ugyazilim@users.noreply.github.com>
2025-12-01 14:59:02 +0300

    Remove conditional display for user ID 9 in left_side_bar.php, streamlining the navigation structure and enhancing code clarity.

--- application/views/includes/left_side_bar.php
+++ application/views/includes/left_side_bar.php
@@ -2048,7 +2048,6 @@ if($this->session->userdata('aktif_kullanici_id') == 1 || $this->session->userda
                 </p>
                 </a>
             </li>
-            <?php if(aktif_kullanici()->kullanici_id == 9): ?>
             <li class="nav-item">
                 <a href="<?=base_url("ayar/arac_kilometre_ortalamalari")?>" class="nav-link">
                 <i class="fas fa-tachometer-alt nav-icon" style="font-size:13px"></i>
